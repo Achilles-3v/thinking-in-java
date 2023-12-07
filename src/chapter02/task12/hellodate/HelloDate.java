7db2f14465f88a3a10e5ebbd1b46a32946e86499
Achilles-3v <sh17vv@gmail.com>
2023-12-07 22:44:35 +0500

    task12 added

---
+++ src/chapter02/task12/hellodate/HelloDate.java
@@ -0,0 +1,21 @@
+package chapter02.task12.hellodate;
+import java.util.Date;
+
+/** Первая программа-пример книги.
+ * Выводит строку и текущее число
+ * @author Achilles-3v
+ * @version 3.0
+ */
+
+public class HelloDate {
+
+    /** Точка входа в класс и приложение
+     * @param args Массив строковых аргументов
+     * @throws exceptions Исключения не выдаются
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello, now: ");
+        System.out.println(new Date());
+    }
+}

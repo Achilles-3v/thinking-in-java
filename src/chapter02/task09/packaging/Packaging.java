a9d07cb17c310f87d9441d725bce841d7b9e8ce1
Achilles-3v <sh17vv@gmail.com>
2023-12-07 22:28:28 +0500

    task09 and task10 added

---
+++ src/chapter02/task09/packaging/Packaging.java
@@ -0,0 +1,16 @@
+package chapter02.task09.packaging;
+
+public class Packaging {
+
+    public static void main(String[] args) {
+        Integer i = 45;
+        System.out.println(i);
+        int ni = i;
+        System.out.println(ni + 2);
+
+        Character ch = 'F';
+        System.out.println(ch);
+        char nch = ch;
+        System.out.println(nch);
+    }
+}
---
+++ src/chapter02/task10/allthecolors/AllTheColors.java
@@ -0,0 +1,14 @@
+package chapter02.task10.allthecolors;
+
+public class AllTheColors {
+
+    public static void main(String[] args) {
+        int anIntegerRepresentingColors = 74025;
+
+        changeTheHueOfTheColor(anIntegerRepresentingColors);
+    }
+
+    static void changeTheHueOfTheColor(int newHue) {
+        System.out.println("Color: " + newHue);
+    }
+}

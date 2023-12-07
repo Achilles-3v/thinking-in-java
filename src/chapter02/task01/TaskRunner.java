aa323b713d08089775b519209fd1575cf8f24bf1
Achilles-3v <sh17vv@gmail.com>
2023-12-07 20:46:31 +0500

    chapter02.task01 added

---
+++ src/chapter02/task01/TaskRunner.java
@@ -0,0 +1,12 @@
+package chapter02.task01;
+
+public class TaskRunner {
+
+    static int a;
+    static char b;
+
+    public static void main(String[] args) {
+
+        System.out.println(a + " " + b);
+    }
+}

102494cc77c3c76e830a3ed8dfcf6317637be42f
Achilles-3v <sh17vv@gmail.com>
2023-12-09 00:50:15 +0500

    chapter04.task17 added

--- src/chapter04/task17/randomwhile/RandomWhile.java
+++ src/chapter04/task17/randomwhile/RandomWhile.java
@@ -1,4 +1,27 @@
 package chapter04.task17.randomwhile;
 
+import java.util.Random;
+
 public class RandomWhile {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        while (true) {
+            int a = random.nextInt(10);
+            int b = random.nextInt(10);
+
+            if (a > b) {
+                System.out.println(a + " > " + b);
+            }
+            else if (a < b) {
+                System.out.println(a + " < " + b);
+            }
+            else {
+                System.out.println(a + " = " + b);
+                break;
+            }
+        }
+    }
 }

1a581da47a30e6590bc180e98ca846a96332695b
Achilles-3v <sh17vv@gmail.com>
2023-12-09 02:23:55 +0500

    chapter04.task18 added

--- src/chapter04/task18/primenumber/PrimeNumber.java
+++ src/chapter04/task18/primenumber/PrimeNumber.java
@@ -1,4 +1,21 @@
 package chapter04.task18.primenumber;
 
 public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 1000; i++) {
+            int count = 0;
+
+            for (int j = 2; j < i; j++) {
+                if ((i % j) == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }

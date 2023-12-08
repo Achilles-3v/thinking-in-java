2b5036b006fb27fd47a61d015ac83ca7f0cfdf2a
Achilles-3v <sh17vv@gmail.com>
2023-12-09 00:32:21 +0500

    chapter04.task16 added

--- src/chapter04/task16/randomnumber/RandomNumber.java
+++ src/chapter04/task16/randomnumber/RandomNumber.java
@@ -1,4 +1,25 @@
 package chapter04.task16.randomnumber;
 
+import java.util.Random;
+
 public class RandomNumber {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        for (int i = 0; i < 25; i++) {
+            int a = random.nextInt(10);
+            int b = random.nextInt(10);
+
+            if (a > b) {
+                System.out.println(a + " > " + b);
+            }
+            else if (a < b) {
+                System.out.println(a + " < " + b);
+            }
+            else {
+                System.out.println(a + " = " + b);
+            }
+        }
+    }
 }

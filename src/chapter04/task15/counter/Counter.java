dfaaf901bc612731c7bed5d22227b9e1b5020a0f
Achilles-3v <sh17vv@gmail.com>
2023-12-09 00:16:22 +0500

    chapter04.task15 added

--- src/chapter04/task15/counter/Counter.java
+++ src/chapter04/task15/counter/Counter.java
@@ -1,4 +1,13 @@
 package chapter04.task15.counter;
 
 public class Counter {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            int c = i + 1;
+            if((c % 2) == 0)
+                System.out.println("i = " + c);
+        }
+    }
 }

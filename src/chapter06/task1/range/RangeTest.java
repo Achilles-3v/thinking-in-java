e3f3a30f24fb407c674de56fe1b55eb56b840573
Achilles-3v <sh17vv@gmail.com>
2023-12-18 15:23:01 +0500

    chapter06.task1.range RangeTest

--- src/chapter06/task1/range/RangeTest.java
+++ src/chapter06/task1/range/RangeTest.java
@@ -1,5 +1,14 @@
 package chapter06.task1.range;
 
+import java.util.Arrays;
+import static net.achilles.util.Range.range;
+
 public class RangeTest {
 
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(range(10)));
+        System.out.println(Arrays.toString(range(10, 20)));
+        System.out.println(Arrays.toString(range(10, 50, 4)));
+    }
 }

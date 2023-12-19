de7d4d2b890401bb68e670be74b8e82e8795fd27
Achilles-3v <sh17vv@gmail.com>
2023-12-19 05:56:18 +0500

    chapter07.task10 SpaceShipControl

---
+++ src/chapter07/task10/spaceshipdelegation/SpaceShip.java
@@ -0,0 +1,17 @@
+package chapter07.task10.spaceshipdelegation;
+
+public class SpaceShip extends SpaceShipControls{
+
+    private String name;
+
+    public SpaceShip(String name) {
+        this.name = name;
+    }
+
+    public String toString() { return name; }
+
+    public static void main(String[] args) {
+        SpaceShip protector = new SpaceShip("NSEA Protector");
+        protector.forward(100);
+    }
+}

631aa1f4b6d69aed6a847f74467b6cef7e3dde15
Achilles-3v <sh17vv@gmail.com>
2023-12-20 05:59:30 +0500

    chapter07.task16 Amphibian

---
+++ src/chapter07/task16/amphibian/Amphibian.java
@@ -0,0 +1,26 @@
+package chapter07.task16.amphibian;
+
+public class Amphibian {
+
+    private String name;
+    private double weight;
+
+    public Amphibian(String name) {
+        this.name = name;
+        this.weight = 0.0;
+    }
+
+    protected void set(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String toString() {
+        return name + " weight " + weight + " kg";
+    }
+
+    public void swim() {
+        System.out.println(name + " swims");
+    }
+    public void regeneration() {}
+}

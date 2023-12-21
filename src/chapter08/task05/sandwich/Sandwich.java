19a1cbcfd28144ec73125d707e3caa4c6af972aa
Achilles-3v <sh17vv@gmail.com>
2023-12-21 12:15:32 +0500

    chapter08.task05 Sandwich

---
+++ src/chapter08/task05/sandwich/Sandwich.java
@@ -0,0 +1,57 @@
+package chapter08.task05.sandwich;
+
+/**
+ * Порядок вызова конструкторов
+ * @version 1.5 2023-12-21
+ * @author Bruce Eckel
+ */
+
+class Meal {
+    Meal() {
+        System.out.println("Meal()");
+    }
+}
+
+class Bread {
+    Bread() {
+        System.out.println("Bread()");
+    }
+}
+
+class Cheese {
+    Cheese() {
+        System.out.println("Cheese()");
+    }
+}
+
+class Lettuce {
+    Lettuce() {
+        System.out.println("Lettuce");
+    }
+}
+
+class Lunch extends Meal {
+    Lunch() {
+        System.out.println("Lunch()");
+    }
+}
+
+class PortableLunch extends Lunch {
+    PortableLunch() {
+        System.out.println("PortableLunch()");
+    }
+}
+
+public class Sandwich extends PortableLunch {
+    private Bread b = new Bread();
+    private Cheese c = new Cheese();
+    private Lettuce l = new Lettuce();
+
+    public Sandwich() {
+        System.out.println("Sandwich()");
+    }
+
+    public static void main(String[] args) {
+        new Sandwich();
+    }
+}

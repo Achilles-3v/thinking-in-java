529c195e9714ddae70074d1c6ec778c5ba5c9a3e
Achilles-3v <sh17vv@gmail.com>
2023-12-24 21:52:46 +0500

    chapter09.task19 Factories update

--- src/chapter09/task19/interfaces/Factories.java
+++ src/chapter09/task19/interfaces/Factories.java
@@ -16,10 +16,59 @@ interface ServiceFactory {
     Service getService();
 }
 
+class Implementation1 implements Service {
+    Implementation1() {} // Доступ в пределах пакета
+
+    @Override
+    public void method1() {
+        System.out.println("Implementation1 method1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Implementation1 method2");
+    }
+}
+
+class Implementation1Factory implements ServiceFactory {
+    @Override
+    public Service getService() {
+        return new Implementation1();
+    }
+}
+
+class Implementation2 implements Service {
+    Implementation2() {} // Доступ в пределах пакета
+
+    @Override
+    public void method1() {
+        System.out.println("Implementation2 method1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Implementation2 method2");
+    }
+}
+
+class Implementation2Factory implements ServiceFactory {
+    @Override
+    public Service getService() {
+        return new Implementation2();
+    }
+}
+
 public class Factories {
 
+    public static void serviceConsumer(ServiceFactory factory) {
+        Service s = factory.getService();
+        s.method1();
+        s.method2();
+    }
+
     public static void main(String[] args) {
-
-
+        serviceConsumer(new Implementation1Factory());
+        // Реализация полностью взаимозаменяемы:
+        serviceConsumer(new Implementation2Factory());
     }
 }

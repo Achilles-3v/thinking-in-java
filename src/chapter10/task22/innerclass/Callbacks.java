42dd574f1733d068195e38fedf3ffbe1542eadf7
Achilles-3v <sh17vv@gmail.com>
2023-12-28 22:27:47 +0500

    chapter10.task22 Callbacks update

--- src/chapter10/task22/innerclass/Callbacks.java
+++ src/chapter10/task22/innerclass/Callbacks.java
@@ -20,5 +20,35 @@ class Callee1 implements Incrementable {
     }
 }
 
+class MyIncrement {
+    public void increment() {
+        System.out.println("Другая операция");
+    }
+
+    public static void f(MyIncrement mi) { mi.increment(); }
+}
+
+// Если класс должен вызвать метод increment()
+// по-другому, необходимо использовать внутренний класс:
+class Callee2 extends MyIncrement {
+    private int i = 0;
+    public void increment() {
+        super.increment();
+        i++;
+        System.out.println(i);
+    }
+    private class Closure implements Incrementable {
+        @Override
+        public void increment() {
+            // Указывает метод внешнего класса, иначе
+            // возникает бесконечная рекурсия
+            Callee2.this.increment();
+        }
+    }
+    Incrementable getCallbackReference() {
+        return new Closure();
+    }
+}
+
 public class Callbacks {
 }

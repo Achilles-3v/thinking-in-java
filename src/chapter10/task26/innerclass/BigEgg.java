5539c735e4044e93203418035eb3157a84d08e98
Achilles-3v <sh17vv@gmail.com>
2023-12-30 21:36:23 +0500

    chapter10.task26 BigEgg

---
+++ src/chapter10/task26/innerclass/BigEgg.java
@@ -0,0 +1,11 @@
+package chapter10.task26.innerclass;
+
+/**
+ * Внутренний класс нельзя переопределить
+ * подобно обычному методу
+ * @version 1.26 2023-12-30
+ * @author Bruce Eckel
+ */
+
+public class BigEgg {
+}

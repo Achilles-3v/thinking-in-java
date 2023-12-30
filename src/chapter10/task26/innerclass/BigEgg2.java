7358670168e12686c80699b097ebc8213d9a1784
Achilles-3v <sh17vv@gmail.com>
2023-12-30 22:50:02 +0500

    chapter10.task26 BigEgg2 update

--- src/chapter10/task26/innerclass/BigEgg2.java
+++ src/chapter10/task26/innerclass/BigEgg2.java
@@ -25,5 +25,22 @@ class Egg2 {
     public void g() { y.f(); }
 }
 
-public class BigEgg2 {
+public class BigEgg2 extends Egg2 {
+    public class Yolk extends Egg2.Yolk {
+        public Yolk() {
+            System.out.println("BigEgg2.Yolk()");
+        }
+
+        @Override
+        public void f() {
+            System.out.println("BigEgg2.Yolk.f()");
+        }
+    }
+
+    public BigEgg2() { insertYolk(new Yolk()); }
+
+    public static void main(String[] args) {
+        Egg2 e2 = new BigEgg2();
+        e2.g();
+    }
 }

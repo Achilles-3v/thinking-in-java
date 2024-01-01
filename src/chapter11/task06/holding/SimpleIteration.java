e98b21cc3d944b441ea0cad02f14f8adbe499c73
Achilles-3v <sh17vv@gmail.com>
2024-01-01 22:18:15 +0500

    chapter11.task06 SimpleIteration

---
+++ src/chapter11/task06/holding/SimpleIteration.java
@@ -0,0 +1,15 @@
+package chapter11.task06.holding;
+
+import java.util.*;
+
+/**
+ * Работа итератора
+ * @version 1.06 2024-01-01
+ * @author Achilles
+ */
+
+public class SimpleIteration {
+    public static void main(String[] args) {
+        System.out.println(Pet.iterator(new ArrayList<String>()));
+    }
+}

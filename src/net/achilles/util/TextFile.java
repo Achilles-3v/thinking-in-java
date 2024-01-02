18aa33a3a18df3413637c9e0edf1e2d2989e7533
Achilles-3v <sh17vv@gmail.com>
2024-01-02 22:00:52 +0500

    net.achilles.util.TextFile

---
+++ src/net/achilles/util/TextFile.java
@@ -0,0 +1,55 @@
+package net.achilles.util;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Static functions for reading and writing text files as
+ * a single string, and treating a file as an ArrayList.
+ * @version 1.03 2024-01-02
+ * @author Bruce Eckel
+ */
+
+public class TextFile extends ArrayList<String> {
+    // Read a file as a single string:
+    public static String read(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader in= new BufferedReader(new FileReader(
+                    new File(fileName).getAbsoluteFile()));
+            try {
+                String s;
+                while((s = in.readLine()) != null) {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            } finally {
+                in.close();
+            }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+    // Write a single file in one method call:
+    public static void write(String fileName, String text) {
+        try {
+            PrintWriter out = new PrintWriter(
+                    new File(fileName).getAbsoluteFile());
+            try {
+                out.print(text);
+            } finally {
+                out.close();
+            }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    // Read a file, split by any regular expression:
+    public TextFile(String fileName, String splitter) {
+        super(Arrays.asList(read(fileName).split(splitter)));
+        // Regular expression split() often leaves an empty
+        // String at the first position:
+        if(get(0).equals("")) remove(0);
+    }
+
+}

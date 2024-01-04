429a333bc33051362a95fa8d6cae08db6cff3b06
Achilles-3v <sh17vv@gmail.com>
2024-01-04 21:31:02 +0500

    chapter11.task17 AdapterMethodIdiom

---
+++ src/chapter11/task17/holding/AdapterMethodIdiom.java
@@ -0,0 +1,44 @@
+package chapter11.task17.holding;
+
+import java.util.*;
+
+/**
+ * Идиома "Метод-Адаптер" позволяет использовать foreach
+ * с другими разновидностями Iterable
+ * @version 1.17 2024-01-04
+ * @author Bruce Eckel
+ * @param <T>
+ */
+
+class ReversibleArrayList<T> extends ArrayList<T> {
+    public ReversibleArrayList(Collection<T> c) { super(c); }
+    public Iterable<T> reversed() {
+        return new Iterable<T>() {
+            public Iterator<T> iterator() {
+                return new Iterator<T>() {
+                    int current = size() - 1;
+                    public boolean hasNext() { return current > -1; }
+                    public T next() { return get(current--); }
+                    public void remove() { // Not implemented
+                        throw new UnsupportedOperationException();
+                    }
+                };
+            }
+        };
+    }
+}
+
+public class AdapterMethodIdiom {
+    public static void main(String[] args) {
+        ReversibleArrayList<String> ral =
+                new ReversibleArrayList<String>(
+                        Arrays.asList("To be or not to be".split(" ")));
+        // Grabs the ordinary iterator via iterator():
+        for(String s : ral)
+            System.out.print(s + " ");
+        System.out.println();
+        // Hand it the Iterable of your choice
+        for(String s : ral.reversed())
+            System.out.print(s + " ");
+    }
+}

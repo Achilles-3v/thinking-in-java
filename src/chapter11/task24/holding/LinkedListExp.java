c2585c33ad4d3b9ce0abf7e01ea8b9a0a7d18ecb
Achilles-3v <sh17vv@gmail.com>
2024-01-06 21:57:42 +0500

    chapter11.task24 LinkedListExp update

--- src/chapter11/task24/holding/LinkedListExp.java
+++ src/chapter11/task24/holding/LinkedListExp.java
@@ -1,5 +1,7 @@
 package chapter11.task24.holding;
 
+import java.util.LinkedList;
+
 public class LinkedListExp {
     public static void main(String[] args) {
         Student st1 = new Student("Ivan", 3);
@@ -8,6 +10,26 @@ public class LinkedListExp {
         Student st4 = new Student("Petr", 4);
         Student st5 = new Student("Mariya", 3);
 
+        LinkedList<Student> linkedList = new LinkedList<>();
+        linkedList.add(st1);
+        linkedList.add(st2);
+        linkedList.add(st3);
+        linkedList.add(st4);
+        linkedList.add(st5);
+        System.out.println(linkedList);
+        System.out.println(linkedList.get(2));
+
+        Student st6 = new Student("Zaur", 3);
+        Student st7 = new Student("Igor", 4);
+
+        linkedList.add(st6);
+        System.out.println(linkedList);
+
+        linkedList.add(1, st7);
+        System.out.println(linkedList);
+
+        linkedList.remove(3);
+        System.out.println(linkedList);
     }
 }
 

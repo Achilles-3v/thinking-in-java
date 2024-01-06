e01fea367b9f65f1ac1081d803a1753d4d069de8
Achilles-3v <sh17vv@gmail.com>
2024-01-06 22:14:39 +0500

    chapter11.task25 ListIteratorExample

---
+++ src/chapter11/ListIteratorExample.java
@@ -0,0 +1,29 @@
+package chapter11;
+
+import java.util.*;
+
+public class ListIteratorExample {
+    public static void main(String[] args) {
+        String s = "madam";
+        List<Character> list = new LinkedList<>();
+
+        for(char ch : s.toCharArray())
+            list.add(ch);
+
+        ListIterator<Character> iterator = list.listIterator();
+        ListIterator<Character> reversIterator = list.listIterator(list.size());
+        boolean isPalindrome = true;
+
+        while(iterator.hasNext() && reversIterator.hasPrevious()) {
+            if (iterator.next() != reversIterator.previous()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome)
+            System.out.println("Palindrome");
+        else
+            System.out.println("NOT Palindrome");
+    }
+}

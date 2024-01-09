069ed88696b6395d7c2e6394430850bf8a31d6b6
Achilles-3v <sh17vv@gmail.com>
2024-01-09 18:32:52 +0500

    chapter11.hashcode TypesForSets added class TreeType

--- src/chapter11/hashcode/TypesForSets.java
+++ src/chapter11/hashcode/TypesForSets.java
@@ -21,6 +21,14 @@ class HashType extends SetType {
     public int hashCode() { return i; }
 }
 
+class TreeType extends SetType
+        implements Comparable<TreeType> {
+    public TreeType(int n) { super(n); }
+    public int compareTo(TreeType arg) {
+        return (arg.i < i ? -1 : (arg.i == i ? 0 : 1));
+    }
+}
+
 public class TypesForSets {
     static <T> Set<T> fill(Set<T> set, Class<T> type) {
         try {

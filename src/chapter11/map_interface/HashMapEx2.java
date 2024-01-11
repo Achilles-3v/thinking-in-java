505204353ff0cb36bda7b8acdb9e219872cfa4b9
Achilles-3v <sh17vv@gmail.com>
2024-01-11 20:52:38 +0500

    chapter11.map_interface HashMapEx2

---
+++ src/chapter11/map_interface/HashMapEx2.java
@@ -0,0 +1,20 @@
+package chapter11.map_interface;
+
+import java.util.*;
+
+/**
+ * Попробуем добавить новый элемент с уже имеющимся ключом
+ * @version 1.41 2024-01-11
+ */
+public class HashMapEx2 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> passportsAndNames = new HashMap<>();
+
+        passportsAndNames.put(323211, "Ivanov Ivan");
+        passportsAndNames.put(627232, "Petrov Petr");
+        passportsAndNames.put(324811, "Sidorov Sidor");
+        passportsAndNames.put(627232, "Orlov Igor");
+
+        System.out.println(passportsAndNames);
+    }
+}

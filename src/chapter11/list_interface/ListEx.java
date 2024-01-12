ce0da2b0cab050050bfb1a5695eee8e98ad93e05
Achilles-3v <sh17vv@gmail.com>
2024-01-12 20:00:37 +0500

    chapter11.list_interface ListEx

---
+++ src/chapter11/list_interface/ListEx.java
@@ -0,0 +1,21 @@
+package chapter11.list_interface;
+
+import java.util.*;
+
+/**
+ * Реализация интерфейса List
+ * @version 1.51 2024-01-12
+ * @author Achilles
+ */
+public class ListEx {
+    public static void main(String[] args) {
+        List<String> people = new ArrayList<>();
+        people.add("Ivan");
+        people.add("Oleg");
+        people.add("Stepan");
+        people.add("Viktor");
+        people.add(1, "Marat");
+
+        System.out.println(people);
+    }
+}

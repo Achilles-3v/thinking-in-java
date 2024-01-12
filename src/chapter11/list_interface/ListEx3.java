4eb36199a0c08ad0c5d1c58f7632fb14e670a497
Achilles-3v <sh17vv@gmail.com>
2024-01-12 20:08:13 +0500

    chapter11.list_interface ListEx3

---
+++ src/chapter11/list_interface/ListEx3.java
@@ -0,0 +1,25 @@
+package chapter11.list_interface;
+
+import java.util.*;
+
+/**
+ * Перебор в цикле foreach
+ * @version 1.51 2024-01-12
+ * @author Achilles
+ */
+public class ListEx3 {
+    public static void main(String[] args) {
+        List<String> people = new ArrayList<>();
+        people.add("Ivan");
+        people.add("Oleg");
+        people.add("Stepan");
+        people.add("Viktor");
+        people.add(1, "Marat");
+
+        System.out.printf("ArrayList has %d elements \n", people.size());
+
+        for(String person : people){
+            System.out.println(person);
+        }
+    }
+}

6036cdbdcc053dcccc60e5a4b48dfd8b0d8e884c
Achilles-3v <sh17vv@gmail.com>
2024-01-14 20:43:09 +0500

    chapter12.stream ReduceOptional

--- src/chapter12/stream/ReduceOptional.java
+++ src/chapter12/stream/ReduceOptional.java
@@ -4,12 +4,18 @@ import java.util.*;
 
 /**
  * Использование Optional в stream().reduce()
- * @version 1.5 2024-01-14
+ * @version 1.6 2024-01-14
  * @author Achilles
  */
 public class ReduceOptional {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(2);
+        list.add(7);
+        list.add(3);
+        list.add(2);
+
         Optional<Integer> o = list.stream().reduce((accumulator, element) ->
                 accumulator*element);
         if (o.isPresent())

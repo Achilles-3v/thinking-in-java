15deea377cfdd0cc922ad0aacf3dc6fc66bc1dd9
Achilles-3v <sh17vv@gmail.com>
2024-01-15 21:21:00 +0500

    chapter21.multithreading StudentQueue

---
+++ src/chapter21/multithreading/StudentQueue.java
@@ -0,0 +1,14 @@
+package chapter21.multithreading;
+
+/**
+ * Пример многопоточности Java
+ * @version 21.2 2024-01-15
+ * @author Achilles
+ */
+public class StudentQueue {
+    private String[] names;
+
+    StudentQueue(String... names) {
+        this.names = names;
+    }
+}

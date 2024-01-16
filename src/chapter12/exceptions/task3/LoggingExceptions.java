54e6b0aa8574b13095b8d8ebba8bd5d9ff88b9ae
Achilles-3v <sh17vv@gmail.com>
2024-01-16 19:35:46 +0500

    chapter12.exceptions.task3 update

--- src/chapter12/exceptions/task3/LoggingExceptions.java
+++ src/chapter12/exceptions/task3/LoggingExceptions.java
@@ -8,6 +8,17 @@ import java.io.*;
  * @version 12.3 2024-01-16
  * @author Bruce Eckel
  */
+
+class LoggingException extends Exception {
+    private static Logger logger =
+            Logger.getLogger("LoggingException");
+    public LoggingException() {
+        StringWriter trace = new StringWriter();
+        printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
+}
+
 public class LoggingExceptions {
     public static void main(String[] args) {
 

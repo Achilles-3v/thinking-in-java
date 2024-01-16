fffc9baa5c716926d3bb48b346a6d3fbc6c79888
Achilles-3v <sh17vv@gmail.com>
2024-01-16 19:37:58 +0500

    chapter12.exceptions.task3 added main

--- src/chapter12/exceptions/task3/LoggingExceptions.java
+++ src/chapter12/exceptions/task3/LoggingExceptions.java
@@ -21,6 +21,15 @@ class LoggingException extends Exception {
 
 public class LoggingExceptions {
     public static void main(String[] args) {
-
+        try {
+            throw new LoggingException();
+        } catch(LoggingException e) {
+            System.err.println("Caught " + e);
+        }
+        try {
+            throw new LoggingException();
+        } catch(LoggingException e) {
+            System.err.println("Caught " + e);
+        }
     }
 }

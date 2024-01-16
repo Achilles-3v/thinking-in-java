d84d78c8c263aa381698141443d0953cb7dc662f
Achilles-3v <sh17vv@gmail.com>
2024-01-16 19:34:46 +0500

    chapter12.exception.task3

---
+++ src/chapter12/exceptions/task3/LoggingExceptions.java
@@ -0,0 +1,15 @@
+package chapter12.exceptions.task3;
+
+import java.util.logging.*;
+import java.io.*;
+
+/**
+ * Вывод информации об исключении через объект Logger
+ * @version 12.3 2024-01-16
+ * @author Bruce Eckel
+ */
+public class LoggingExceptions {
+    public static void main(String[] args) {
+
+    }
+}

722441165f5525645d2eed21dfbd2ce73dc90163
Achilles-3v <sh17vv@gmail.com>
2024-01-17 21:20:15 +0500

    chapter12.task5 added method main

--- src/chapter12/exceptions/task5/ExtraFeatures.java
+++ src/chapter12/exceptions/task5/ExtraFeatures.java
@@ -33,4 +33,23 @@ public class ExtraFeatures {
         System.out.println("Throwing MyException2 from h()");
         throw new MyException2("Originated in h()", 47);
     }
+
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch(MyException2 e) {
+            e.printStackTrace(System.out);
+        }
+        try {
+            g();
+        } catch(MyException2 e) {
+            e.printStackTrace(System.out);
+        }
+        try {
+            h();
+        } catch(MyException2 e) {
+            e.printStackTrace(System.out);
+            System.out.println("e.val() = " + e.val());
+        }
+    }
 }

614825ea96a87a2ff6af5537f8f571a04fad0cfb
Achilles-3v <sh17vv@gmail.com>
2024-01-17 21:31:37 +0500

    chapter12.task8

---
+++ src/chapter12/exceptions/task8/Rethrowing.java
@@ -0,0 +1,31 @@
+package chapter12.exceptions.task8;
+
+/**
+ * Демонстрация fillInStackTrace()
+ * @version 12.8 2024-01-17
+ * @author Bruce Eckel
+ */
+public class Rethrowing {
+    public static void f() throws Exception {
+        System.out.println("originating the exception in f()");
+        throw new Exception("thrown from f()");
+    }
+    public static void g() throws Exception {
+        try {
+            f();
+        } catch(Exception e) {
+            System.out.println("Inside g(),e.printStackTrace()");
+            e.printStackTrace(System.out);
+            throw e;
+        }
+    }
+    public static void h() throws Exception {
+        try {
+            f();
+        } catch(Exception e) {
+            System.out.println("Inside h(),e.printStackTrace()");
+            e.printStackTrace(System.out);
+            throw (Exception)e.fillInStackTrace();
+        }
+    }
+}

3074c1adaf98c174c406978fe8ec93d0e37a9757
Achilles-3v <sh17vv@gmail.com>
2024-01-18 21:34:52 +0500

    chapter12.task10 added method main

--- src/chapter12/exceptions/task10/DynamicFields.java
+++ src/chapter12/exceptions/task10/DynamicFields.java
@@ -84,4 +84,23 @@ public class DynamicFields {
         fields[fieldNumber][1] = value;
         return result;
     }
+    public static void main(String[] args) {
+        DynamicFields df = new DynamicFields(3);
+        System.out.println(df);
+        try {
+            df.setField("d", "A value for d");
+            df.setField("number", 47);
+            df.setField("number2", 48);
+            System.out.println(df);
+            df.setField("d", "A new value for d");
+            df.setField("number3", 11);
+            System.out.println("df: " + df);
+            System.out.println("df.getField(\"d\") : " + df.getField("d"));
+            Object field = df.setField("d", null); // Exception
+        } catch(NoSuchFieldException e) {
+            e.printStackTrace(System.out);
+        } catch(DynamicFieldsException e) {
+            e.printStackTrace(System.out);
+        }
+    }
 }

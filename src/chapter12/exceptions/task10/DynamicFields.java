1be9c4dd3a0f7b80fa3c51209af185fda4536b05
Achilles-3v <sh17vv@gmail.com>
2024-01-18 21:33:20 +0500

    chapter12.task10 update

--- src/chapter12/exceptions/task10/DynamicFields.java
+++ src/chapter12/exceptions/task10/DynamicFields.java
@@ -56,4 +56,32 @@ public class DynamicFields {
         // Recursive call with expanded fields:
         return makeField(id);
     }
+    public Object
+    getField(String id) throws NoSuchFieldException {
+        return fields[getFieldNumber(id)][1];
+    }
+    public Object setField(String id, Object value)
+            throws DynamicFieldsException {
+        if(value == null) {
+            // Most exceptions don't have a "cause" constructor.
+            // In these cases you must use initCause(),
+            // available in all Throwable subclasses.
+            DynamicFieldsException dfe =
+                    new DynamicFieldsException();
+            dfe.initCause(new NullPointerException());
+            throw dfe;
+        }
+        int fieldNumber = hasField(id);
+        if(fieldNumber == -1)
+            fieldNumber = makeField(id);
+        Object result = null;
+        try {
+            result = getField(id); // Get old value
+        } catch(NoSuchFieldException e) {
+            // Use constructor that takes "cause":
+            throw new RuntimeException(e);
+        }
+        fields[fieldNumber][1] = value;
+        return result;
+    }
 }

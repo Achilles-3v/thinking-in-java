d30b4d0118266b77ce1fc7ce0c547cec47e3c974
Achilles-3v <sh17vv@gmail.com>
2024-01-20 22:13:17 +0500

    chapter12.task15 LostMessage update

--- src/chapter12/exceptions/task15/LostMessage.java
+++ src/chapter12/exceptions/task15/LostMessage.java
@@ -19,5 +19,22 @@ class HoHumException extends Exception {
 }
 
 public class LostMessage {
-
+    void f() throws VeryImportantException {
+        throw new VeryImportantException();
+    }
+    void dispose() throws HoHumException {
+        throw new HoHumException();
+    }
+    public static void main(String[] args) {
+        try {
+            LostMessage lm = new LostMessage();
+            try {
+                lm.f();
+            } finally {
+                lm.dispose();
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
 }

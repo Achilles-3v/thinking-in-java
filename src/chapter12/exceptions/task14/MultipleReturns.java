14d0f896b43ebd2756d941096792668b2ce0cbff
Achilles-3v <sh17vv@gmail.com>
2024-01-20 22:07:24 +0500

    chapter12.task14 MultipleReturns

---
+++ src/chapter12/exceptions/task14/MultipleReturns.java
@@ -0,0 +1,28 @@
+package chapter12.exceptions.task14;
+
+/**
+ * Finally выполняется всегда
+ * @version 12.13 2024-01-20
+ * @author Bruce Eckel
+ */
+public class MultipleReturns {
+    public static void f(int i) {
+        System.out.println("Initialization that requires cleanup");
+        try {
+            System.out.println("Point 1");
+            if(i == 1) return;
+            System.out.println("Point 2");
+            if(i == 2) return;
+            System.out.println("Point 3");
+            if(i == 3) return;
+            System.out.println("End");
+            return;
+        } finally {
+            System.out.println("Performing cleanup");
+        }
+    }
+    public static void main(String[] args) {
+        for(int i = 1; i <= 4; i++)
+            f(i);
+    }
+}

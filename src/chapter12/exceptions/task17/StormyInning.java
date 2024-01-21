73978c5b74f29bebb619f428c6fa7395d9debc47
Achilles-3v <sh17vv@gmail.com>
2024-01-21 21:58:58 +0500

    chapter12.task17 extends Inning

--- src/chapter12/exceptions/task17/StormyInning.java
+++ src/chapter12/exceptions/task17/StormyInning.java
@@ -31,5 +31,24 @@ interface Storm {
     public void rainHard() throws RainedOut;
 }
 
-public class StormyInning {
+public class StormyInning extends Inning implements Storm {
+    // OK to add new exceptions for constructors, but you
+    // must deal with the base constructor exceptions:
+    public StormyInning()
+            throws RainedOut, BaseballException {}
+    public StormyInning(String s)
+            throws Foul, BaseballException {}
+    // Regular methods must conform to base class:
+//! void walk() throws PopFoul {} //Compile error
+    // Interface CANNOT add exceptions to existing
+    // methods from the base class:
+//! public void event() throws RainedOut {}
+    // If the method doesn't already exist in the
+    // base class, the exception is OK:
+    public void rainHard() throws RainedOut {}
+    // You can choose to not throw any exceptions,
+    // even if the base version does:
+    public void event() {}
+    // Overridden methods can throw inherited exceptions:
+    public void atBat() throws PopFoul {}
 }

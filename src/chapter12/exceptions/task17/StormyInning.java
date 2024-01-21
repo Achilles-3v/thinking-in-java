9668b5315ef9b17efb672c68db908917320d824b
Achilles-3v <sh17vv@gmail.com>
2024-01-21 22:00:04 +0500

    chapter12.task17 added method main

--- src/chapter12/exceptions/task17/StormyInning.java
+++ src/chapter12/exceptions/task17/StormyInning.java
@@ -51,4 +51,33 @@ public class StormyInning extends Inning implements Storm {
     public void event() {}
     // Overridden methods can throw inherited exceptions:
     public void atBat() throws PopFoul {}
+
+    public static void main(String[] args) {
+        try {
+            StormyInning si = new StormyInning();
+            si.atBat();
+        } catch(PopFoul e) {
+            System.out.println("Pop foul");
+        } catch(RainedOut e) {
+            System.out.println("Rained out");
+        } catch(BaseballException e) {
+            System.out.println("Generic baseball exception");
+        }
+        // Strike not thrown in derived version.
+        try {
+            // What happens if you upcast?
+            Inning i = new StormyInning();
+            i.atBat();
+            // You must catch the exceptions from the
+            // base-class version of the method:
+        } catch(Strike e) {
+            System.out.println("Strike");
+        } catch(Foul e) {
+            System.out.println("Foul");
+        } catch(RainedOut e) {
+            System.out.println("Rained out");
+        } catch(BaseballException e) {
+            System.out.println("Generic baseball exception");
+        }
+    }
 }

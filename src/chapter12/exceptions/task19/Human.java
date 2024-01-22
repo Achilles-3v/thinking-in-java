fcf936538f2c59f47b57c590be1af180a9b27dda
Achilles-3v <sh17vv@gmail.com>
2024-01-22 20:34:28 +0500

    chaper12.task19

---
+++ src/chapter12/exceptions/task19/Human.java
@@ -0,0 +1,13 @@
+package chapter12.exceptions.task19;
+
+/**
+ * Перехвать иерархий исключений
+ * @version 12.19 2024-01-22
+ * @author Bruce Eckel
+ */
+
+class Annoyance extends Exception {}
+class Sneeze extends Annoyance {}
+
+public class Human {
+}

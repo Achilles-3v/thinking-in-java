cbbbbe2877bac5bc3489ec9eb3849f695f8c0d93
Achilles-3v <sh17vv@gmail.com>
2024-01-22 20:38:03 +0500

    chapter12.task20

---
+++ src/chapter12/exceptions/task20/MainException.java
@@ -0,0 +1,18 @@
+package chapter12.exceptions.task20;
+
+import java.io.*;
+
+/**
+ * @version 12.19 2024-01-22
+ * @author Bruce Eckel
+ */
+public class MainException {
+    public static void main(String[] args) throws Exception {
+        // Open the file:
+        FileInputStream file =
+                new FileInputStream("MainException.java");
+        // Use the file ...
+        // Close the file:
+        file.close();
+    }
+}

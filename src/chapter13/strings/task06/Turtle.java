4c70acc7f4a226a34bc7b0597a93dc1317ac2d9b
Achilles-3v <sh17vv@gmail.com>
2024-01-23 19:19:48 +0500

    chapter12.task06 added method main

--- src/chapter13/strings/task06/Turtle.java
+++ src/chapter13/strings/task06/Turtle.java
@@ -18,4 +18,18 @@ public class Turtle {
     public void move(int x, int y) {
         f.format("%s The Turtle is at (%d,%d)\n", name, x, y);
     }
+
+    public static void main(String[] args) {
+        PrintStream outAlias = System.out;
+        Turtle tommy = new Turtle("Tommy",
+                new Formatter(System.out));
+        Turtle terry = new Turtle("Terry",
+                new Formatter(outAlias));
+        tommy.move(0,0);
+        terry.move(4,8);
+        tommy.move(3,4);
+        terry.move(2,5);
+        tommy.move(3,3);
+        terry.move(3,3);
+    }
 }

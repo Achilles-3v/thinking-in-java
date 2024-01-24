f00c312c9c41a4d5ab5482bdbd099a1002925c09
Achilles-3v <sh17vv@gmail.com>
2024-01-24 20:26:26 +0500

    chapter13.task07 added method printTotal

--- src/chapter13/strings/task07/Receipt.java
+++ src/chapter13/strings/task07/Receipt.java
@@ -18,4 +18,10 @@ public class Receipt {
         f.format("%-15.15s %5d %10.2f\n", name, qty, price);
         total += price;
     }
+    public void printTotal() {
+        f.format("%-15s %5s %10.2f\n", "Tax", "", total*0.06);
+        f.format("%-15s %5s %10s\n", "", "", "-----");
+        f.format("%-15s %5s %10.2f\n", "Total", "",
+                total * 1.06);
+    }
 }

55b9169215e30caeda4235791e4a1f902af0f537
Achilles-3v <sh17vv@gmail.com>
2024-01-24 20:42:05 +0500

    chapter13.task11 Splitting

---
+++ src/chapter13/strings/task11/Splitting.java
@@ -0,0 +1,24 @@
+package chapter13.strings.task11;
+
+import java.util.*;
+
+/**
+ * Разбивает строку по совподениям
+ * @version 13.11 2024-01-24
+ * @author Bruce Eckel
+ */
+public class Splitting {
+    public static String knights =
+            "Then, when you have found the shrubbery, you must " +
+                    "cut down the mightiest tree in the forest... " +
+                    "with... a herring!";
+    public static void split(String regex) {
+        System.out.println(
+                Arrays.toString(knights.split(regex)));
+    }
+    public static void main(String[] args) {
+        split(" "); // Doesn't have to contain regex chars
+        split("\\W+"); // Non-word characters
+        split("n\\W+"); // 'n' followed by non-word characters
+    }
+}

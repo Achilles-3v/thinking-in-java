d478dffa13fd529c9bdb46dfc4fadfda3b4e3a59
Achilles-3v <sh17vv@gmail.com>
2024-01-25 20:52:35 +0500

    chapter13.task15 Groups

---
+++ src/chapter13/strings/task15/Groups.java
@@ -0,0 +1,20 @@
+package chapter13.strings.task15;
+
+import java.util.regex.*;
+
+/**
+ * Группы
+ * @version 13.15 2024-01-25
+ * @author Bruce Eckel
+ */
+public class Groups {
+    static public final String POEM =
+            "Twas brillig, and the slithy toves\n" +
+                    "Did gyre and gimble in the wabe.\n" +
+                    "All mimsy were the borogoves,\n" +
+                    "And the mome raths outgrabe.\n\n" +
+                    "Beware the Jabberwock, my son,\n" +
+                    "The jaws that bite, the claws that catch.\n" +
+                    "Beware the Jubjub bird, and shun\n" +
+                    "The frumious Bandersnatch.";
+}

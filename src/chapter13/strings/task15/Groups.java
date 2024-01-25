59473820f2173cf98c951623d94fba17482c1a05
Achilles-3v <sh17vv@gmail.com>
2024-01-25 20:53:44 +0500

    chapter13.task15 added method main

--- src/chapter13/strings/task15/Groups.java
+++ src/chapter13/strings/task15/Groups.java
@@ -17,4 +17,14 @@ public class Groups {
                     "The jaws that bite, the claws that catch.\n" +
                     "Beware the Jubjub bird, and shun\n" +
                     "The frumious Bandersnatch.";
+    public static void main(String[] args) {
+        Matcher m =
+                Pattern.compile("(?m)(\\S+)\\s+((\\S+)\\s+(\\S+))$")
+                        .matcher(POEM);
+        while(m.find()) {
+            for(int j = 0; j <= m.groupCount(); j++)
+                System.out.print("[" + m.group(j) + "]");
+            System.out.println();
+        }
+    }
 }

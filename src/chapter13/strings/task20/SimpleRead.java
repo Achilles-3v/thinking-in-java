9d6821d2421f5dc9ff0f94c034ed0dad5ca8cb2a
Achilles-3v <sh17vv@gmail.com>
2024-01-26 23:08:24 +0500

    chapter13.task20

---
+++ src/chapter13/strings/task20/SimpleRead.java
@@ -0,0 +1,13 @@
+package chapter13.strings.task20;
+
+import java.io.*;
+
+/**
+ * Сканирование ввода
+ * @version 13.20 2024-01-26
+ * @author Bruce Eckel
+ */
+public class SimpleRead {
+    public static BufferedReader input = new BufferedReader(
+            new StringReader("Sir Robin of Camelot\n22 1.61803"));
+}

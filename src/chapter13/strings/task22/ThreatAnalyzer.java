7e4c2dee4e208591008ec61bed2744bfdeaba92d
Achilles-3v <sh17vv@gmail.com>
2024-01-27 20:10:45 +0500

    chapter13.task22

---
+++ src/chapter13/strings/task22/ThreatAnalyzer.java
@@ -0,0 +1,19 @@
+package chapter13.strings.task22;
+
+import java.util.regex.*;
+import java.util.*;
+
+/**
+ * Сканирование данных о потенциальных угрозах
+ * @version 13.22 2024-01-27
+ * @author Bruce Eckel
+ */
+public class ThreatAnalyzer {
+    static String threatData =
+            "58.27.82.161@02/10/2005\n" +
+                    "204.45.234.40@02/11/2005\n" +
+                    "58.27.82.161@02/11/2005\n" +
+                    "58.27.82.161@02/12/2005\n" +
+                    "58.27.82.161@02/12/2005\n" +
+                    "[Next log section with different data format]";
+}

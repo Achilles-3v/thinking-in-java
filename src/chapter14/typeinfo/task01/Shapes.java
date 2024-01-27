caa5a0b0e821b4ba36720dfc6f6b9ddfb767eb74
Achilles-3v <sh17vv@gmail.com>
2024-01-27 20:20:47 +0500

    chapter14.task01 added method main

--- src/chapter14/typeinfo/task01/Shapes.java
+++ src/chapter14/typeinfo/task01/Shapes.java
@@ -26,5 +26,11 @@ class Triangle extends Shape {
 }
 
 public class Shapes {
-
+    public static void main(String[] args) {
+        List<Shape> shapeList = Arrays.asList(
+                new Circle(), new Square(), new Triangle()
+        );
+        for(Shape shape : shapeList)
+            shape.draw();
+    }
 }

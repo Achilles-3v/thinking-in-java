a09bb48b7bf7716019a3bb7804f332eada35a038
Achilles-3v <sh17vv@gmail.com>
2024-01-28 20:55:43 +0500

    chapter14.task03

---
+++ src/chapter14/typeinfo/task03/ClassInitialization.java
@@ -0,0 +1,37 @@
+package chapter14.typeinfo.task03;
+
+import java.util.*;
+
+/**
+ * Инициализация откладывается до первого
+ * обращения к статическому методу
+ * @version 14.03 2024-01-28
+ * @author Bruce Eckel
+ */
+
+class Initable {
+    static final int staticFinal = 47;
+    static final int staticFinal2 =
+            ClassInitialization.rand.nextInt(1000);
+    static {
+        System.out.println("Initializing Initable");
+    }
+}
+
+class Initable2 {
+    static int staticNonFinal = 147;
+    static {
+        System.out.println("Initializing Initable2");
+    }
+}
+
+class Initable3 {
+    static int staticNonFinal = 74;
+    static {
+        System.out.println("Initializing Initable3");
+    }
+}
+
+public class ClassInitialization {
+    public static Random rand = new Random(47);
+}

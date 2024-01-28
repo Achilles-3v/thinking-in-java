f788418c9a6d8d4e3fb5c8b4293e92887af6336c
Achilles-3v <sh17vv@gmail.com>
2024-01-28 20:58:18 +0500

    chapter14.task03 added method main

--- src/chapter14/typeinfo/task03/ClassInitialization.java
+++ src/chapter14/typeinfo/task03/ClassInitialization.java
@@ -34,4 +34,18 @@ class Initable3 {
 
 public class ClassInitialization {
     public static Random rand = new Random(47);
+
+    public static void main(String[] args) throws Exception {
+        Class initable = Initable.class;
+        System.out.println("After creating Initable ref");
+        // Does not trigger initialization:
+        System.out.println(Initable.staticFinal);
+        // Does trigger initialization:
+        System.out.println(Initable.staticFinal2);
+        // Does trigger initialization:
+        System.out.println(Initable2.staticNonFinal);
+        Class initable3 = Class.forName("Initable3");
+        System.out.println("After creating Initable3 ref");
+        System.out.println(Initable3.staticNonFinal);
+    }
 }

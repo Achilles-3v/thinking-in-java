8f5915c9b0e12ab191b912618f74ab3251eb9e61
Achilles-3v <sh17vv@gmail.com>
2024-01-28 20:51:22 +0500

    chapter14.toys added method main

--- src/chapter14/typeinfo/toys/ToyTest.java
+++ src/chapter14/typeinfo/toys/ToyTest.java
@@ -27,4 +27,30 @@ public class ToyTest {
         System.out.println("Simple name: " + cc.getSimpleName());
         System.out.println("Canonical name : " + cc.getCanonicalName());
     }
+
+    public static void main(String[] args) {
+        Class c = null;
+        try {
+            c = Class.forName("typeinfo.toys.FancyToy");
+        } catch(ClassNotFoundException e) {
+            System.out.println("Can't find FancyToy");
+            System.exit(1);
+        }
+        printInfo(c);
+        for(Class face : c.getInterfaces())
+            printInfo(face);
+        Class up = c.getSuperclass();
+        Object obj = null;
+        try {
+            // Requires default constructor:
+            obj = up.newInstance();
+        } catch(InstantiationException e) {
+            System.out.println("Cannot instantiate");
+            System.exit(1);
+        } catch(IllegalAccessException e) {
+            System.out.println("Cannot access");
+            System.exit(1);
+        }
+        printInfo(obj.getClass());
+    }
 }

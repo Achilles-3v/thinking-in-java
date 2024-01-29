b6715d1b1a345ef3573516138bbb45b1b3046c33
Achilles-3v <sh17vv@gmail.com>
2024-01-29 20:48:04 +0500

    chapter14.toys GenericToyTest

---
+++ src/chapter14/typeinfo/toys/GenericToyTest.java
@@ -0,0 +1,19 @@
+package chapter14.typeinfo.toys;
+
+/**
+ * Тестирование класса Class
+ * @version 14.05 2024-01-29
+ * @author Bruce Eckel
+ */
+public class GenericToyTest {
+    public static void main(String[] args) throws Exception {
+        Class<FancyToy> ftClass = FancyToy.class;
+        // Produces exact type:
+        FancyToy fancyToy = ftClass.newInstance();
+        Class<? super FancyToy> up = ftClass.getSuperclass();
+        // This won't compile:
+        // Class<Toy> up2 = ftClass.getSuperclass();
+        // Only produces Object:
+        Object obj = up.newInstance();
+    }
+}

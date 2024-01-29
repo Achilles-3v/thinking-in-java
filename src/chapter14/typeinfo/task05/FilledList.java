f519999627a5866e21c3adae52cd2afdfc631e7e
Achilles-3v <sh17vv@gmail.com>
2024-01-29 20:44:09 +0500

    chapter14.task05 update

--- src/chapter14/typeinfo/task05/FilledList.java
+++ src/chapter14/typeinfo/task05/FilledList.java
@@ -15,5 +15,16 @@ class CountedInteger {
 }
 
 public class FilledList<T> {
-
+    private Class<T> type;
+    public FilledList(Class<T> type) { this.type = type; }
+    public List<T> create(int nElements) {
+        List<T> result = new ArrayList<T>();
+        try {
+            for(int i = 0; i < nElements; i++)
+                result.add(type.newInstance());
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
 }

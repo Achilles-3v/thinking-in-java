e0425e779cae7d76b91b67c4024ffc30718a52f1
Achilles-3v <sh17vv@gmail.com>
2024-01-30 22:31:09 +0500

    chapter14.task06

---
+++ src/chapter14/typeinfo/task06/ClassCasts.java
@@ -0,0 +1,14 @@
+package chapter14.typeinfo.task06;
+
+/**
+ * Привидения типов для ссылок на Class
+ * @version 14.06 2024-01-30
+ * @author Bruce Eckel
+ */
+
+class Building {}
+class House extends Building {}
+
+public class ClassCasts {
+
+}

e543fcbed1544a3355ac35c01c17dec79f51a3e4
Achilles-3v <sh17vv@gmail.com>
2024-02-01 22:45:09 +0500

    chapter14.pets PetCreator added method randomPet()

--- src/chapter14/typeinfo/pets/PetCreator.java
+++ src/chapter14/typeinfo/pets/PetCreator.java
@@ -11,4 +11,15 @@ public abstract class PetCreator {
     private Random rand = new Random(47);
     // The List of the different types of Pet to create:
     public abstract List<Class<? extends Pet>> types();
+
+    public Pet randomPet() { // Create one random Pet
+        int n = rand.nextInt(types().size());
+        try {
+            return types().get(n).newInstance();
+        } catch(InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch(IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

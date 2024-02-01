357c8126fdba4b94146ef6f045b4c38082eb985d
Achilles-3v <sh17vv@gmail.com>
2024-02-01 22:46:07 +0500

    chapter14.pets PetCreator added method createArray

--- src/chapter14/typeinfo/pets/PetCreator.java
+++ src/chapter14/typeinfo/pets/PetCreator.java
@@ -22,4 +22,11 @@ public abstract class PetCreator {
             throw new RuntimeException(e);
         }
     }
+
+    public Pet[] createArray(int size) {
+        Pet[] result = new Pet[size];
+        for(int i = 0; i < size; i++)
+            result[i] = randomPet();
+        return result;
+    }
 }

4ec0c1af2768f6158ca118642873f731b29d157f
Achilles-3v <sh17vv@gmail.com>
2024-02-04 21:09:00 +0500

    chapter14.task11

---
+++ src/chapter14/typeinfo/task11/SimpleProxyDemo.java
@@ -0,0 +1,15 @@
+package chapter14.typeinfo.task11;
+
+/**
+ * Структура заместителя
+ * @version 14.11 2024-02-04
+ * @author Bruce Eckel
+ */
+
+interface Interface {
+    void doSomething();
+    void somethingElse(String arg);
+}
+
+public class SimpleProxyDemo {
+}

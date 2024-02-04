2edf89a75be9bb37e0e3db7636a108d0a782f287
Achilles-3v <sh17vv@gmail.com>
2024-02-04 21:11:07 +0500

    chapter14.task11 added class SimpleProxy

--- src/chapter14/typeinfo/task11/SimpleProxyDemo.java
+++ src/chapter14/typeinfo/task11/SimpleProxyDemo.java
@@ -19,5 +19,20 @@ class RealObject implements Interface {
     }
 }
 
+class SimpleProxy implements Interface {
+    private Interface proxied;
+    public SimpleProxy(Interface proxied) {
+        this.proxied = proxied;
+    }
+    public void doSomething() {
+        System.out.println("SimpleProxy doSomething");
+        proxied.doSomething();
+    }
+    public void somethingElse(String arg) {
+        System.out.println("SimpleProxy somethingElse " + arg);
+        proxied.somethingElse(arg);
+    }
+}
+
 public class SimpleProxyDemo {
 }

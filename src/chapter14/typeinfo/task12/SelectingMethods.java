1eb9fc03a6085c0694edb2f3114d4f6eab7c7b0e
Achilles-3v <sh17vv@gmail.com>
2024-02-05 19:51:06 +0500

    chapter14.task12 added method main

--- src/chapter14/typeinfo/task12/SelectingMethods.java
+++ src/chapter14/typeinfo/task12/SelectingMethods.java
@@ -42,4 +42,14 @@ class Implementation implements SomeMethods {
 }
 
 public class SelectingMethods  {
+    public static void main(String[] args) {
+        SomeMethods proxy= (SomeMethods)Proxy.newProxyInstance(
+                SomeMethods.class.getClassLoader(),
+                new Class[]{ SomeMethods.class },
+                new MethodSelector(new Implementation()));
+        proxy.boring1();
+        proxy.boring2();
+        proxy.interesting("bonobo");
+        proxy.boring3();
+    }
 }

02a5c62566355c9aa637d7d8a9f6bef672e7483f
Achilles-3v <sh17vv@gmail.com>
2024-02-05 19:50:10 +0500

    chapter14.task12 added class Implementation

--- src/chapter14/typeinfo/task12/SelectingMethods.java
+++ src/chapter14/typeinfo/task12/SelectingMethods.java
@@ -29,5 +29,17 @@ interface SomeMethods {
     void boring3();
 }
 
+class Implementation implements SomeMethods {
+    public void boring1() {
+        System.out.println("boring1"); }
+    public void boring2() {
+        System.out.println("boring2"); }
+    public void interesting(String arg) {
+        System.out.println("interesting " + arg);
+    }
+    public void boring3() {
+        System.out.println("boring3"); }
+}
+
 public class SelectingMethods  {
 }

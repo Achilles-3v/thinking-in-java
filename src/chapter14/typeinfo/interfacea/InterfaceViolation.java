ae0e1feaf422b1fc2cd8726e20588ad788fcd8d0
Achilles-3v <sh17vv@gmail.com>
2024-02-07 19:58:20 +0500

    chapter14.interfacea InterfaceViolation

---
+++ src/chapter14/typeinfo/interfacea/InterfaceViolation.java
@@ -0,0 +1,15 @@
+package chapter14.typeinfo.interfacea;
+
+/**
+ * Программирование в обход интерфейса
+ * @version 14.15 2024-02-07
+ * @author Bruce Eckel
+ */
+
+class B implements A {
+    public void f() {}
+    public void g() {}
+}
+
+public class InterfaceViolation {
+}

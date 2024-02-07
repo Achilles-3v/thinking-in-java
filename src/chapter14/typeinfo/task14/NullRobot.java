adc9b80d04a437f6d2962959dc3c93c98fc9d17c
Achilles-3v <sh17vv@gmail.com>
2024-02-07 19:52:49 +0500

    chapter14.task14 added method main

--- src/chapter14/typeinfo/task14/NullRobot.java
+++ src/chapter14/typeinfo/task14/NullRobot.java
@@ -38,4 +38,13 @@ public class NullRobot {
                 new Class[]{ Null.class, Robot.class },
                 new NullRobotProxyHandler(type));
     }
+
+    public static void main(String[] args) {
+        Robot[] bots = {
+                new SnowRemovalRobot("SnowBee"),
+                newNullRobot(SnowRemovalRobot.class)
+        };
+        for(Robot bot : bots)
+            Robot.Test.test(bot);
+    }
 }

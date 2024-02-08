006c4473d3554327b6f3906857fa51edffd53acf
Achilles-3v <sh17vv@gmail.com>
2024-02-08 19:35:28 +0500

    chapter14.task15 InnerImplementation

---
+++ src/chapter14/typeinfo/task15/InnerImplementation.java
@@ -0,0 +1,29 @@
+package chapter14.typeinfo.task15;
+
+import chapter14.typeinfo.interfacea.*;
+
+/**
+ * Закрытые внутренние классы не скрыты от отражения
+ * @version 14.15 2024-02-08
+ * @author Bruce Eckel
+ */
+
+class InnerA {
+    private static class C implements A {
+        public void f() {
+            System.out.println("public C.f()"); }
+        public void g() {
+            System.out.println("public C.g()"); }
+        void u() {
+            System.out.println("package C.u()"); }
+        protected void v() {
+            System.out.println("protected C.v()"); }
+        private void w() {
+            System.out.println("private C.w()"); }
+    }
+    public static A makeA() { return new C(); }
+}
+
+public class InnerImplementation {
+
+}

6de650ab4e4460a50790a51ee2674af3ee7ee099
Achilles-3v <sh17vv@gmail.com>
2024-02-09 20:35:20 +0500

    chapter15 Holder3

---
+++ src/chapter15/generics/Holder3.java
@@ -0,0 +1,20 @@
+package chapter15.generics;
+
+/**
+ * Класс преднозначенный для хранения одного объекта
+ * @version 15.03 2024-02-09
+ * @author Bruce Eckel
+ */
+public class Holder3<T> {
+    private T a;
+    public Holder3(T a) { this.a = a; }
+    public void set(T a) { this.a = a; }
+    public T get() { return a; }
+    public static void main(String[] args) {
+        Holder3<Automobile> h3 =
+                new Holder3<Automobile>(new Automobile());
+        Automobile a = h3.get(); // No cast needed
+        // h3.set("Not an Automobile"); // Error
+        // h3.set(1); // Error
+    }
+}

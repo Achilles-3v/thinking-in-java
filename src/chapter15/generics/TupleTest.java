718b137069a7a2771b38aec985552087557e5341
Achilles-3v <sh17vv@gmail.com>
2024-02-10 19:41:11 +0500

    chapter15 TupleTest

---
+++ src/chapter15/generics/TupleTest.java
@@ -0,0 +1,15 @@
+package chapter15.generics;
+
+import net.achilles.util.*;
+
+/**
+ * Создание произвольного кортежа благодаря обобщениям
+ * @version 15.04 2024-02-09
+ * @author Bruce Eckel
+ */
+
+class Amphibian {}
+class Vehicle {}
+
+public class TupleTest {
+}

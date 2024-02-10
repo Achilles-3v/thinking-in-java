77f0eb965cb32e7c086084027afd06784e8cc088
Achilles-3v <sh17vv@gmail.com>
2024-02-10 19:48:59 +0500

    chapter15 LinkedStack added method main

--- src/chapter15/generics/LinkedStack.java
+++ src/chapter15/generics/LinkedStack.java
@@ -16,8 +16,25 @@ public class LinkedStack<T> {
         }
         boolean end() { return item == null && next == null; }
     }
+
     private Node<T> top = new Node<T>(); // End sentinel
     public void push(T item) {
         top = new Node<T>(item, top);
+    }
+
+    public T pop() {
+        T result = top.item;
+        if(!top.end())
+            top = top.next;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LinkedStack<String> lss = new LinkedStack<String>();
+        for(String s : "Phasers on stun!".split(" "))
+            lss.push(s);
+        String s;
+        while((s = lss.pop()) != null)
+            System.out.println(s);
     }
 }

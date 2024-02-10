ffcd0adf0540db0c4311676b56d8c965c4b67325
Achilles-3v <sh17vv@gmail.com>
2024-02-10 19:52:17 +0500

    chapter15 RandomList

---
+++ src/chapter15/generics/RandomList.java
@@ -0,0 +1,25 @@
+package chapter15.generics;
+
+import java.util.*;
+
+/**
+ * Рандомный вызов элементов
+ * @version 15.06 2024-02-09
+ * @author Bruce Eckel
+ */
+public class RandomList<T> {
+    private ArrayList<T> storage = new ArrayList<T>();
+    private Random rand = new Random(47);
+    public void add(T item) { storage.add(item); }
+    public T select() {
+        return storage.get(rand.nextInt(storage.size()));
+    }
+    public static void main(String[] args) {
+        RandomList<String> rs = new RandomList<String>();
+        for(String s: ("The quick brown fox jumped over " +
+                "the lazy brown dog").split(" "))
+            rs.add(s);
+        for(int i = 0; i < 11; i++)
+            System.out.print(rs.select() + " ");
+    }
+}

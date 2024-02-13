bfe13faada0cafe139e127214e235578b0eb22e6
Achilles-3v <sh17vv@gmail.com>
2024-02-13 22:12:03 +0500

    Generators added method main

--- src/chapter15/generics/Generators.java
+++ src/chapter15/generics/Generators.java
@@ -16,4 +16,15 @@ public class Generators {
             coll.add(gen.next());
         return coll;
     }
+
+    public static void main(String[] args) {
+        Collection<Coffee> coffee = fill(
+                new ArrayList<Coffee>(), new CoffeeGenerator(), 4);
+        for(Coffee c : coffee)
+            System.out.println(c);
+        Collection<Integer> fnumbers = fill(
+                new ArrayList<Integer>(), new Fibonacci(), 12);
+        for(int i : fnumbers)
+            System.out.print(i + ", ");
+    }
 }

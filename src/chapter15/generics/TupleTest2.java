dbfa0fc81cc801296217cb28dab0afae17bdaf43
Achilles-3v <sh17vv@gmail.com>
2024-02-14 20:24:03 +0500

    TupleTest2 added method main

--- src/chapter15/generics/TupleTest2.java
+++ src/chapter15/generics/TupleTest2.java
@@ -23,4 +23,12 @@ public class TupleTest2 {
         return tuple(new Vehicle(), new Amphibian(),
                 "hi", 47, 11.1);
     }
+    public static void main(String[] args) {
+        TwoTuple<String,Integer> ttsi = f();
+        System.out.println(ttsi);
+        System.out.println(f2());
+        System.out.println(g());
+        System.out.println(h());
+        System.out.println(k());
+    }
 }

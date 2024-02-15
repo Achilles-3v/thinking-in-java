bffcfc785324919819873bb706bd1e0758c7f3ab
Achilles-3v <sh17vv@gmail.com>
2024-02-15 21:36:53 +0500

    chapter15 WatercolorSets

---
+++ src/chapter15/generics/WatercolorSets.java
@@ -0,0 +1,30 @@
+package chapter15.generics;
+
+import chapter15.generics.watercolors.*;
+import java.util.*;
+import static net.achilles.util.Sets.*;
+import static chapter15.generics.watercolors.Watercolors.*;
+
+/**
+ * @version 15.18 2024-02-15
+ * @author Bruce Eckel
+ */
+public class WatercolorSets {
+    public static void main(String[] args) {
+        Set<Watercolors> set1 =
+                EnumSet.range(BRILLIANT_RED, VIRIDIAN_HUE);
+        Set<Watercolors> set2 =
+                EnumSet.range(CERULEAN_BLUE_HUE, BURNT_UMBER);
+        System.out.println("set1: " + set1);
+        System.out.println("set2: " + set2);
+        System.out.println("union(set1, set2): " + union(set1, set2));
+        Set<Watercolors> subset = intersection(set1, set2);
+        System.out.println("intersection(set1, set2): " + subset);
+        System.out.println("difference(set1, subset): " +
+                difference(set1, subset));
+        System.out.println("difference(set2, subset): " +
+                difference(set2, subset));
+        System.out.println("complement(set1, set2): " +
+                complement(set1, set2));
+    }
+}

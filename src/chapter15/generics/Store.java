423cf279260cbba37bc4cc7b5391a5677884dbdb
Achilles-3v <sh17vv@gmail.com>
2024-02-15 21:51:25 +0500

    chapter15 Store

---
+++ src/chapter15/generics/Store.java
@@ -0,0 +1,39 @@
+package chapter15.generics;
+
+import java.util.*;
+import net.achilles.util.*;
+
+/**
+ * Построение сложной модели с использованием
+ * обобщенных контейнеров
+ * @version 15.21 2024-02-15
+ * @author Bruce Eckel
+ */
+
+class Product {
+    private final int id;
+    private String description;
+    private double price;
+    public Product(int IDnumber, String descr, double price){
+        id = IDnumber;
+        description = descr;
+        this.price = price;
+        System.out.println(toString());
+    }
+    public String toString() {
+        return id + ": " + description + ", price: $" + price;
+    }
+    public void priceChange(double change) {
+        price += change;
+    }
+    public static Generator<Product> generator = new Generator<Product>() {
+        private Random rand = new Random(47);
+        public Product next() {
+            return new Product(rand.nextInt(1000), "Test",
+                    Math.round(rand.nextDouble() * 1000.0) + 0.99);
+        }
+    };
+}
+
+public class Store {
+}

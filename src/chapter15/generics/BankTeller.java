d2fdd527876bdc617a6394b244f911af86066688
Achilles-3v <sh17vv@gmail.com>
2024-02-15 21:43:17 +0500

    BankTeller added method main

--- src/chapter15/generics/BankTeller.java
+++ src/chapter15/generics/BankTeller.java
@@ -35,4 +35,16 @@ class Teller {
 }
 
 public class BankTeller {
+    public static void serve(Teller t, Customer c) {
+        System.out.println(t + " serves " + c);
+    }
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        Queue<Customer> line = new LinkedList<Customer>();
+        Generators.fill(line, Customer.generator(), 15);
+        List<Teller> tellers = new ArrayList<Teller>();
+        Generators.fill(tellers, Teller.generator, 4);
+        for(Customer c : line)
+            serve(tellers.get(rand.nextInt(tellers.size())), c);
+    }
 }

b6895ee8ff6e9161a50563a0f100985ad899a371
Achilles-3v <sh17vv@gmail.com>
2024-02-17 20:34:08 +0500

    ErasureAndInheritance added method main

--- src/chapter15/generics/ErasureAndInheritance.java
+++ src/chapter15/generics/ErasureAndInheritance.java
@@ -11,4 +11,10 @@ class Derived1<T> extends GenericBase<T> {}
 class Derived2 extends GenericBase {} // No warning
 
 public class ErasureAndInheritance {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        Derived2 d2 = new Derived2();
+        Object obj = d2.get();
+        d2.set(obj); // Warning here!
+    }
 }

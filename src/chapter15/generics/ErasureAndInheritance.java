0d408d44a0c366c425ebd2579be85a0d4ecf3373
Achilles-3v <sh17vv@gmail.com>
2024-02-17 20:33:15 +0500

    chapter15 ErasureAndInheritance

---
+++ src/chapter15/generics/ErasureAndInheritance.java
@@ -0,0 +1,14 @@
+package chapter15.generics;
+
+class GenericBase<T> {
+    private T element;
+    public void set(T arg) { arg = element; }
+    public T get() { return element; }
+}
+
+class Derived1<T> extends GenericBase<T> {}
+
+class Derived2 extends GenericBase {} // No warning
+
+public class ErasureAndInheritance {
+}

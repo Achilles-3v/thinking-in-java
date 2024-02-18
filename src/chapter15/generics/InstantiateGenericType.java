4b28f433d9e2c1d4c8ea7fd00a8934f8349f4a44
Achilles-3v <sh17vv@gmail.com>
2024-02-18 21:17:39 +0500

    InstantiateGenericType added method main

--- src/chapter15/generics/InstantiateGenericType.java
+++ src/chapter15/generics/InstantiateGenericType.java
@@ -19,4 +19,15 @@ class ClassAsFactory<T> {
 class Employee {}
 
 public class InstantiateGenericType {
+    public static void main(String[] args) {
+        ClassAsFactory<Employee> fe =
+                new ClassAsFactory<Employee>(Employee.class);
+        System.out.println("ClassAsFactory<Employee> succeeded");
+        try {
+            ClassAsFactory<Integer> fi =
+                    new ClassAsFactory<Integer>(Integer.class);
+        } catch(Exception e) {
+            System.out.println("ClassAsFactory<Integer> failed");
+        }
+    }
 }

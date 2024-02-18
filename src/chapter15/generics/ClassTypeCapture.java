57ca9c36d07bf18a0614abe867c19789d231f380
Achilles-3v <sh17vv@gmail.com>
2024-02-18 21:12:08 +0500

    ClassTypeCapture added method main

--- src/chapter15/generics/ClassTypeCapture.java
+++ src/chapter15/generics/ClassTypeCapture.java
@@ -15,4 +15,14 @@ public class ClassTypeCapture<T> {
     public boolean f(Object arg) {
         return kind.isInstance(arg);
     }
+    public static void main(String[] args) {
+        ClassTypeCapture<Building> ctt1 =
+                new ClassTypeCapture<Building>(Building.class);
+        System.out.println(ctt1.f(new Building()));
+        System.out.println(ctt1.f(new House()));
+        ClassTypeCapture<House> ctt2 =
+                new ClassTypeCapture<House>(House.class);
+        System.out.println(ctt2.f(new Building()));
+        System.out.println(ctt2.f(new House()));
+    }
 }

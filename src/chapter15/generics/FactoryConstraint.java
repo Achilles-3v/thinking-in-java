d6ad40e94ca204fc473320399c21622c03c1d0c9
Achilles-3v <sh17vv@gmail.com>
2024-02-19 20:46:08 +0500

    FactoryConstraint added method main

--- src/chapter15/generics/FactoryConstraint.java
+++ src/chapter15/generics/FactoryConstraint.java
@@ -33,4 +33,8 @@ class Widget {
 }
 
 public class FactoryConstraint {
+    public static void main(String[] args) {
+        new Foo2<Integer>(new IntegerFactory());
+        new Foo2<Widget>(new Widget.Factory());
+    }
 }

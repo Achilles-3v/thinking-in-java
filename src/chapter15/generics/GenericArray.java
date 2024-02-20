78d7ef816636d799c4dafc6d781197decd506538
Achilles-3v <sh17vv@gmail.com>
2024-02-20 20:00:50 +0500

    chapter15 GenericArray

---
+++ src/chapter15/generics/GenericArray.java
@@ -0,0 +1,20 @@
+package chapter15.generics;
+
+/**
+ * Обобщенная обертка для массива
+ * @version 15.25 2024-02-20
+ * @author Bruce Eckel
+ */
+public class GenericArray<T> {
+    private T[] array;
+    @SuppressWarnings("unchecked")
+    public GenericArray(int sz) {
+        array = (T[])new Object[sz];
+    }
+    public void put(int index, T item) {
+        array[index] = item;
+    }
+    public T get(int index) { return array[index]; }
+    // Method that exposes the underlying representation:
+    public T[] rep() { return array; }
+}

e6232d1b01a0a5fdd81263a0810dd3fb59a12ad0
Achilles-3v <sh17vv@gmail.com>
2024-02-21 21:07:15 +0500

    BasicBounds added classes Solid and Bounded

--- src/chapter15/generics/BasicBounds.java
+++ src/chapter15/generics/BasicBounds.java
@@ -24,5 +24,22 @@ class ColoredDimension<T extends Dimension & HasColor> {
 
 interface Weight { int weight(); }
 
+class Solid<T extends Dimension & HasColor & Weight> {
+    T item;
+    Solid(T item) { this.item = item; }
+    T getItem() { return item; }
+    java.awt.Color color() { return item.getColor(); }
+    int getX() { return item.x; }
+    int getY() { return item.y; }
+    int getZ() { return item.z; }
+    int weight() { return item.weight(); }
+}
+
+class Bounded
+        extends Dimension implements HasColor, Weight {
+    public java.awt.Color getColor() { return null; }
+    public int weight() { return 0; }
+}
+
 public class BasicBounds {
 }

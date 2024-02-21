f8430a11fa8d9f5f82b092035de6a2be46ff8b85
Achilles-3v <sh17vv@gmail.com>
2024-02-21 21:06:04 +0500

    BasicBounds added class ColoredDimension

--- src/chapter15/generics/BasicBounds.java
+++ src/chapter15/generics/BasicBounds.java
@@ -12,5 +12,17 @@ class Colored<T extends HasColor> {
 
 class Dimension { public int x, y, z; }
 
+class ColoredDimension<T extends Dimension & HasColor> {
+    T item;
+    ColoredDimension(T item) { this.item = item; }
+    T getItem() { return item; }
+    java.awt.Color color() { return item.getColor(); }
+    int getX() { return item.x; }
+    int getY() { return item.y; }
+    int getZ() { return item.z; }
+}
+
+interface Weight { int weight(); }
+
 public class BasicBounds {
 }

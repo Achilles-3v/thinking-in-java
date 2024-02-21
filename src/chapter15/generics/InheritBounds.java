5b51f9f6e2e894cb8ccf5d507b37a3f2284e2b36
Achilles-3v <sh17vv@gmail.com>
2024-02-21 21:12:12 +0500

    InheritBounds added method main

--- src/chapter15/generics/InheritBounds.java
+++ src/chapter15/generics/InheritBounds.java
@@ -31,4 +31,11 @@ class Solid2<T extends Dimension & HasColor & Weight>
 }
 
 public class InheritBounds {
+    public static void main(String[] args) {
+        Solid2<Bounded> solid2 =
+                new Solid2<Bounded>(new Bounded());
+        solid2.color();
+        solid2.getY();
+        solid2.weight();
+    }
 }

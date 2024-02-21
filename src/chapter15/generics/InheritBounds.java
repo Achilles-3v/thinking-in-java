7ac1285a0b7321628e3d78dfcdeb50b42c167a94
Achilles-3v <sh17vv@gmail.com>
2024-02-21 21:11:30 +0500

    InheritBounds added classes ColoredDimension2 and Solid2

--- src/chapter15/generics/InheritBounds.java
+++ src/chapter15/generics/InheritBounds.java
@@ -16,5 +16,19 @@ class Colored2<T extends HasColor> extends HoldItem<T> {
     java.awt.Color color() { return item.getColor(); }
 }
 
+class ColoredDimension2<T extends Dimension & HasColor>
+        extends Colored2<T> {
+    ColoredDimension2(T item) {  super(item); }
+    int getX() { return item.x; }
+    int getY() { return item.y; }
+    int getZ() { return item.z; }
+}
+
+class Solid2<T extends Dimension & HasColor & Weight>
+        extends ColoredDimension2<T> {
+    Solid2(T item) {  super(item); }
+    int weight() { return item.weight(); }
+}
+
 public class InheritBounds {
 }

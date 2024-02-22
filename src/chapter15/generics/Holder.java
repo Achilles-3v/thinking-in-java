4800375c74f2b65893b9eacfb154c1bdef4961e4
Achilles-3v <sh17vv@gmail.com>
2024-02-22 21:04:16 +0500

    Holder added method main

--- src/chapter15/generics/Holder.java
+++ src/chapter15/generics/Holder.java
@@ -14,4 +14,20 @@ public class Holder<T> {
     public boolean equals(Object obj) {
         return value.equals(obj);
     }
+
+    public static void main(String[] args) {
+        Holder<Apple> Apple = new Holder<Apple>(new Apple());
+        Apple d = Apple.get();
+        Apple.set(d);
+        // Holder<Fruit> Fruit = Apple; // Cannot upcast
+        Holder<? extends Fruit> fruit = Apple; // OK
+        Fruit p = fruit.get();
+        d = (Apple)fruit.get(); // Returns 'Object'
+        try {
+            Orange c = (Orange)fruit.get(); // No warning
+        } catch(Exception e) { System.out.println(e); }
+        // fruit.set(new Apple()); // Cannot call set()
+        // fruit.set(new Fruit()); // Cannot call set()
+        System.out.println(fruit.equals(d)); // OK
+    }
 }

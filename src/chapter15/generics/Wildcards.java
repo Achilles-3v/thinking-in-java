916be8426fea8928be90c5e69a9630b154960f87
Achilles-3v <sh17vv@gmail.com>
2024-02-24 21:32:22 +0500

    Wildcards added method main

--- src/chapter15/generics/Wildcards.java
+++ src/chapter15/generics/Wildcards.java
@@ -31,4 +31,34 @@ public class Wildcards {
         holder.set(arg);
         Object obj = holder.get();
     }
+
+    public static void main(String[] args) {
+        Holder raw = new Holder<Long>();
+        raw = new Holder();
+        Holder<Long> qualified = new Holder<Long>();
+        Holder<?> unbounded = new Holder<Long>();
+        Holder<? extends Long> bounded = new Holder<Long>();
+        Long lng = 1L;
+
+        rawArgs(raw, lng);
+        rawArgs(qualified, lng);
+        rawArgs(unbounded, lng);
+        rawArgs(bounded, lng);
+
+        unboundedArg(raw, lng);
+        unboundedArg(qualified, lng);
+        unboundedArg(unbounded, lng);
+        unboundedArg(bounded, lng);
+
+        Long r2 = exact1(qualified);
+        Object r3 = exact1(unbounded);
+        Long r4 = exact1(bounded);
+
+        Long r6 = exact2(qualified, lng);
+        Long r10 = wildSubtype(qualified, lng);
+        Object r11 = wildSubtype(unbounded, lng);
+        Long r12 = wildSubtype(bounded, lng);
+
+        wildSupertype(qualified, lng);
+    }
 }

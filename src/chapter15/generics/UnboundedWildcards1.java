5aa99635d9b890b8af9dacfbd6a1ecaf0916e8cc
Achilles-3v <sh17vv@gmail.com>
2024-02-24 21:23:23 +0500

    UnboundedWildcards1 added methods assign's

--- src/chapter15/generics/UnboundedWildcards1.java
+++ src/chapter15/generics/UnboundedWildcards1.java
@@ -11,4 +11,20 @@ public class UnboundedWildcards1 {
     static List list1;
     static List<?> list2;
     static List<? extends Object> list3;
+    static void assign1(List list) {
+        list1 = list;
+        list2 = list;
+        // list3 = list; // Warning: unchecked conversion
+        // Found: List, Required: List<? extends Object>
+    }
+    static void assign2(List<?> list) {
+        list1 = list;
+        list2 = list;
+        list3 = list;
+    }
+    static void assign3(List<? extends Object> list) {
+        list1 = list;
+        list2 = list;
+        list3 = list;
+    }
 }

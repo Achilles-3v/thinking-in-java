16ea761b84da06c4709e11c9245844ee0efa7c38
Achilles-3v <sh17vv@gmail.com>
2024-02-24 21:24:25 +0500

    UnboundedWildcards1 added method main

--- src/chapter15/generics/UnboundedWildcards1.java
+++ src/chapter15/generics/UnboundedWildcards1.java
@@ -27,4 +27,21 @@ public class UnboundedWildcards1 {
         list2 = list;
         list3 = list;
     }
+
+    public static void main(String[] args) {
+        assign1(new ArrayList());
+        assign2(new ArrayList());
+        // assign3(new ArrayList()); // Warning:
+        // Unchecked conversion. Found: ArrayList
+        // Required: List<? extends Object>
+        assign1(new ArrayList<String>());
+        assign2(new ArrayList<String>());
+        assign3(new ArrayList<String>());
+        // Both forms are acceptable as List<?>:
+        List<?> wildList = new ArrayList();
+        wildList = new ArrayList<String>();
+        assign1(wildList);
+        assign2(wildList);
+        assign3(wildList);
+    }
 }

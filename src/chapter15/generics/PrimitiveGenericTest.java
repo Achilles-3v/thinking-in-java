c7de58e3aa0c782090c11b202458f3a9399d66cb
Achilles-3v <sh17vv@gmail.com>
2024-02-25 21:59:00 +0500

    PrimitiveGenericTest added method main

--- src/chapter15/generics/PrimitiveGenericTest.java
+++ src/chapter15/generics/PrimitiveGenericTest.java
@@ -16,5 +16,14 @@ class FArray {
 }
 
 public class PrimitiveGenericTest {
-
+    public static void main(String[] args) {
+        String[] strings = FArray.fill(
+                new String[7], new RandomGenerator.String(10));
+        for(String s : strings)
+            System.out.println(s);
+        Integer[] integers = FArray.fill(
+                new Integer[7], new RandomGenerator.Integer());
+        for(int i: integers)
+            System.out.println(i);
+    }
 }

39a54c4c6dbce9ad066206ed027225a4103d025f
Achilles-3v <sh17vv@gmail.com>
2024-03-06 22:20:58 +0500

    NotSelfBounded added class D2

--- src/chapter15/generics/NotSelfBounded.java
+++ src/chapter15/generics/NotSelfBounded.java
@@ -15,3 +15,7 @@ class B2 extends NotSelfBounded<A2> {}
 class C2 extends NotSelfBounded<C2> {
     C2 setAndGet(C2 arg) { set(arg); return get(); }
 }
+
+class D2 {}
+// Now this is OK:
+class E2 extends NotSelfBounded<D2> {}

e7e019a72571a72a5eeafb19400aec6b350c5b65
Achilles-3v <sh17vv@gmail.com>
2024-03-06 22:30:40 +0500

    chapter15 added class OrdinaryArguments

---
+++ src/chapter15/generics/OrdinaryArguments.java
@@ -0,0 +1,16 @@
+package chapter15.generics;
+
+class OrdinarySetter {
+    void set(Base base) {
+        System.out.println("OrdinarySetter.set(Base)");
+    }
+}
+
+class DerivedSetter extends OrdinarySetter {
+    void set(Derived derived) {
+        System.out.println("DerivedSetter.set(Derived)");
+    }
+}
+
+public class OrdinaryArguments {
+}

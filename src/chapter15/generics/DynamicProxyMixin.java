097819387a00f675e3f665ca3cb4d47bf4ad334d
Achilles-3v <sh17vv@gmail.com>
2024-03-08 21:57:56 +0500

    DynamicProxyMixin added method main

--- src/chapter15/generics/DynamicProxyMixin.java
+++ src/chapter15/generics/DynamicProxyMixin.java
@@ -39,4 +39,17 @@ class MixinProxy implements InvocationHandler {
 }
 
 public class DynamicProxyMixin {
+    public static void main(String[] args) {
+        Object mixin = MixinProxy.newInstance(
+                tuple(new BasicImp(), Basic.class),
+                tuple(new TimeStampedImp(), TimeStamped.class),
+                tuple(new SerialNumberedImp(),SerialNumbered.class));
+        Basic b = (Basic)mixin;
+        TimeStamped t = (TimeStamped)mixin;
+        SerialNumbered s = (SerialNumbered)mixin;
+        b.set("Hello");
+        System.out.println(b.get());
+        System.out.println(t.getStamp());
+        System.out.println(s.getSerialNumber());
+    }
 }

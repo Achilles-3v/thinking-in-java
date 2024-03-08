f0be96af8a4a5f9bbf0c749b2b8df23f9ac26166
Achilles-3v <sh17vv@gmail.com>
2024-03-08 21:50:46 +0500

    Decoration added new classes

--- src/chapter15/generics/decorator/Decoration.java
+++ src/chapter15/generics/decorator/Decoration.java
@@ -8,5 +8,28 @@ class Basic {
     public String get() { return value; }
 }
 
+class Decorator extends Basic {
+    protected Basic basic;
+    public Decorator(Basic basic) { this.basic = basic; }
+    public void set(String val) { basic.set(val); }
+    public String get() { return basic.get(); }
+}
+
+class TimeStamped extends Decorator {
+    private final long timeStamp;
+    public TimeStamped(Basic basic) {
+        super(basic);
+        timeStamp = new Date().getTime();
+    }
+    public long getStamp() { return timeStamp; }
+}
+
+class SerialNumbered extends Decorator {
+    private static long counter = 1;
+    private final long serialNumber = counter++;
+    public SerialNumbered(Basic basic) { super(basic); }
+    public long getSerialNumber() { return serialNumber; }
+}
+
 public class Decoration {
 }

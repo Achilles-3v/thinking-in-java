539582cbaccad9d114010f9d3c08d1964b2fe5a1
Achilles-3v <sh17vv@gmail.com>
2024-03-08 21:51:30 +0500

    Decoration added method main

--- src/chapter15/generics/decorator/Decoration.java
+++ src/chapter15/generics/decorator/Decoration.java
@@ -32,4 +32,14 @@ class SerialNumbered extends Decorator {
 }
 
 public class Decoration {
+    public static void main(String[] args) {
+        TimeStamped t = new TimeStamped(new Basic());
+        TimeStamped t2 = new TimeStamped(
+                new SerialNumbered(new Basic()));
+        //! t2.getSerialNumber(); // Not available
+        SerialNumbered s = new SerialNumbered(new Basic());
+        SerialNumbered s2 = new SerialNumbered(
+                new TimeStamped(new Basic()));
+        //! s2.getStamp(); // Not available
+    }
 }

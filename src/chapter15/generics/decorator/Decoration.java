51b40de3e33e3c1f10a930736976e4aa7412a2a6
Achilles-3v <sh17vv@gmail.com>
2024-03-08 21:49:36 +0500

    chapter15.decorator Decoration

---
+++ src/chapter15/generics/decorator/Decoration.java
@@ -0,0 +1,12 @@
+package chapter15.generics.decorator;
+
+import java.util.*;
+
+class Basic {
+    private String value;
+    public void set(String val) { value = val; }
+    public String get() { return value; }
+}
+
+public class Decoration {
+}

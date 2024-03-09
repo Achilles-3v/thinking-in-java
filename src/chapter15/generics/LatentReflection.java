88b4f4d4462ffeb7123d1c59cc5f722d5ef9ca29
Achilles-3v <sh17vv@gmail.com>
2024-03-09 21:10:05 +0500

    LatentReflection added method main

--- src/chapter15/generics/LatentReflection.java
+++ src/chapter15/generics/LatentReflection.java
@@ -43,4 +43,9 @@ class CommunicateReflectively {
 }
 
 public class LatentReflection {
+    public static void main(String[] args) {
+        CommunicateReflectively.perform(new SmartDog());
+        CommunicateReflectively.perform(new Robot());
+        CommunicateReflectively.perform(new Mime());
+    }
 }

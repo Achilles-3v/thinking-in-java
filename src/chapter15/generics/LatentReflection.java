ac8662c52f993516c85b6ae65647c42c8138c078
Achilles-3v <sh17vv@gmail.com>
2024-03-09 21:09:15 +0500

    LatentReflection added class CommunicationReflectively

--- src/chapter15/generics/LatentReflection.java
+++ src/chapter15/generics/LatentReflection.java
@@ -20,5 +20,27 @@ class SmartDog {
     public void reproduce() {}
 }
 
+class CommunicateReflectively {
+    public static void perform(Object speaker) {
+        Class<?> spkr = speaker.getClass();
+        try {
+            try {
+                Method speak = spkr.getMethod("speak");
+                speak.invoke(speaker);
+            } catch(NoSuchMethodException e) {
+                System.out.println(speaker + " cannot speak");
+            }
+            try {
+                Method sit = spkr.getMethod("sit");
+                sit.invoke(speaker);
+            } catch(NoSuchMethodException e) {
+                System.out.println(speaker + " cannot sit");
+            }
+        } catch(Exception e) {
+            throw new RuntimeException(speaker.toString(), e);
+        }
+    }
+}
+
 public class LatentReflection {
 }

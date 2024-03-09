f5a5ca3c651a57932e49489372541ad1288138e1
Achilles-3v <sh17vv@gmail.com>
2024-03-09 21:05:35 +0500

    SimpleDogsAndRobots added method main

--- src/chapter15/generics/SimpleDogsAndRobots.java
+++ src/chapter15/generics/SimpleDogsAndRobots.java
@@ -8,4 +8,8 @@ class CommunicateSimply {
 }
 
 public class SimpleDogsAndRobots {
+    public static void main(String[] args) {
+        CommunicateSimply.perform(new PerformingDog());
+        CommunicateSimply.perform(new Robot());
+    }
 }

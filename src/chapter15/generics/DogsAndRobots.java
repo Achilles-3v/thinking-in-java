029c6e31a25498496eac4097b3a6f142e84034f9
Achilles-3v <sh17vv@gmail.com>
2024-03-09 21:02:57 +0500

    chapter15 added method main

--- src/chapter15/generics/DogsAndRobots.java
+++ src/chapter15/generics/DogsAndRobots.java
@@ -27,4 +27,10 @@ class Communicate {
 }
 
 public class DogsAndRobots {
+    public static void main(String[] args) {
+        PerformingDog d = new PerformingDog();
+        Robot r = new Robot();
+        Communicate.perform(d);
+        Communicate.perform(r);
+    }
 }

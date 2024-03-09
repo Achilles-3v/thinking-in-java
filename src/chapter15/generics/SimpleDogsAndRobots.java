112417eb6594245f65e3fdb40f9433510fe81771
Achilles-3v <sh17vv@gmail.com>
2024-03-09 21:04:33 +0500

    chapter15 added class SimpleDogsAndRobots

---
+++ src/chapter15/generics/SimpleDogsAndRobots.java
@@ -0,0 +1,11 @@
+package chapter15.generics;
+
+class CommunicateSimply {
+    static void perform(Performs performer) {
+        performer.speak();
+        performer.sit();
+    }
+}
+
+public class SimpleDogsAndRobots {
+}

500fd01d35298e4d83f58ffe0b7396cb74583f1f
Achilles-3v <sh17vv@gmail.com>
2024-03-10 21:35:10 +0500

    Apply class update

--- src/chapter15/generics/Apply.java
+++ src/chapter15/generics/Apply.java
@@ -4,6 +4,16 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class Apply {
+    public static <T, S extends Iterable<? extends T>>
+    void apply(S seq, Method f, Object... args) {
+        try {
+            for(T t: seq)
+                f.invoke(t, args);
+        } catch(Exception e) {
+            // Failures are programmer errors
+            throw new RuntimeException(e);
+        }
+    }
 }
 
 class Shape {

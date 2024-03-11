fafef0aebf52b2e366a260826f873a206b11a0a5
Achilles-3v <sh17vv@gmail.com>
2024-03-11 20:53:07 +0500

    Fill2 added class Fill2Test

--- src/chapter15/generics/Fill2.java
+++ src/chapter15/generics/Fill2.java
@@ -43,3 +43,26 @@ class AddableSimpleQueue<T>
         extends SimpleQueue<T> implements Addable<T> {
     public void add(T item) { super.add(item); }
 }
+
+class Fill2Test {
+    public static void main(String[] args) {
+        // Adapt a Collection:
+        List<Coffee> carrier = new ArrayList<Coffee>();
+        Fill2.fill(
+                new AddableCollectionAdapter<Coffee>(carrier),
+                Coffee.class, 3);
+        // Helper method captures the type:
+        Fill2.fill(Adapter.collectionAdapter(carrier),
+                Latte.class, 2);
+        for(Coffee c: carrier)
+            System.out.println(c);
+        System.out.println("----------------------");
+        // Use an adapted class:
+        AddableSimpleQueue<Coffee> coffeeQueue =
+                new AddableSimpleQueue<Coffee>();
+        Fill2.fill(coffeeQueue, Mocha.class, 4);
+        Fill2.fill(coffeeQueue, Latte.class, 1);
+        for(Coffee c: coffeeQueue)
+            System.out.println(c);
+    }
+}

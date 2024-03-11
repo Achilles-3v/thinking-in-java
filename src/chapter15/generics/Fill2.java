33f16e95eaa632cd9436a2380c2b3e406cdd0888
Achilles-3v <sh17vv@gmail.com>
2024-03-11 20:51:29 +0500

    Fill2 update

--- src/chapter15/generics/Fill2.java
+++ src/chapter15/generics/Fill2.java
@@ -7,6 +7,21 @@ import net.achilles.util.*;
 interface Addable<T> { void add(T t); }
 
 public class Fill2 {
+    public static <T> void fill(Addable<T> addable,
+                                Class<? extends T> classToken, int size) {
+        for(int i = 0; i < size; i++)
+            try {
+                addable.add(classToken.newInstance());
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+    }
+    // Generator version:
+    public static <T> void fill(Addable<T> addable,
+                                Generator<T> generator, int size) {
+        for(int i = 0; i < size; i++)
+            addable.add(generator.next());
+    }
 }
 
 class AddableCollectionAdapter<T> implements Addable<T> {

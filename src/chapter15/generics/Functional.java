bc3fee400c5e9dc4f84c15a1e46d00e99d02f3e9
Achilles-3v <sh17vv@gmail.com>
2024-03-11 20:59:42 +0500

    chapter15 added class Functional

--- src/chapter15/generics/Functional.java
+++ src/chapter15/generics/Functional.java
@@ -12,4 +12,51 @@ interface Collector<T> extends UnaryFunction<T,T> {
 interface UnaryPredicate<T> { boolean test(T x); }
 
 public class Functional {
+    public static <T> T
+    reduce(Iterable<T> seq, Combiner<T> combiner) {
+        Iterator<T> it = seq.iterator();
+        if(it.hasNext()) {
+            T result = it.next();
+            while(it.hasNext())
+                result = combiner.combine(result, it.next());
+            return result;
+        }
+        return null; // Or throw exception
+    }
+
+    public static <T> Collector<T>
+    forEach(Iterable<T> seq, Collector<T> func) {
+        for(T t : seq)
+            func.function(t);
+        return func;
+    }
+
+    public static <R,T> List<R>
+    transform(Iterable<T> seq, UnaryFunction<R,T> func) {
+        List<R> result = new ArrayList<R>();
+        for(T t : seq)
+            result.add(func.function(t));
+        return result;
+    }
+
+    public static <T> List<T>
+    filter(Iterable<T> seq, UnaryPredicate<T> pred) {
+        List<T> result = new ArrayList<T>();
+        for(T t : seq)
+            if(pred.test(t))
+                result.add(t);
+        return result;
+    }
+
+    static class IntegerAdder implements Combiner<Integer> {
+        public Integer combine(Integer x, Integer y) {
+            return x + y;
+        }
+    }
+    static class
+    IntegerSubtracter implements Combiner<Integer> {
+        public Integer combine(Integer x, Integer y) {
+            return x - y;
+        }
+    }
 }

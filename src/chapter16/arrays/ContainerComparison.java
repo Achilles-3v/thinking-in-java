7c5f1250404feb5afd74b41a3ccd9d5108b72916
Achilles-3v <sh17vv@gmail.com>
2024-03-12 21:28:34 +0500

    chapter16 added class ContainerComparison

---
+++ src/chapter16/arrays/ContainerComparison.java
@@ -0,0 +1,13 @@
+package chapter16.arrays;
+
+import java.util.*;
+
+class BerylliumSphere {
+    private static long counter;
+    private final long id = counter++;
+    public String toString() { return "Sphere " + id; }
+}
+
+public class ContainerComparison {
+
+}

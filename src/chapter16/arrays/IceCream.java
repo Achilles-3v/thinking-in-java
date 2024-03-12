13cd232ad658cfd191839af8061e38f4eceed86e
Achilles-3v <sh17vv@gmail.com>
2024-03-12 21:37:54 +0500

    chapter16 added class IceCream

---
+++ src/chapter16/arrays/IceCream.java
@@ -0,0 +1,12 @@
+package chapter16.arrays;
+
+import java.util.*;
+
+public class IceCream {
+    private static Random rand = new Random(47);
+    static final String[] FLAVORS = {
+            "Chocolate", "Strawberry", "Vanilla Fudge Swirl",
+            "Mint Chip", "Mocha Almond Fudge", "Rum Raisin",
+            "Praline Cream", "Mud Pie"
+    };
+}

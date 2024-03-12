97783b901e1e33313a87a8c484e928b3819bf5c3
Achilles-3v <sh17vv@gmail.com>
2024-03-12 21:36:07 +0500

    ArrayOptions added methods ptints

--- src/chapter16/arrays/ArrayOptions.java
+++ src/chapter16/arrays/ArrayOptions.java
@@ -21,5 +21,27 @@ public class ArrayOptions {
         a = new BerylliumSphere[]{
                 new BerylliumSphere(), new BerylliumSphere(),
         };
+
+        System.out.println("a.length = " + a.length);
+        System.out.println("b.length = " + b.length);
+        System.out.println("c.length = " + c.length);
+        System.out.println("d.length = " + d.length);
+        a = d;
+        System.out.println("a.length = " + a.length);
+
+        int[] e;
+        int[] f = new int[5];
+        System.out.println("f: " + Arrays.toString(f));
+        int[] g = new int[4];
+        for(int i = 0; i < g.length; i++)
+            g[i] = i*i;
+        int[] h = { 11, 47, 93 };
+        System.out.println("f.length = " + f.length);
+        System.out.println("g.length = " + g.length);
+        System.out.println("h.length = " + h.length);
+        e = h;
+        System.out.println("e.length = " + e.length);
+        e = new int[]{ 1, 2 };
+        System.out.println("e.length = " + e.length);
     }
 }

7975419d28e10010c82aa2b9a8e991de97cd672a
Achilles-3v <sh17vv@gmail.com>
2024-03-13 21:31:20 +0500

    chapter16 added class MultidimensionalObjectArrays

---
+++ src/chapter16/arrays/MultidimensionalObjectArrays.java
@@ -0,0 +1,18 @@
+package chapter16.arrays;
+
+import java.util.*;
+
+public class MultidimensionalObjectArrays {
+    public static void main(String[] args) {
+        BerylliumSphere[][] spheres = {
+                { new BerylliumSphere(), new BerylliumSphere() },
+                { new BerylliumSphere(), new BerylliumSphere(),
+                        new BerylliumSphere(), new BerylliumSphere() },
+                { new BerylliumSphere(), new BerylliumSphere(),
+                        new BerylliumSphere(), new BerylliumSphere(),
+                        new BerylliumSphere(), new BerylliumSphere(),
+                        new BerylliumSphere(), new BerylliumSphere() },
+        };
+        System.out.println(Arrays.deepToString(spheres));
+    }
+}

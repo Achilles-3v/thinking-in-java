96c722a4df8eb0d8038a075f468bd3d87384e7f6
Achilles-3v <sh17vv@gmail.com>
2024-03-14 20:48:24 +0500

    ArrayOfGenerics added

---
+++ src/chapter16/arrays/ArrayOfGenerics.java
@@ -0,0 +1,19 @@
+package chapter16.arrays;
+
+import java.util.*;
+
+public class ArrayOfGenerics {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        List<String>[] ls;
+        List[] la = new List[10];
+        ls = (List<String>[])la;
+        ls[0] = new ArrayList<String>();
+        Object[] objects = ls;
+        objects[1] = new ArrayList<Integer>();
+        List<BerylliumSphere>[] spheres =
+                (List<BerylliumSphere>[])new List[10];
+        for(int i = 0; i < spheres.length; i++)
+            spheres[i] = new ArrayList<BerylliumSphere>();
+    }
+}

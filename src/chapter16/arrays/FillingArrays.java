d61315a39083fb8e673606457922aa6bfafc8038
Achilles-3v <sh17vv@gmail.com>
2024-03-14 20:52:14 +0500

    chapter16 added class FillingArrays

---
+++ src/chapter16/arrays/FillingArrays.java
@@ -0,0 +1,19 @@
+package chapter16.arrays;
+
+import java.util.*;
+
+public class FillingArrays {
+    public static void main(String[] args) {
+        int size = 6;
+        boolean[] a1 = new boolean[size];
+        byte[] a2 = new byte[size];
+        char[] a3 = new char[size];
+        short[] a4 = new short[size];
+        int[] a5 = new int[size];
+        long[] a6 = new long[size];
+        float[] a7 = new float[size];
+        double[] a8 = new double[size];
+        String[] a9 = new String[size];
+        Arrays.fill(a1, true);
+    }
+}

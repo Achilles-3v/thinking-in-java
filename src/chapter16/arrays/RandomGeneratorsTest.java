d07a4c42983b43678c3f423e5f9bbf8455d6d866
Achilles-3v <sh17vv@gmail.com>
2024-03-15 21:29:10 +0500

    chapter16 added class RandomGeneratorsTest

---
+++ src/chapter16/arrays/RandomGeneratorsTest.java
@@ -0,0 +1,9 @@
+package chapter16.arrays;
+
+import net.achilles.util.*;
+
+public class RandomGeneratorsTest {
+    public static void main(String[] args) {
+        GeneratorsTest.test(RandomGenerator.class);
+    }
+}

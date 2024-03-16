10db9ebe3f29325bfe5829262c3dabcc81dcd76b
Achilles-3v <sh17vv@gmail.com>
2024-03-16 17:38:17 +0500

    chapter16 added class Reverse

---
+++ src/chapter16/arrays/Reverse.java
@@ -0,0 +1,16 @@
+package chapter16.arrays;
+
+import java.util.*;
+import net.achilles.util.*;
+
+public class Reverse {
+    public static void main(String[] args) {
+        CompType[] a = Generated.array(
+                new CompType[12], CompType.generator());
+        System.out.println("before sorting:");
+        System.out.println(Arrays.toString(a));
+        Arrays.sort(a, Collections.reverseOrder());
+        System.out.println("after sorting:");
+        System.out.println(Arrays.toString(a));
+    }
+}

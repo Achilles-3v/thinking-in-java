4c83bf8cd24175f57db1b5c3261a411fc8e03782
Achilles-3v <sh17vv@gmail.com>
2024-03-17 21:47:54 +0500

    chapter17 added class FillingLists

---
+++ src/chapter17/containers/FillingLists.java
@@ -0,0 +1,14 @@
+package chapter17.containers;
+
+import java.util.*;
+
+class StringAddress {
+    private String s;
+    public StringAddress(String s) { this.s = s; }
+    public String toString() {
+        return super.toString() + " " + s;
+    }
+}
+
+public class FillingLists {
+}

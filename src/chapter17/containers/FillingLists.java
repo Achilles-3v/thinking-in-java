bd22d9036dc29d2b574542cba28a2092bd2341c2
Achilles-3v <sh17vv@gmail.com>
2024-03-17 21:49:05 +0500

    FillingLists added method main

--- src/chapter17/containers/FillingLists.java
+++ src/chapter17/containers/FillingLists.java
@@ -11,4 +11,11 @@ class StringAddress {
 }
 
 public class FillingLists {
+    public static void main(String[] args) {
+        List<StringAddress> list= new ArrayList<StringAddress>(
+                Collections.nCopies(4, new StringAddress("Hello")));
+        System.out.println(list);
+        Collections.fill(list, new StringAddress("World!"));
+        System.out.println(list);
+    }
 }

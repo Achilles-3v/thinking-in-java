828af5e749e660d9db67b7be133c09f7cd5a3e9e
Achilles-3v <sh17vv@gmail.com>
2024-03-18 20:48:55 +0500

    net.achilles.util MapDate update

--- src/net/achilles/util/MapData.java
+++ src/net/achilles/util/MapData.java
@@ -17,4 +17,22 @@ public class MapData<K,V> extends LinkedHashMap<K,V> {
             put(genK.next(), genV.next());
         }
     }
+    // A key Generator and a single value:
+    public MapData(Generator<K> genK, V value, int quantity){
+        for(int i = 0; i < quantity; i++) {
+            put(genK.next(), value);
+        }
+    }
+    // An Iterable and a value Generator:
+    public MapData(Iterable<K> genK, Generator<V> genV) {
+        for(K key : genK) {
+            put(key, genV.next());
+        }
+    }
+    // An Iterable and a single value:
+    public MapData(Iterable<K> genK, V value) {
+        for(K key : genK) {
+            put(key, value);
+        }
+    }
 }

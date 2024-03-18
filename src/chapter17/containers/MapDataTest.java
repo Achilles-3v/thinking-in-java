986a98fbc3c94494cdb6966ca62789bf505004d6
Achilles-3v <sh17vv@gmail.com>
2024-03-18 20:53:24 +0500

    MapDateTest added method main

--- src/chapter17/containers/MapDataTest.java
+++ src/chapter17/containers/MapDataTest.java
@@ -24,4 +24,19 @@ class Letters implements Generator<Pair<Integer,String>>,
 }
 
 public class MapDataTest {
+    public static void main(String[] args) {
+        // Pair Generator:
+        System.out.println(MapData.map(new Letters(), 11));
+        // Two separate generators:
+        System.out.println(MapData.map(new CountingGenerator.Character(),
+                new RandomGenerator.String(3), 8));
+        // A key Generator and a single value:
+        System.out.println(MapData.map(new CountingGenerator.Character(),
+                "Value", 6));
+        // An Iterable and a value Generator:
+        System.out.println(MapData.map(new Letters(),
+                new RandomGenerator.String(3)));
+        // An Iterable and a single value:
+        System.out.println(MapData.map(new Letters(), "Pop"));
+    }
 }

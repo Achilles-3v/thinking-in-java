dc16506ef18e3191bbdbfafad0f9818f16cdf545
Achilles-3v <sh17vv@gmail.com>
2024-03-19 21:32:04 +0500

    Countries added static class FlyweightMap

--- src/net/achilles/util/Countries.java
+++ src/net/achilles/util/Countries.java
@@ -129,5 +129,62 @@ public class Countries {
             {"URUGUAY","Montevideo"}, {"VENEZUELA","Caracas"},
     };
 
-
+    private static class FlyweightMap
+            extends AbstractMap<String,String> {
+        private static class Entry
+                implements Map.Entry<String,String> {
+            int index;
+            Entry(int index) { this.index = index; }
+            public boolean equals(Object o) {
+                return DATA[index][0].equals(o);
+            }
+            public String getKey() { return DATA[index][0]; }
+            public String getValue() { return DATA[index][1]; }
+            public String setValue(String value) {
+                throw new UnsupportedOperationException();
+            }
+            public int hashCode() {
+                return DATA[index][0].hashCode();
+            }
+        }
+        // Use AbstractSet by implementing size() & iterator()
+        static class EntrySet
+                extends AbstractSet<Map.Entry<String,String>> {
+            private int size;
+            EntrySet(int size) {
+                if(size < 0)
+                    this.size = 0;
+                    // Can't be any bigger than the array:
+                else if(size > DATA.length)
+                    this.size = DATA.length;
+                else
+                    this.size = size;
+            }
+            public int size() { return size; }
+            private class Iter
+                    implements Iterator<Map.Entry<String,String>> {
+                // Only one Entry object per Iterator:
+                private Entry entry = new Entry(-1);
+                public boolean hasNext() {
+                    return entry.index < size - 1;
+                }
+                public Map.Entry<String,String> next() {
+                    entry.index++;
+                    return entry;
+                }
+                public void remove() {
+                    throw new UnsupportedOperationException();
+                }
+            }
+            public
+            Iterator<Map.Entry<String,String>> iterator() {
+                return new Iter();
+            }
+        }
+        private static Set<Map.Entry<String,String>> entries =
+                new EntrySet(DATA.length);
+        public Set<Map.Entry<String,String>> entrySet() {
+            return entries;
+        }
+    }
 }

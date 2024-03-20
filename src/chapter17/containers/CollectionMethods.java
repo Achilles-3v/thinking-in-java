e94c32303d5d9af4fd9191da48b8d109e4154db0
Achilles-3v <sh17vv@gmail.com>
2024-03-20 21:52:42 +0500

    chapter17 CollectionMethods

---
+++ src/chapter17/containers/CollectionMethods.java
@@ -0,0 +1,22 @@
+package chapter17.containers;
+
+import java.util.*;
+import net.achilles.util.*;
+
+public class CollectionMethods {
+    public static void main(String[] args) {
+        Collection<String> c = new ArrayList<String>();
+        c.addAll(Countries.names(6));
+        c.add("ten");
+        c.add("eleven");
+        System.out.println(c);
+        Object[] array = c.toArray();
+        String[] str = c.toArray(new String[0]);
+        System.out.println("Collections.max(c) = " + Collections.max(c));
+        System.out.println("Collections.min(c) = " + Collections.min(c));
+        Collection<String> c2 = new ArrayList<String>();
+        c2.addAll(Countries.names(6));
+        c.addAll(c2);
+        System.out.println(c);
+    }
+}

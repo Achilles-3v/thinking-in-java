d6c1415ab6fe2c6fdfcd6e6357db9bab7042474a
Achilles-3v <sh17vv@gmail.com>
2024-03-20 21:53:38 +0500

    chapter17 CollectionMethods update

--- src/chapter17/containers/CollectionMethods.java
+++ src/chapter17/containers/CollectionMethods.java
@@ -18,5 +18,27 @@ public class CollectionMethods {
         c2.addAll(Countries.names(6));
         c.addAll(c2);
         System.out.println(c);
+        c.remove(Countries.DATA[0][0]);
+        System.out.println(c);
+        c.remove(Countries.DATA[1][0]);
+        System.out.println(c);
+        c.removeAll(c2);
+        System.out.println(c);
+        c.addAll(c2);
+        System.out.println(c);
+        String val = Countries.DATA[3][0];
+        System.out.println("c.contains(" + val  + ") = " + c.contains(val));
+        System.out.println("c.containsAll(c2) = " + c.containsAll(c2));
+        Collection<String> c3 =
+                ((List<String>)c).subList(3, 5);
+        c2.retainAll(c3);
+        System.out.println(c2);
+        c2.removeAll(c3);
+        System.out.println("c2.isEmpty() = " +  c2.isEmpty());
+        c = new ArrayList<String>();
+        c.addAll(Countries.names(6));
+        System.out.println(c);
+        c.clear();
+        System.out.println("after c.clear():" + c);
     }
 }

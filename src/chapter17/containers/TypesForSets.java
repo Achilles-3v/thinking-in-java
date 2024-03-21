8563fb4f90783c66541d0d22077c4188b8c83f20
Achilles-3v <sh17vv@gmail.com>
2024-03-21 22:31:58 +0500

    TypesForSets added classes HashType and TreeType

--- src/chapter17/containers/TypesForSets.java
+++ src/chapter17/containers/TypesForSets.java
@@ -11,5 +11,18 @@ class SetType {
     public String toString() { return Integer.toString(i); }
 }
 
+class HashType extends SetType {
+    public HashType(int n) { super(n); }
+    public int hashCode() { return i; }
+}
+
+class TreeType extends SetType
+        implements Comparable<TreeType> {
+    public TreeType(int n) { super(n); }
+    public int compareTo(TreeType arg) {
+        return (arg.i < i ? -1 : (arg.i == i ? 0 : 1));
+    }
+}
+
 public class TypesForSets {
 }

372744019de3a5188ca397084ea606139f7c94a8
Achilles-3v <sh17vv@gmail.com>
2024-03-21 22:33:06 +0500

    TypesForSets added methods fill and test

--- src/chapter17/containers/TypesForSets.java
+++ src/chapter17/containers/TypesForSets.java
@@ -25,4 +25,20 @@ class TreeType extends SetType
 }
 
 public class TypesForSets {
+    static <T> Set<T> fill(Set<T> set, Class<T> type) {
+        try {
+            for(int i = 0; i < 10; i++)
+                set.add(
+                        type.getConstructor(int.class).newInstance(i));
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+        return set;
+    }
+    static <T> void test(Set<T> set, Class<T> type) {
+        fill(set, type);
+        fill(set, type); // Try to add duplicates
+        fill(set, type);
+        System.out.println(set);
+    }
 }

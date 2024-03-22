f68d3ef5439db1cc47514a8e282198a34b97c6b8
Achilles-3v <sh17vv@gmail.com>
2024-03-22 21:43:09 +0500

    ToDoList added class ToDoItem

--- src/chapter17/containers/ToDoList.java
+++ src/chapter17/containers/ToDoList.java
@@ -13,5 +13,23 @@ public class ToDoList extends PriorityQueue<ToDoList.ToDoItem> {
             secondary = sec;
             item = td;
         }
+
+        public int compareTo(ToDoItem arg) {
+            if(primary > arg.primary)
+                return +1;
+            if(primary == arg.primary)
+                if(secondary > arg.secondary)
+                    return +1;
+                else if(secondary == arg.secondary)
+                    return 0;
+            return -1;
+        }
+        public String toString() {
+            return Character.toString(primary) +
+                    secondary + ": " + item;
+        }
+    }
+    public void add(String td, char pri, int sec) {
+        super.add(new ToDoItem(td, pri, sec));
     }
 }

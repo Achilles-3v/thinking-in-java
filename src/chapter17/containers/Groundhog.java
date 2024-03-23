ce4bd9ef5f69d00654fe1ab5dd018843fd44099a
Achilles-3v <sh17vv@gmail.com>
2024-03-23 22:15:30 +0500

    chapter17 added class Groundhog

---
+++ src/chapter17/containers/Groundhog.java
@@ -0,0 +1,9 @@
+package chapter17.containers;
+
+public class Groundhog {
+    protected int number;
+    public Groundhog(int n) { number = n; }
+    public String toString() {
+        return "Groundhog #" + number;
+    }
+}

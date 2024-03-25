154b6c144b7f2701ffc25a9d9b1001ee1c0a0ec2
Achilles-3v <sh17vv@gmail.com>
2024-03-25 21:02:37 +0500

    chapter17 added class TestParam

---
+++ src/chapter17/containers/TestParam.java
@@ -0,0 +1,10 @@
+package chapter17.containers;
+
+public class TestParam {
+    public final int size;
+    public final int loops;
+    public TestParam(int size, int loops) {
+        this.size = size;
+        this.loops = loops;
+    }
+}

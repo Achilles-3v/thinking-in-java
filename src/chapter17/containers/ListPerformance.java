cf86e47bfd5be74c62b927fa3c9a70048f1ae8c2
Achilles-3v <sh17vv@gmail.com>
2024-03-26 21:48:28 +0500

    ListPerformance added class ListTester

--- src/chapter17/containers/ListPerformance.java
+++ src/chapter17/containers/ListPerformance.java
@@ -125,4 +125,22 @@ public class ListPerformance {
             }
         });
     }
+
+    static class ListTester extends Tester<List<Integer>> {
+        public ListTester(List<Integer> container,
+                          List<Test<List<Integer>>> tests) {
+            super(container, tests);
+        }
+        // Fill to the appropriate size before each test:
+        @Override protected List<Integer> initialize(int size){
+            container.clear();
+            container.addAll(new CountingIntegerList(size));
+            return container;
+        }
+        // Convenience method:
+        public static void run(List<Integer> list,
+                               List<Test<List<Integer>>> tests) {
+            new ListTester(list, tests).timedTest();
+        }
+    }
 }

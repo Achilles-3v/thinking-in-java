fc887a1a640b66cc69fe2a6e66c9a56cf667ed61
Achilles-3v <sh17vv@gmail.com>
2024-03-28 21:25:55 +0500

    chapter17 added class References

---
+++ src/chapter17/containers/References.java
@@ -0,0 +1,18 @@
+package chapter17.containers;
+
+import java.lang.ref.*;
+import java.util.*;
+
+class VeryBig {
+    private static final int SIZE = 10000;
+    private long[] la = new long[SIZE];
+    private String ident;
+    public VeryBig(String id) { ident = id; }
+    public String toString() { return ident; }
+    protected void finalize() {
+        System.out.println("Finalizing " + ident);
+    }
+}
+
+public class References {
+}

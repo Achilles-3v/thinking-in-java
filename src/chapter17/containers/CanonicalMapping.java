56f9eef92028cec3966ce3628d6f8a7a698efe35
Achilles-3v <sh17vv@gmail.com>
2024-03-28 21:32:22 +0500

    CanonicalMapping added method main

--- src/chapter17/containers/CanonicalMapping.java
+++ src/chapter17/containers/CanonicalMapping.java
@@ -26,4 +26,21 @@ class Value extends Element {
 }
 
 public class CanonicalMapping {
+    public static void main(String[] args) {
+        int size = 1000;
+        // Or, choose size via the command line:
+        if(args.length > 0)
+            size = new Integer(args[0]);
+        Key[] keys = new Key[size];
+        WeakHashMap<Key,Value> map =
+                new WeakHashMap<Key,Value>();
+        for(int i = 0; i < size; i++) {
+            Key k = new Key(Integer.toString(i));
+            Value v = new Value(Integer.toString(i));
+            if(i % 3 == 0)
+                keys[i] = k; // Save as "real" references
+            map.put(k, v);
+        }
+        System.gc();
+    }
 }

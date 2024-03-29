8a458f222cd6770e622d058b8d7232a242059341
Achilles-3v <sh17vv@gmail.com>
2024-03-29 22:09:58 +0500

    chapter17 added class Stacks

---
+++ src/chapter17/containers/Stacks.java
@@ -0,0 +1,9 @@
+package chapter17.containers;
+
+import java.util.*;
+
+enum Month { JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE,
+    JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER }
+
+public class Stacks {
+}

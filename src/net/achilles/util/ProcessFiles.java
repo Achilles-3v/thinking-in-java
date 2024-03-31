58631fa5cc8801485a664140037e786f3dbf8318
Achilles-3v <sh17vv@gmail.com>
2024-03-31 20:43:31 +0500

    net.achilles.util added class ProcessFiles

---
+++ src/net/achilles/util/ProcessFiles.java
@@ -0,0 +1,15 @@
+package net.achilles.util;
+
+import java.io.*;
+
+public class ProcessFiles {
+    public interface Strategy {
+        void process(File file);
+    }
+    private Strategy strategy;
+    private String ext;
+    public ProcessFiles(Strategy strategy, String ext) {
+        this.strategy = strategy;
+        this.ext = ext;
+    }
+}

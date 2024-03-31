d70751a80cfe7a235e2330f01dcb19541148721c
Achilles-3v <sh17vv@gmail.com>
2024-03-31 20:49:05 +0500

    MakeDirectories added method fileData

--- src/chapter18/io/MakeDirectories.java
+++ src/chapter18/io/MakeDirectories.java
@@ -13,4 +13,19 @@ public class MakeDirectories {
                         "Renames from path1 to path2");
         System.exit(1);
     }
+    private static void fileData(File f) {
+        System.out.println(
+                "Absolute path: " + f.getAbsolutePath() +
+                        "\n Can read: " + f.canRead() +
+                        "\n Can write: " + f.canWrite() +
+                        "\n getName: " + f.getName() +
+                        "\n getParent: " + f.getParent() +
+                        "\n getPath: " + f.getPath() +
+                        "\n length: " + f.length() +
+                        "\n lastModified: " + f.lastModified());
+        if(f.isFile())
+            System.out.println("It's a file");
+        else if(f.isDirectory())
+            System.out.println("It's a directory");
+    }
 }

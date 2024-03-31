bb71adc9dc0f0c311fdbf6134425eeb0246d84a9
Achilles-3v <sh17vv@gmail.com>
2024-03-31 20:44:36 +0500

    ProcessFiles added method start

--- src/net/achilles/util/ProcessFiles.java
+++ src/net/achilles/util/ProcessFiles.java
@@ -12,4 +12,31 @@ public class ProcessFiles {
         this.strategy = strategy;
         this.ext = ext;
     }
+    public void start(String[] args) {
+        try {
+            if(args.length == 0)
+                processDirectoryTree(new File("."));
+            else
+                for(String arg : args) {
+                    File fileArg = new File(arg);
+                    if(fileArg.isDirectory())
+                        processDirectoryTree(fileArg);
+                    else {
+                        // Allow user to leave off extension:
+                        if(!arg.endsWith("." + ext))
+                            arg += "." + ext;
+                        strategy.process(
+                                new File(arg).getCanonicalFile());
+                    }
+                }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void
+    processDirectoryTree(File root) throws IOException {
+        for(File file : Directory.walk(
+                root.getAbsolutePath(), ".*\\." + ext))
+            strategy.process(file.getCanonicalFile());
+    }
 }

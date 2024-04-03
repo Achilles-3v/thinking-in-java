3a2519ade0082f0d0d5391ca233dd0e3a669fb8b
Achilles-3v <sh17vv@gmail.com>
2024-04-03 21:10:27 +0500

    chapter18 added class OSExecuteDemo

---
+++ src/chapter18/io/OSExecuteDemo.java
@@ -0,0 +1,9 @@
+package chapter18.io;
+
+import net.achilles.util.*;
+
+public class OSExecuteDemo {
+    public static void main(String[] args) {
+        OSExecute.command("javap OSExecuteDemo");
+    }
+}

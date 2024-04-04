b9af946e16dad1117b3e5b9e635910999aa976af
Achilles-3v <sh17vv@gmail.com>
2024-04-04 21:29:28 +0500

    chapter18 added class BufferToText

---
+++ src/chapter18/io/BufferToText.java
@@ -0,0 +1,10 @@
+package chapter18.io;
+
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.charset.*;
+import java.io.*;
+
+public class BufferToText {
+    private static final int BSIZE = 1024;
+}

86d5a00a13cb819970146524cf2e24c2dd56736d
Achilles-3v <sh17vv@gmail.com>
2024-04-05 22:36:16 +0500

    UsingBuffers added method main

--- src/chapter18/io/UsingBuffers.java
+++ src/chapter18/io/UsingBuffers.java
@@ -12,4 +12,15 @@ public class UsingBuffers {
             buffer.put(c2).put(c1);
         }
     }
+    public static void main(String[] args) {
+        char[] data = "UsingBuffers".toCharArray();
+        ByteBuffer bb = ByteBuffer.allocate(data.length * 2);
+        CharBuffer cb = bb.asCharBuffer();
+        cb.put(data);
+        System.out.println(cb.rewind());
+        symmetricScramble(cb);
+        System.out.println(cb.rewind());
+        symmetricScramble(cb);
+        System.out.println(cb.rewind());
+    }
 }

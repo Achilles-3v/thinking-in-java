9a7d223e6280d30f8310d245996d96ea6ee52c07
Achilles-3v <sh17vv@gmail.com>
2024-04-06 22:18:05 +0500

    LockingMappedFiles added class LockAndModify

--- src/chapter18/io/LockingMappedFiles.java
+++ src/chapter18/io/LockingMappedFiles.java
@@ -7,4 +7,31 @@ import java.io.*;
 public class LockingMappedFiles {
     static final int LENGTH = 0x8FFFFFF; // 128 MB
     static FileChannel fc;
+
+    private static class LockAndModify extends Thread {
+        private ByteBuffer buff;
+        private int start, end;
+        LockAndModify(ByteBuffer mbb, int start, int end) {
+            this.start = start;
+            this.end = end;
+            mbb.limit(end);
+            mbb.position(start);
+            buff = mbb.slice();
+            start();
+        }
+        public void run() {
+            try {
+                // Exclusive lock with no overlap:
+                FileLock fl = fc.lock(start, end, false);
+                System.out.println("Locked: "+ start +" to "+ end);
+                // Perform modification:
+                while(buff.position() < buff.limit() - 1)
+                    buff.put((byte)(buff.get() + 1));
+                fl.release();
+                System.out.println("Released: "+start+" to "+ end);
+            } catch(IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }

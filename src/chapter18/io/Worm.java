3b20e5724bee8129d011a296b9036763045669e6
Achilles-3v <sh17vv@gmail.com>
2024-04-07 19:17:36 +0500

    class Worm update

--- src/chapter18/io/Worm.java
+++ src/chapter18/io/Worm.java
@@ -9,5 +9,23 @@ class Data implements Serializable {
     public String toString() { return Integer.toString(n); }
 }
 
-public class Worm {
+public class Worm implements Serializable {
+    private static Random rand = new Random(47);
+    private Data[] d = {
+            new Data(rand.nextInt(10)),
+            new Data(rand.nextInt(10)),
+            new Data(rand.nextInt(10))
+    };
+    private Worm next;
+    private char c;
+    // Value of i == number of segments
+    public Worm(int i, char x) {
+        System.out.println("Worm constructor: " + i);
+        c = x;
+        if(--i > 0)
+            next = new Worm(i, (char)(x + 1));
+    }
+    public Worm() {
+        System.out.println("Default constructor");
+    }
 }

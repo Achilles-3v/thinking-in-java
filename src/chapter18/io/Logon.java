aff4ee60a4650593cb8121383f9df5f04198f002
Achilles-3v <sh17vv@gmail.com>
2024-04-08 21:11:26 +0500

    Logon added method main

--- src/chapter18/io/Logon.java
+++ src/chapter18/io/Logon.java
@@ -16,4 +16,18 @@ public class Logon implements Serializable {
         return "logon info: \n   username: " + username +
                 "\n   date: " + date + "\n   password: " + password;
     }
+    public static void main(String[] args) throws Exception {
+        Logon a = new Logon("Hulk", "myLittlePony");
+        System.out.println("logon a = " + a);
+        ObjectOutputStream o = new ObjectOutputStream(
+                new FileOutputStream("Logon.out"));
+        o.writeObject(a);
+        o.close();
+        TimeUnit.SECONDS.sleep(1);
+        ObjectInputStream in = new ObjectInputStream(
+                new FileInputStream("Logon.out"));
+        System.out.println("Recovering object at " + new Date());
+        a = (Logon)in.readObject();
+        System.out.println("logon a = " + a);
+    }
 }

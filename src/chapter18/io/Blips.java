d7e52cebef905e8948b59d7bbdc180a9d4ccf254
Achilles-3v <sh17vv@gmail.com>
2024-04-08 21:02:56 +0500

    chapter18 added class Blips

---
+++ src/chapter18/io/Blips.java
@@ -0,0 +1,20 @@
+package chapter18.io;
+
+import java.io.*;
+
+class Blip1 implements Externalizable {
+    public Blip1() {
+        System.out.println("Blip1 Constructor");
+    }
+    public void writeExternal(ObjectOutput out)
+            throws IOException {
+        System.out.println("Blip1.writeExternal");
+    }
+    public void readExternal(ObjectInput in)
+            throws IOException, ClassNotFoundException {
+        System.out.println("Blip1.readExternal");
+    }
+}
+
+public class Blips {
+}

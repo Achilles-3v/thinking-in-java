926012d72e8b02496259b77ee48dee0e50b3a4dc
Achilles-3v <sh17vv@gmail.com>
2024-04-11 20:06:17 +0500

    SpaceShip added method main

--- src/chapter19/enumerated/SpaceShip.java
+++ src/chapter19/enumerated/SpaceShip.java
@@ -7,4 +7,9 @@ public enum SpaceShip {
         String lower = id.substring(1).toLowerCase();
         return id.charAt(0) + lower;
     }
+    public static void main(String[] args) {
+        for(SpaceShip s : values()) {
+            System.out.println(s);
+        }
+    }
 }

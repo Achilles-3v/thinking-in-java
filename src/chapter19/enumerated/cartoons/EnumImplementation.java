918a8a08496ac9063c2ffdfec4ed89eccea01a41
Achilles-3v <sh17vv@gmail.com>
2024-04-12 20:32:53 +0500

    EnumImplementation added method main

--- src/chapter19/enumerated/cartoons/EnumImplementation.java
+++ src/chapter19/enumerated/cartoons/EnumImplementation.java
@@ -16,4 +16,9 @@ public class EnumImplementation {
     public static <T> void printNext(Generator<T> rg) {
         System.out.print(rg.next() + ", ");
     }
+    public static void main(String[] args) {
+        CartoonCharacter cc = CartoonCharacter.BOB;
+        for(int i = 0; i < 10; i++)
+            printNext(cc);
+    }
 }

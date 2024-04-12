7da987bf9abd3624661553c69cc781b2a96b8587
Achilles-3v <sh17vv@gmail.com>
2024-04-12 20:36:46 +0500

    RandomTest added method main

--- src/chapter19/enumerated/RandomTest.java
+++ src/chapter19/enumerated/RandomTest.java
@@ -6,4 +6,8 @@ enum Activity { SITTING, LYING, STANDING, HOPPING,
     RUNNING, DODGING, JUMPING, FALLING, FLYING }
 
 public class RandomTest {
+    public static void main(String[] args) {
+        for(int i = 0; i < 20; i++)
+            System.out.print(Enums.random(Activity.class) + " ");
+    }
 }

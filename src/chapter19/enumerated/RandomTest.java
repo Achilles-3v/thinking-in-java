6b91dff0ec5012c9745bc3e7fb5ceb9562d0ff80
Achilles-3v <sh17vv@gmail.com>
2024-04-12 20:36:12 +0500

    chapter19 added class RandomTest

---
+++ src/chapter19/enumerated/RandomTest.java
@@ -0,0 +1,9 @@
+package chapter19.enumerated;
+
+import net.achilles.util.*;
+
+enum Activity { SITTING, LYING, STANDING, HOPPING,
+    RUNNING, DODGING, JUMPING, FALLING, FLYING }
+
+public class RandomTest {
+}

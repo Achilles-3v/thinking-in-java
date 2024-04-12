6562a0e7027eb6bd61170bc460e5e21a3ef0be67
Achilles-3v <sh17vv@gmail.com>
2024-04-12 20:32:02 +0500

    chapter19 added class EnumImplementation

---
+++ src/chapter19/enumerated/cartoons/EnumImplementation.java
@@ -0,0 +1,19 @@
+package chapter19.enumerated.cartoons;
+
+import java.util.*;
+import net.achilles.util.*;
+
+enum CartoonCharacter
+        implements Generator<CartoonCharacter> {
+    SLAPPY, SPANKY, PUNCHY, SILLY, BOUNCY, NUTTY, BOB;
+    private Random rand = new Random(47);
+    public CartoonCharacter next() {
+        return values()[rand.nextInt(values().length)];
+    }
+}
+
+public class EnumImplementation {
+    public static <T> void printNext(Generator<T> rg) {
+        System.out.print(rg.next() + ", ");
+    }
+}

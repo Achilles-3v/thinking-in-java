83fee7b2fbbc17a3b2786f02d8ce577494051464
Achilles-3v <sh17vv@gmail.com>
2024-04-13 20:30:18 +0500

    Meal2 added interface Food:

--- src/chapter19/enumerated/menu/Meal2.java
+++ src/chapter19/enumerated/menu/Meal2.java
@@ -11,4 +11,24 @@ public enum Meal2 {
     private Meal2(Class<? extends Food> kind) {
         values = kind.getEnumConstants();
     }
+    public interface Food {
+        enum Appetizer implements Food {
+            SALAD, SOUP, SPRING_ROLLS;
+        }
+        enum MainCourse implements Food {
+            LASAGNE, BURRITO, PAD_THAI,
+            LENTILS, HUMMOUS, VINDALOO;
+        }
+        enum Dessert implements Food {
+            TIRAMISU, GELATO, BLACK_FOREST_CAKE,
+            FRUIT, CREME_CARAMEL;
+        }
+        enum Coffee implements Food {
+            BLACK_COFFEE, DECAF_COFFEE, ESPRESSO,
+            LATTE, CAPPUCCINO, TEA, HERB_TEA;
+        }
+    }
+    public Food randomSelection() {
+        return Enums.random(values);
+    }
 }

c0c31fea54a201ac1a20da5a9f9d2e315c2d22ec
Achilles-3v <sh17vv@gmail.com>
2024-04-13 20:18:17 +0500

    chapter19 added class SecurityCategory

---
+++ src/chapter19/enumerated/SecurityCategory.java
@@ -0,0 +1,18 @@
+package chapter19.enumerated;
+
+import net.achilles.util.*;
+
+enum SecurityCategory {
+    STOCK(Security.Stock.class), BOND(Security.Bond.class);
+    Security[] values;
+    SecurityCategory(Class<? extends Security> kind) {
+        values = kind.getEnumConstants();
+    }
+    interface Security {
+        enum Stock implements Security { SHORT, LONG, MARGIN }
+        enum Bond implements Security { MUNICIPAL, JUNK }
+    }
+    public Security randomSelection() {
+        return Enums.random(values);
+    }
+}

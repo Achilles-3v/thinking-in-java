bfeb2dba743f75fbc2d47fba65196f8b1ee1a9dc
Achilles-3v <sh17vv@gmail.com>
2024-04-13 20:08:56 +0500

    chapter19.menu added class TypeOfFood

---
+++ src/chapter19/enumerated/menu/TypeOfFood.java
@@ -0,0 +1,12 @@
+package chapter19.enumerated.menu;
+
+import static chapter19.enumerated.menu.Food.*;
+
+public class TypeOfFood {
+    public static void main(String[] args) {
+        Food food = Appetizer.SALAD;
+        food = MainCourse.LASAGNE;
+        food = Dessert.GELATO;
+        food = Coffee.CAPPUCCINO;
+    }
+}

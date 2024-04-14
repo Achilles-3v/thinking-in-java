2732e014668b90bc31ed61ad78c72934ad618f3b
Achilles-3v <sh17vv@gmail.com>
2024-04-14 21:20:55 +0500

    BigEnumSet added method main

--- src/chapter19/enumerated/BigEnumSet.java
+++ src/chapter19/enumerated/BigEnumSet.java
@@ -10,4 +10,8 @@ public class BigEnumSet {
         A44, A45, A46, A47, A48, A49, A50, A51, A52, A53, A54,
         A55, A56, A57, A58, A59, A60, A61, A62, A63, A64, A65,
         A66, A67, A68, A69, A70, A71, A72, A73, A74, A75 }
+    public static void main(String[] args) {
+        EnumSet<Big> bigEnumSet = EnumSet.allOf(Big.class);
+        System.out.println(bigEnumSet);
+    }
 }

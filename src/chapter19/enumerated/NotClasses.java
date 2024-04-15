6373952b9ee4ccf62f623d65f071ab02b65069af
Achilles-3v <sh17vv@gmail.com>
2024-04-15 19:57:31 +0500

    chapter19 added class NotClasses

---
+++ src/chapter19/enumerated/NotClasses.java
@@ -0,0 +1,15 @@
+package chapter19.enumerated;
+
+enum LikeClasses {
+    WINKEN { void behavior() {
+        System.out.println("Behavior1"); } },
+    BLINKEN { void behavior() {
+        System.out.println("Behavior2"); } },
+    NOD { void behavior() {
+        System.out.println("Behavior3"); } };
+    abstract void behavior();
+}
+
+public class NotClasses {
+    // void f1(LikeClasses.WINKEN instance) {} // Nope
+}

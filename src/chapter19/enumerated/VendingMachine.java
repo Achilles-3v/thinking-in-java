789c9d0e865edc8474eea6224d85f5407c2a0b1d
Achilles-3v <sh17vv@gmail.com>
2024-04-16 22:30:41 +0500

    VendingMachine added classes RandomInputGenerator and FileInputGenerator

--- src/chapter19/enumerated/VendingMachine.java
+++ src/chapter19/enumerated/VendingMachine.java
@@ -24,5 +24,20 @@ enum Category {
 }
 
 public class VendingMachine {
+}
 
+class RandomInputGenerator implements Generator<Input> {
+    public Input next() { return Input.randomSelection(); }
+}
+
+class FileInputGenerator implements Generator<Input> {
+    private Iterator<String> input;
+    public FileInputGenerator(String fileName) {
+        input = new TextFile(fileName, ";").iterator();
+    }
+    public Input next() {
+        if(!input.hasNext())
+            return null;
+        return Enum.valueOf(Input.class, input.next().trim());
+    }
 }

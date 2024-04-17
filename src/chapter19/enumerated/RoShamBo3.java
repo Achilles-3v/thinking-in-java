c50c297ecc6304d52d07fab6ba5045e18866ef80
Achilles-3v <sh17vv@gmail.com>
2024-04-17 22:49:56 +0500

    RoShamBo3 added method main

--- src/chapter19/enumerated/RoShamBo3.java
+++ src/chapter19/enumerated/RoShamBo3.java
@@ -34,4 +34,7 @@ public enum RoShamBo3 implements Competitor<RoShamBo3> {
         }
     };
     public abstract Outcome compete(RoShamBo3 it);
+    public static void main(String[] args) {
+        RoShamBo.play(RoShamBo3.class, 20);
+    }
 }

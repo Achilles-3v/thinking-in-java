8e024e4663c0d120f81fa49104da41df490225d8
Achilles-3v <sh17vv@gmail.com>
2024-04-19 22:15:01 +0500

    database added interface SQLInteger

---
+++ src/chapter20/annotations/database/SQLInteger.java
@@ -0,0 +1,10 @@
+package chapter20.annotations.database;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SQLInteger {
+    String name() default "";
+    Constraints constraints() default @Constraints;
+}

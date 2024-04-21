5d79494379c5487bfbfb1c6916442730902a89ff
Achilles-3v <sh17vv@gmail.com>
2024-04-21 22:46:19 +0500

    chapter20 added class ExtractInterface

---
+++ src/chapter20/annotations/ExtractInterface.java
@@ -0,0 +1,9 @@
+package chapter20.annotations;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ExtractInterface {
+    public String value();
+}

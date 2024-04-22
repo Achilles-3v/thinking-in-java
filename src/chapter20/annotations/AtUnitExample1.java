cea8235d153313dc0f0a2f0a8295f5bb796309ba
Achilles-3v <sh17vv@gmail.com>
2024-04-22 23:02:46 +0500

    chapter20 added class AtUnitExample1

---
+++ src/chapter20/annotations/AtUnitExample1.java
@@ -0,0 +1,21 @@
+package chapter20.annotations;
+
+import net.achilles.atunit.*;
+import net.achilles.util.*;
+
+public class AtUnitExample1 {
+    public String methodOne() {
+        return "This is methodOne";
+    }
+    public int methodTwo() {
+        System.out.println("This is methodTwo");
+        return 2;
+    }
+    @Test boolean methodOneTest() {
+        return methodOne().equals("This is methodOne");
+    }
+    @Test boolean m2() { return methodTwo() == 2; }
+    @Test private boolean m3() { return true; }
+    @Test boolean failureTest() { return false; }
+    @Test boolean anotherDisappointment() { return false; }
+}

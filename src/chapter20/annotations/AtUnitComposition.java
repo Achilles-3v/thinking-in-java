bd07fb18d576897fa97a27102fcf0a4844889b91
Achilles-3v <sh17vv@gmail.com>
2024-04-23 21:33:50 +0500

    chapter20 added class AtUnitComposition

---
+++ src/chapter20/annotations/AtUnitComposition.java
@@ -0,0 +1,15 @@
+package chapter20.annotations;
+
+import net.achilles.atunit.*;
+import net.achilles.util.*;
+
+public class AtUnitComposition {
+    AtUnitExample1 testObject = new AtUnitExample1();
+    @Test boolean _methodOne() {
+        return
+                testObject.methodOne().equals("This is methodOne");
+    }
+    @Test boolean _methodTwo() {
+        return testObject.methodTwo() == 2;
+    }
+}

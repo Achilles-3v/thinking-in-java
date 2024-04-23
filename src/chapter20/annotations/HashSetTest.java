e9688ba7ce9f81e7dd133bb4e7b4a1cde153aba0
Achilles-3v <sh17vv@gmail.com>
2024-04-23 21:41:24 +0500

    chapter20 added class HashSetTest

---
+++ src/chapter20/annotations/HashSetTest.java
@@ -0,0 +1,21 @@
+package chapter20.annotations;
+
+import java.util.*;
+import net.achilles.atunit.*;
+import net.achilles.util.*;
+
+public class HashSetTest {
+    HashSet<String> testObject = new HashSet<String>();
+    @Test void initialization() {
+        assert testObject.isEmpty();
+    }
+    @Test void _contains() {
+        testObject.add("one");
+        assert testObject.contains("one");
+    }
+    @Test void _remove() {
+        testObject.add("one");
+        testObject.remove("one");
+        assert testObject.isEmpty();
+    }
+}

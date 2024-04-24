7edcec0aa09883f3109bf2e475122b0c2abcdb64
Achilles-3v <sh17vv@gmail.com>
2024-04-24 22:20:33 +0500

    chapter20 added class StackLStringTest

---
+++ src/chapter20/annotations/StackLStringTest.java
@@ -0,0 +1,25 @@
+package chapter20.annotations;
+
+import net.achilles.atunit.*;
+import net.achilles.util.*;
+
+public class StackLStringTest extends StackL<String> {
+    @Test void _push() {
+        push("one");
+        assert top().equals("one");
+        push("two");
+        assert top().equals("two");
+    }
+    @Test void _pop() {
+        push("one");
+        push("two");
+        assert pop().equals("two");
+        assert pop().equals("one");
+    }
+    @Test void _top() {
+        push("A");
+        push("B");
+        assert top().equals("B");
+        assert top().equals("B");
+    }
+}

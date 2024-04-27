faff24ae2b32217e07af9d7547ac49f8244b2f8e
Achilles-3v <sh17vv@gmail.com>
2024-04-27 20:50:18 +0500

    SleepingTask added method main

--- src/chapter21/concurrency/SleepingTask.java
+++ src/chapter21/concurrency/SleepingTask.java
@@ -13,4 +13,10 @@ public class SleepingTask extends LiftOff {
             System.err.println("Interrupted");
         }
     }
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0; i < 5; i++)
+            exec.execute(new SleepingTask());
+        exec.shutdown();
+    }
 }

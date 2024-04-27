90856fefe6ad7d50d0bd81ad29fa74e76669660b
Achilles-3v <sh17vv@gmail.com>
2024-04-27 20:54:29 +0500

    chapter21 added class SimpleDaemons

---
+++ src/chapter21/concurrency/SimpleDaemons.java
@@ -0,0 +1,16 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+
+public class SimpleDaemons implements Runnable {
+    public void run() {
+        try {
+            while(true) {
+                TimeUnit.MILLISECONDS.sleep(100);
+                System.out.println(Thread.currentThread() + " " + this);
+            }
+        } catch(InterruptedException e) {
+            System.out.println("sleep() interrupted");
+        }
+    }
+}

8936242c926c840cb8b00efe837bf7c86e130273
Achilles-3v <sh17vv@gmail.com>
2024-04-28 22:35:44 +0500

    DaemonFromFactory added method main

--- src/chapter21/concurrency/DaemonFromFactory.java
+++ src/chapter21/concurrency/DaemonFromFactory.java
@@ -14,4 +14,12 @@ public class DaemonFromFactory implements Runnable {
             System.out.println("Interrupted");
         }
     }
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool(
+                new DaemonThreadFactory());
+        for(int i = 0; i < 10; i++)
+            exec.execute(new DaemonFromFactory());
+        System.out.println("All daemons started");
+        TimeUnit.MILLISECONDS.sleep(500);
+    }
 }

061f1fcf2dd97c083944c47e13cda9260681fd33
Achilles-3v <sh17vv@gmail.com>
2024-05-01 22:49:41 +0500

    chapter21 added class SimpleThread

---
+++ src/chapter21/concurrency/SimpleThread.java
@@ -0,0 +1,20 @@
+package chapter21.concurrency;
+
+public class SimpleThread extends Thread {
+    private int countDown = 5;
+    private static int threadCount = 0;
+    public SimpleThread() {
+        super(Integer.toString(++threadCount));
+        start();
+    }
+    public String toString() {
+        return "#" + getName() + "(" + countDown + "), ";
+    }
+    public void run() {
+        while(true) {
+            System.out.print(this);
+            if(--countDown == 0)
+                return;
+        }
+    }
+}

90c2727ae94bdc9f9a52ccb121998cec1792a98c
Achilles-3v <sh17vv@gmail.com>
2024-05-01 22:50:24 +0500

    SimpleThread added method mian

--- src/chapter21/concurrency/SimpleThread.java
+++ src/chapter21/concurrency/SimpleThread.java
@@ -17,4 +17,8 @@ public class SimpleThread extends Thread {
                 return;
         }
     }
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++)
+            new SimpleThread();
+    }
 }

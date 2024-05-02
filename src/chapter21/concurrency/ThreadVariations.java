5cdd66d561cd9e4fcd90caa47b158c61f028eeb5
Achilles-3v <sh17vv@gmail.com>
2024-05-02 21:24:49 +0500

    ThreadVariations addeds classes InnerThread2 and InnerRunnable2

--- src/chapter21/concurrency/ThreadVariations.java
+++ src/chapter21/concurrency/ThreadVariations.java
@@ -30,6 +30,30 @@ class InnerThread1 {
     }
 }
 
+class InnerThread2 {
+    private int countDown = 5;
+    private Thread t;
+    public InnerThread2(String name) {
+        t = new Thread(name) {
+            public void run() {
+                try {
+                    while(true) {
+                        System.out.println(this);
+                        if(--countDown == 0) return;
+                        sleep(10);
+                    }
+                } catch(InterruptedException e) {
+                    System.out.println("sleep() interrupted");
+                }
+            }
+            public String toString() {
+                return getName() + ": " + countDown;
+            }
+        };
+        t.start();
+    }
+}
+
 class InnerRunnable1 {
     private int countDown = 5;
     private Inner inner;
@@ -56,6 +80,31 @@ class InnerRunnable1 {
     }
     public InnerRunnable1(String name) {
         inner = new Inner(name);
+    }
+}
+
+class InnerRunnable2 {
+    private int countDown = 5;
+    private Thread t;
+    public InnerRunnable2(String name) {
+        t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    while(true) {
+                        System.out.println(this);
+                        if(--countDown == 0) return;
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    }
+                } catch(InterruptedException e) {
+                    System.out.println("sleep() interrupted");
+                }
+            }
+            public String toString() {
+                return Thread.currentThread().getName() +
+                        ": " + countDown;
+            }
+        }, name);
+        t.start();
     }
 }
 

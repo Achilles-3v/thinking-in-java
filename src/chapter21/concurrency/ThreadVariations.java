61817320f6fef9cfa8f84509e72471c4929725c7
Achilles-3v <sh17vv@gmail.com>
2024-05-02 21:25:41 +0500

    ThreadVariations added class ThreadMethod

--- src/chapter21/concurrency/ThreadVariations.java
+++ src/chapter21/concurrency/ThreadVariations.java
@@ -108,5 +108,33 @@ class InnerRunnable2 {
     }
 }
 
+class ThreadMethod {
+    private int countDown = 5;
+    private Thread t;
+    private String name;
+    public ThreadMethod(String name) { this.name = name; }
+    public void runTask() {
+        if(t == null) {
+            t = new Thread(name) {
+                public void run() {
+                    try {
+                        while(true) {
+                            System.out.println(this);
+                            if(--countDown == 0) return;
+                            sleep(10);
+                        }
+                    } catch(InterruptedException e) {
+                        System.out.println("sleep() interrupted");
+                    }
+                }
+                public String toString() {
+                    return getName() + ": " + countDown;
+                }
+            };
+            t.start();
+        }
+    }
+}
+
 public class ThreadVariations {
 }

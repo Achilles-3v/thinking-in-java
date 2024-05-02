410d991d15baaf97cc4c8e12c2ba94abf81513a1
Achilles-3v <sh17vv@gmail.com>
2024-05-02 21:28:20 +0500

    chapter21 added class Joining

---
+++ src/chapter21/concurrency/Joining.java
@@ -0,0 +1,40 @@
+package chapter21.concurrency;
+
+class Sleeper extends Thread {
+    private int duration;
+    public Sleeper(String name, int sleepTime) {
+        super(name);
+        duration = sleepTime;
+        start();
+    }
+    public void run() {
+        try {
+            sleep(duration);
+        } catch(InterruptedException e) {
+            System.out.println(getName() + " was interrupted. " +
+                    "isInterrupted(): " + isInterrupted());
+            return;
+        }
+        System.out.println(getName() + " has awakened");
+    }
+}
+
+class Joiner extends Thread {
+    private Sleeper sleeper;
+    public Joiner(String name, Sleeper sleeper) {
+        super(name);
+        this.sleeper = sleeper;
+        start();
+    }
+    public void run() {
+        try {
+            sleeper.join();
+        } catch(InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println(getName() + " join completed");
+    }
+}
+
+public class Joining {
+}

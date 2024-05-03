88dd9e886e4a6b252c6b878f9335f0631626e9b4
Achilles-3v <sh17vv@gmail.com>
2024-05-03 20:09:08 +0500

    ExceptionThread added method main

--- src/chapter21/concurrency/ExceptionThread.java
+++ src/chapter21/concurrency/ExceptionThread.java
@@ -6,4 +6,8 @@ public class ExceptionThread implements Runnable {
     public void run() {
         throw new RuntimeException();
     }
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(new ExceptionThread());
+    }
 }

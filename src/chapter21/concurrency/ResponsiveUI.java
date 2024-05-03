85be6a43686a4f9ddb7f0b218469b1c75b7db783
Achilles-3v <sh17vv@gmail.com>
2024-05-03 20:04:30 +0500

    ResponiveUI added method run()

--- src/chapter21/concurrency/ResponsiveUI.java
+++ src/chapter21/concurrency/ResponsiveUI.java
@@ -10,4 +10,14 @@ class UnresponsiveUI {
 }
 
 public class ResponsiveUI extends Thread {
+    private static volatile double d = 1;
+    public ResponsiveUI() {
+        setDaemon(true);
+        start();
+    }
+    public void run() {
+        while(true) {
+            d = d + (Math.PI + Math.E) / d;
+        }
+    }
 }

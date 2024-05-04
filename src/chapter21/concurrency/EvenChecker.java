ca1edb86b5f5629e365611b4ebad79bae8efa94c
Achilles-3v <sh17vv@gmail.com>
2024-05-04 21:59:13 +0500

    EvenChecker added methods test()

--- src/chapter21/concurrency/EvenChecker.java
+++ src/chapter21/concurrency/EvenChecker.java
@@ -18,4 +18,14 @@ public class EvenChecker implements Runnable {
             }
         }
     }
+    public static void test(IntGenerator gp, int count) {
+        System.out.println("Press Control-C to exit");
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0; i < count; i++)
+            exec.execute(new EvenChecker(gp, i));
+        exec.shutdown();
+    }
+    public static void test(IntGenerator gp) {
+        test(gp, 10);
+    }
 }

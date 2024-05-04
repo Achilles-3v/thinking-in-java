33f2f5527178a3b4508c63c3058de3c133a01dd1
Achilles-3v <sh17vv@gmail.com>
2024-05-04 21:58:21 +0500

    chapter21 added class EvenCheker

---
+++ src/chapter21/concurrency/EvenChecker.java
@@ -0,0 +1,21 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+
+public class EvenChecker implements Runnable {
+    private IntGenerator generator;
+    private final int id;
+    public EvenChecker(IntGenerator g, int ident) {
+        generator = g;
+        id = ident;
+    }
+    public void run() {
+        while(!generator.isCanceled()) {
+            int val = generator.next();
+            if(val % 2 != 0) {
+                System.out.println(val + " not even!");
+                generator.cancel(); // Cancels all EvenCheckers
+            }
+        }
+    }
+}

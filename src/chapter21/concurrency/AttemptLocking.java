af4b435826e24f5b288678c8220cba504658b5c0
Achilles-3v <sh17vv@gmail.com>
2024-05-05 15:51:32 +0500

    chapter21 added class AttempLocking

---
+++ src/chapter21/concurrency/AttemptLocking.java
@@ -0,0 +1,17 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
+
+public class AttemptLocking {
+    private ReentrantLock lock = new ReentrantLock();
+    public void untimed() {
+        boolean captured = lock.tryLock();
+        try {
+            System.out.println("tryLock(): " + captured);
+        } finally {
+            if(captured)
+                lock.unlock();
+        }
+    }
+}

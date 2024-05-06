7e178fd3035397ac80b4d6fcbaa7262dc328d279
Achilles-3v <sh17vv@gmail.com>
2024-05-06 21:35:07 +0500

    SerialNumberChecker update

--- src/chapter21/concurrency/SerialNumberChecker.java
+++ src/chapter21/concurrency/SerialNumberChecker.java
@@ -24,4 +24,22 @@ class CircularSet {
 }
 
 public class SerialNumberChecker {
+    private static final int SIZE = 10;
+    private static CircularSet serials =
+            new CircularSet(1000);
+    private static ExecutorService exec =
+            Executors.newCachedThreadPool();
+    static class SerialChecker implements Runnable {
+        public void run() {
+            while(true) {
+                int serial =
+                        SerialNumberGenerator.nextSerialNumber();
+                if(serials.contains(serial)) {
+                    System.out.println("Duplicate: " + serial);
+                    System.exit(0);
+                }
+                serials.add(serial);
+            }
+        }
+    }
 }

5eefc7250e0c9954457240fa160dfee265e7f6a9
Achilles-3v <sh17vv@gmail.com>
2024-05-07 20:25:48 +0500

    CriticalSection added class PairManager

--- src/chapter21/concurrency/CriticalSection.java
+++ src/chapter21/concurrency/CriticalSection.java
@@ -30,5 +30,22 @@ class Pair {
     }
 }
 
+abstract class PairManager {
+    AtomicInteger checkCounter = new AtomicInteger(0);
+    protected Pair p = new Pair();
+    private List<Pair> storage =
+            Collections.synchronizedList(new ArrayList<Pair>());
+    public synchronized Pair getPair() {
+        return new Pair(p.getX(), p.getY());
+    }
+    protected void store(Pair p) {
+        storage.add(p);
+        try {
+            TimeUnit.MILLISECONDS.sleep(50);
+        } catch(InterruptedException ignore) {}
+    }
+    public abstract void increment();
+}
+
 public class CriticalSection {
 }

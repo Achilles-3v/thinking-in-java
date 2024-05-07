39b1e1f0cd452a01ea797ab42b38cf11ad3e43f8
Achilles-3v <sh17vv@gmail.com>
2024-05-07 20:29:16 +0500

    CriticalSection update

--- src/chapter21/concurrency/CriticalSection.java
+++ src/chapter21/concurrency/CriticalSection.java
@@ -96,4 +96,25 @@ class PairChecker implements Runnable {
 }
 
 public class CriticalSection {
+    static void
+    testApproaches(PairManager pman1, PairManager pman2) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        PairManipulator
+                pm1 = new PairManipulator(pman1),
+                pm2 = new PairManipulator(pman2);
+        PairChecker
+                pcheck1 = new PairChecker(pman1),
+                pcheck2 = new PairChecker(pman2);
+        exec.execute(pm1);
+        exec.execute(pm2);
+        exec.execute(pcheck1);
+        exec.execute(pcheck2);
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch(InterruptedException e) {
+            System.out.println("Sleep interrupted");
+        }
+        System.out.println("pm1: " + pm1 + "\npm2: " + pm2);
+        System.exit(0);
+    }
 }

9c65678567ff308e0d7599dbb32a70ce6414c0ac
Achilles-3v <sh17vv@gmail.com>
2024-05-08 21:09:04 +0500

    SyncObject added method main

--- src/chapter21/concurrency/SyncObject.java
+++ src/chapter21/concurrency/SyncObject.java
@@ -19,4 +19,13 @@ class DualSynch {
 }
 
 public class SyncObject {
+    public static void main(String[] args) {
+        final DualSynch ds = new DualSynch();
+        new Thread() {
+            public void run() {
+                ds.f();
+            }
+        }.start();
+        ds.g();
+    }
 }

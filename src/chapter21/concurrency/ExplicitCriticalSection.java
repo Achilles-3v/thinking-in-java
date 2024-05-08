98a34a1f9694327be04d3aaf60479dee0f719a09
Achilles-3v <sh17vv@gmail.com>
2024-05-08 21:03:48 +0500

    ExplicitCriticalSection added class ExplicitPairManager2

--- src/chapter21/concurrency/ExplicitCriticalSection.java
+++ src/chapter21/concurrency/ExplicitCriticalSection.java
@@ -16,5 +16,21 @@ class ExplicitPairManager1 extends PairManager {
     }
 }
 
+class ExplicitPairManager2 extends PairManager {
+    private Lock lock = new ReentrantLock();
+    public void increment() {
+        Pair temp;
+        lock.lock();
+        try {
+            p.incrementX();
+            p.incrementY();
+            temp = getPair();
+        } finally {
+            lock.unlock();
+        }
+        store(temp);
+    }
+}
+
 public class ExplicitCriticalSection {
 }

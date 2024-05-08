fcd42c0f5bd132369732f910b55bb3cc1321ed41
Achilles-3v <sh17vv@gmail.com>
2024-05-08 21:13:18 +0500

    chapter21 added class ThreadLocalVariableHolder

---
+++ src/chapter21/concurrency/ThreadLocalVariableHolder.java
@@ -0,0 +1,18 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+import java.util.*;
+
+public class ThreadLocalVariableHolder {
+    private static ThreadLocal<Integer> value =
+            new ThreadLocal<Integer>() {
+                private Random rand = new Random(47);
+                protected synchronized Integer initialValue() {
+                    return rand.nextInt(10000);
+                }
+            };
+    public static void increment() {
+        value.set(value.get() + 1);
+    }
+    public static int get() { return value.get(); }
+}

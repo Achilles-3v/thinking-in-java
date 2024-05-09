768d6d3bf8421e8a89fba85f4b3a422e8e1d811c
Achilles-3v <sh17vv@gmail.com>
2024-05-09 10:11:45 +0500

    Interrupting added class SynchronizedBlocked

--- src/chapter21/concurrency/Interrupting.java
+++ src/chapter21/concurrency/Interrupting.java
@@ -32,5 +32,24 @@ class IOBlocked implements Runnable {
     }
 }
 
+class SynchronizedBlocked implements Runnable {
+    public synchronized void f() {
+        while(true)
+            Thread.yield();
+    }
+    public SynchronizedBlocked() {
+        new Thread() {
+            public void run() {
+                f();
+            }
+        }.start();
+    }
+    public void run() {
+        System.out.println("Trying to call f()");
+        f();
+        System.out.println("Exiting SynchronizedBlocked.run()");
+    }
+}
+
 public class Interrupting {
 }

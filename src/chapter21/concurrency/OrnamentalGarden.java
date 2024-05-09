8bf143d996bee8cf0e2d05156675b02235239805
Achilles-3v <sh17vv@gmail.com>
2024-05-09 10:02:37 +0500

    OrnamentalGarden added class Entrance

--- src/chapter21/concurrency/OrnamentalGarden.java
+++ src/chapter21/concurrency/OrnamentalGarden.java
@@ -15,5 +15,46 @@ class Count {
     public synchronized int value() { return count; }
 }
 
+class Entrance implements Runnable {
+    private static Count count = new Count();
+    private static List<Entrance> entrances =
+            new ArrayList<Entrance>();
+    private int number = 0;
+    private final int id;
+    private static volatile boolean canceled = false;
+    public static void cancel() { canceled = true; }
+    public Entrance(int id) {
+        this.id = id;
+        entrances.add(this);
+    }
+    public void run() {
+        while(!canceled) {
+            synchronized(this) {
+                ++number;
+            }
+            System.out.println(this + " Total: " + count.increment());
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch(InterruptedException e) {
+                System.out.println("sleep interrupted");
+            }
+        }
+        System.out.println("Stopping " + this);
+    }
+    public synchronized int getValue() { return number; }
+    public String toString() {
+        return "Entrance " + id + ": " + getValue();
+    }
+    public static int getTotalCount() {
+        return count.value();
+    }
+    public static int sumEntrances() {
+        int sum = 0;
+        for(Entrance entrance : entrances)
+            sum += entrance.getValue();
+        return sum;
+    }
+}
+
 public class OrnamentalGarden {
 }

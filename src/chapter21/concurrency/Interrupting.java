eb2025a5f7408be6591ac28c27ca257b94d40942
Achilles-3v <sh17vv@gmail.com>
2024-05-09 10:10:27 +0500

    Interrupting added class IOBlocked

--- src/chapter21/concurrency/Interrupting.java
+++ src/chapter21/concurrency/Interrupting.java
@@ -14,5 +14,23 @@ class SleepBlocked implements Runnable {
     }
 }
 
+class IOBlocked implements Runnable {
+    private InputStream in;
+    public IOBlocked(InputStream is) { in = is; }
+    public void run() {
+        try {
+            System.out.println("Waiting for read():");
+            in.read();
+        } catch(IOException e) {
+            if(Thread.currentThread().isInterrupted()) {
+                System.out.println("Interrupted from blocked I/O");
+            } else {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Exiting IOBlocked.run()");
+    }
+}
+
 public class Interrupting {
 }

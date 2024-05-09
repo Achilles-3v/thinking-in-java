a4db6d581f37025c8f5803936a0a04b5d5b1d199
Achilles-3v <sh17vv@gmail.com>
2024-05-09 10:04:18 +0500

    OrnametalGarden added method main

--- src/chapter21/concurrency/OrnamentalGarden.java
+++ src/chapter21/concurrency/OrnamentalGarden.java
@@ -57,4 +57,16 @@ class Entrance implements Runnable {
 }
 
 public class OrnamentalGarden {
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0; i < 5; i++)
+            exec.execute(new Entrance(i));
+        TimeUnit.SECONDS.sleep(3);
+        Entrance.cancel();
+        exec.shutdown();
+        if(!exec.awaitTermination(250, TimeUnit.MILLISECONDS))
+            System.out.println("Some tasks were not terminated!");
+        System.out.println("Total: " + Entrance.getTotalCount());
+        System.out.println("Sum of Entrances: " + Entrance.sumEntrances());
+    }
 }

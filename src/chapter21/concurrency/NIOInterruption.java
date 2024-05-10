8642ef24f9688c38368764e4d11d1dbf4ccc1a89
Achilles-3v <sh17vv@gmail.com>
2024-05-10 17:15:36 +0500

    NIOInterraption added method main

--- src/chapter21/concurrency/NIOInterruption.java
+++ src/chapter21/concurrency/NIOInterruption.java
@@ -25,4 +25,19 @@ class NIOBlocked implements Runnable {
 }
 
 public class NIOInterruption {
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        ServerSocket server = new ServerSocket(8080);
+        InetSocketAddress isa =
+                new InetSocketAddress("localhost", 8080);
+        SocketChannel sc1 = SocketChannel.open(isa);
+        SocketChannel sc2 = SocketChannel.open(isa);
+        Future<?> f = exec.submit(new NIOBlocked(sc1));
+        exec.execute(new NIOBlocked(sc2));
+        exec.shutdown();
+        TimeUnit.SECONDS.sleep(1);
+        f.cancel(true);
+        TimeUnit.SECONDS.sleep(1);
+        sc2.close();
+    }
 }

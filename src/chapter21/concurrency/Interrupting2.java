e9e8ba377dbb90fc0c8a0a2fa86ef52d24f39697
Achilles-3v <sh17vv@gmail.com>
2024-05-10 17:21:14 +0500

    Interrupting added class Blocked2

--- src/chapter21/concurrency/Interrupting2.java
+++ src/chapter21/concurrency/Interrupting2.java
@@ -18,5 +18,14 @@ class BlockedMutex {
     }
 }
 
+class Blocked2 implements Runnable {
+    BlockedMutex blocked = new BlockedMutex();
+    public void run() {
+        System.out.println("Waiting for f() in BlockedMutex");
+        blocked.f();
+        System.out.println("Broken out of blocked call");
+    }
+}
+
 public class Interrupting2 {
 }

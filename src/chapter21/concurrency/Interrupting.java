f12666f9cba07d6220ebed4651f2af48df1cdd5f
Achilles-3v <sh17vv@gmail.com>
2024-05-10 17:09:35 +0500

    Interrupting added method main

--- src/chapter21/concurrency/Interrupting.java
+++ src/chapter21/concurrency/Interrupting.java
@@ -61,4 +61,12 @@ public class Interrupting {
         f.cancel(true);
         System.out.println("Interrupt sent to " + r.getClass().getName());
     }
+    public static void main(String[] args) throws Exception {
+        test(new SleepBlocked());
+        test(new IOBlocked(System.in));
+        test(new SynchronizedBlocked());
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("Aborting with System.exit(0)");
+        System.exit(0);
+    }
 }

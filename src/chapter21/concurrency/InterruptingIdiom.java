242e48051bd3c75b476b23c9177fbad8373ab57a
Achilles-3v <sh17vv@gmail.com>
2024-05-11 13:57:38 +0500

    Interrupting added mthod main

--- src/chapter21/concurrency/InterruptingIdiom.java
+++ src/chapter21/concurrency/InterruptingIdiom.java
@@ -43,4 +43,14 @@ class Blocked3 implements Runnable {
 }
 
 public class InterruptingIdiom {
+    public static void main(String[] args) throws Exception {
+        if(args.length != 1) {
+            System.out.println("usage: java InterruptingIdiom delay-in-mS");
+            System.exit(1);
+        }
+        Thread t = new Thread(new Blocked3());
+        t.start();
+        TimeUnit.MILLISECONDS.sleep(new Integer(args[0]));
+        t.interrupt();
+    }
 }

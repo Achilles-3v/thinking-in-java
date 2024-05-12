9e0941b7dd3cdd34b7fc4aec7e5b754484dabd25
Achilles-3v <sh17vv@gmail.com>
2024-05-12 21:25:35 +0500

    Restaurant update

--- src/chapter21/concurrency/Restaurant.java
+++ src/chapter21/concurrency/Restaurant.java
@@ -59,4 +59,12 @@ class Chef implements Runnable {
 }
 
 public class Restaurant {
+    Meal meal;
+    ExecutorService exec = Executors.newCachedThreadPool();
+    WaitPerson waitPerson = new WaitPerson(this);
+    Chef chef = new Chef(this);
+    public Restaurant() {
+        exec.execute(chef);
+        exec.execute(waitPerson);
+    }
 }

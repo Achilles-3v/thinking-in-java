242e0f77328ab9648e7aaf0c81949219a322c551
Achilles-3v <sh17vv@gmail.com>
2024-05-12 21:18:05 +0500

    NotifyVsNotifyAll added method main

--- src/chapter21/concurrency/NotifyVsNotifyAll.java
+++ src/chapter21/concurrency/NotifyVsNotifyAll.java
@@ -28,4 +28,34 @@ class Task2 implements Runnable {
 }
 
 public class NotifyVsNotifyAll {
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0; i < 5; i++)
+            exec.execute(new Task());
+        exec.execute(new Task2());
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            boolean prod = true;
+            public void run() {
+                if(prod) {
+                    System.out.print("\nnotify() ");
+                    Task.blocker.prod();
+                    prod = false;
+                } else {
+                    System.out.print("\nnotifyAll() ");
+                    Task.blocker.prodAll();
+                    prod = true;
+                }
+            }
+        }, 400, 400);
+        TimeUnit.SECONDS.sleep(5);
+        timer.cancel();
+        System.out.println("\nTimer canceled");
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.print("Task2.blocker.prodAll() ");
+        Task2.blocker.prodAll();
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.println("\nShutting down");
+        exec.shutdownNow();
+    }
 }

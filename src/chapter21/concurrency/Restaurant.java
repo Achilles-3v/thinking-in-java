ed190784b8cfc13185ee6ac882d58a32a3ff4a64
Achilles-3v <sh17vv@gmail.com>
2024-05-12 21:24:09 +0500

    Restaurant added class WaitPerson

--- src/chapter21/concurrency/Restaurant.java
+++ src/chapter21/concurrency/Restaurant.java
@@ -8,5 +8,27 @@ class Meal {
     public String toString() { return "Meal " + orderNum; }
 }
 
+class WaitPerson implements Runnable {
+    private Restaurant restaurant;
+    public WaitPerson(Restaurant r) { restaurant = r; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                synchronized(this) {
+                    while(restaurant.meal == null)
+                        wait();
+                }
+                System.out.println("Waitperson got " + restaurant.meal);
+                synchronized(restaurant.chef) {
+                    restaurant.meal = null;
+                    restaurant.chef.notifyAll();
+                }
+            }
+        } catch(InterruptedException e) {
+            System.out.println("WaitPerson interrupted");
+        }
+    }
+}
+
 public class Restaurant {
 }

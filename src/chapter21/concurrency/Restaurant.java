1b15fa63bcca523a466f4fd41f67048ba62da17f
Achilles-3v <sh17vv@gmail.com>
2024-05-12 21:24:48 +0500

    Restaurant added class Chef

--- src/chapter21/concurrency/Restaurant.java
+++ src/chapter21/concurrency/Restaurant.java
@@ -30,5 +30,33 @@ class WaitPerson implements Runnable {
     }
 }
 
+class Chef implements Runnable {
+    private Restaurant restaurant;
+    private int count = 0;
+    public Chef(Restaurant r) { restaurant = r; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                synchronized(this) {
+                    while(restaurant.meal != null)
+                        wait();
+                }
+                if(++count == 10) {
+                    System.out.println("Out of food, closing");
+                    restaurant.exec.shutdownNow();
+                }
+                System.out.print("Order up! ");
+                synchronized(restaurant.waitPerson) {
+                    restaurant.meal = new Meal(count);
+                    restaurant.waitPerson.notifyAll();
+                }
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+        } catch(InterruptedException e) {
+            System.out.println("Chef interrupted");
+        }
+    }
+}
+
 public class Restaurant {
 }

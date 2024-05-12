0c07fe0feda5b996dd6de253769f8bda48c74ce8
Achilles-3v <sh17vv@gmail.com>
2024-05-12 21:23:27 +0500

    Restaurant added class Meal

---
+++ src/chapter21/concurrency/Restaurant.java
@@ -0,0 +1,12 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+
+class Meal {
+    private final int orderNum;
+    public Meal(int orderNum) { this.orderNum = orderNum; }
+    public String toString() { return "Meal " + orderNum; }
+}
+
+public class Restaurant {
+}

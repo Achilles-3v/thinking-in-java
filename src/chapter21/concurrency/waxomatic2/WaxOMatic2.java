6e8e329c8402184fc06474458f5dec7721531b4e
Achilles-3v <sh17vv@gmail.com>
2024-05-13 22:08:00 +0500

    WaxOMatic2 added method main

--- src/chapter21/concurrency/waxomatic2/WaxOMatic2.java
+++ src/chapter21/concurrency/waxomatic2/WaxOMatic2.java
@@ -82,4 +82,12 @@ class WaxOff implements Runnable {
 }
 
 public class WaxOMatic2 {
+    public static void main(String[] args) throws Exception {
+        Car car = new Car();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(new WaxOff(car));
+        exec.execute(new WaxOn(car));
+        TimeUnit.SECONDS.sleep(5);
+        exec.shutdownNow();
+    }
 }

6b33e96b881c55e8e9374f4b69fea840452e6b99
Achilles-3v <sh17vv@gmail.com>
2024-05-13 22:06:58 +0500

    WaxOMatic2 added class WaxOff

--- src/chapter21/concurrency/waxomatic2/WaxOMatic2.java
+++ src/chapter21/concurrency/waxomatic2/WaxOMatic2.java
@@ -63,5 +63,23 @@ class WaxOn implements Runnable {
     }
 }
 
+class WaxOff implements Runnable {
+    private Car car;
+    public WaxOff(Car c) { car = c; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                car.waitForWaxing();
+                System.out.print("Wax Off! ");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.buffed();
+            }
+        } catch(InterruptedException e) {
+            System.out.println("Exiting via interrupt");
+        }
+        System.out.println("Ending Wax Off task");
+    }
+}
+
 public class WaxOMatic2 {
 }

8425d93ae3ddee064795dab5fbc2c9517987be57
Achilles-3v <sh17vv@gmail.com>
2024-05-15 22:04:39 +0500

    PipedIO added class Sender

---
+++ src/chapter21/concurrency/PipedIO.java
@@ -0,0 +1,27 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+import java.io.*;
+import java.util.*;
+
+class Sender implements Runnable {
+    private Random rand = new Random(47);
+    private PipedWriter out = new PipedWriter();
+    public PipedWriter getPipedWriter() { return out; }
+    public void run() {
+        try {
+            while(true)
+                for(char c = 'A'; c <= 'z'; c++) {
+                    out.write(c);
+                    TimeUnit.MILLISECONDS.sleep(rand.nextInt(500));
+                }
+        } catch(IOException e) {
+            System.out.println(e + " Sender write exception");
+        } catch(InterruptedException e) {
+            System.out.println(e + " Sender sleep interrupted");
+        }
+    }
+}
+
+public class PipedIO {
+}

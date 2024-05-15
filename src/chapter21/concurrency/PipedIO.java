b2910328aaab290d79d25e68767866e23bd0b066
Achilles-3v <sh17vv@gmail.com>
2024-05-15 22:05:19 +0500

    PipedIO added class Reciver

--- src/chapter21/concurrency/PipedIO.java
+++ src/chapter21/concurrency/PipedIO.java
@@ -23,5 +23,21 @@ class Sender implements Runnable {
     }
 }
 
+class Receiver implements Runnable {
+    private PipedReader in;
+    public Receiver(Sender sender) throws IOException {
+        in = new PipedReader(sender.getPipedWriter());
+    }
+    public void run() {
+        try {
+            while(true) {
+                System.out.print("Read: " + (char)in.read() + ", ");
+            }
+        } catch(IOException e) {
+            System.out.println(e + " Receiver read exception");
+        }
+    }
+}
+
 public class PipedIO {
 }

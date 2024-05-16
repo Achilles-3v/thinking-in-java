f07b82f77d77175974952470ecf0912d8bd8ce42
Achilles-3v <sh17vv@gmail.com>
2024-05-16 22:50:46 +0500

    CountDownLatchDemo added class WaitingTask

--- src/chapter21/concurrency/CountDownLatchDemo.java
+++ src/chapter21/concurrency/CountDownLatchDemo.java
@@ -27,5 +27,25 @@ class TaskPortion implements Runnable {
     }
 }
 
+class WaitingTask implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private final CountDownLatch latch;
+    WaitingTask(CountDownLatch latch) {
+        this.latch = latch;
+    }
+    public void run() {
+        try {
+            latch.await();
+            System.out.println("Latch barrier passed for " + this);
+        } catch(InterruptedException ex) {
+            System.out.println(this + " interrupted");
+        }
+    }
+    public String toString() {
+        return String.format("WaitingTask %1$-3d ", id);
+    }
+}
+
 public class CountDownLatchDemo {
 }

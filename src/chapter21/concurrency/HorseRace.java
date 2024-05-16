02997046082de24f61e68c5ab87989b263a5729a
Achilles-3v <sh17vv@gmail.com>
2024-05-16 22:55:57 +0500

    HorseRace update class

--- src/chapter21/concurrency/HorseRace.java
+++ src/chapter21/concurrency/HorseRace.java
@@ -35,4 +35,37 @@ class Horse implements Runnable {
 }
 
 public class HorseRace {
+    static final int FINISH_LINE = 75;
+    private List<Horse> horses = new ArrayList<Horse>();
+    private ExecutorService exec =
+            Executors.newCachedThreadPool();
+    private CyclicBarrier barrier;
+    public HorseRace(int nHorses, final int pause) {
+        barrier = new CyclicBarrier(nHorses, new Runnable() {
+            public void run() {
+                StringBuilder s = new StringBuilder();
+                for(int i = 0; i < FINISH_LINE; i++)
+                    s.append("=");
+                System.out.println(s);
+                for(Horse horse : horses)
+                    System.out.println(horse.tracks());
+                for(Horse horse : horses)
+                    if(horse.getStrides() >= FINISH_LINE) {
+                        System.out.println(horse + "won!");
+                        exec.shutdownNow();
+                        return;
+                    }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(pause);
+                } catch(InterruptedException e) {
+                    System.out.println("barrier-action sleep interrupted");
+                }
+            }
+        });
+        for(int i = 0; i < nHorses; i++) {
+            Horse horse = new Horse(barrier);
+            horses.add(horse);
+            exec.execute(horse);
+        }
+    }
 }

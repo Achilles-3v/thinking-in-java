384fb93f0c6eb3fb0932b41fd3d017cb134cde1e
Achilles-3v <sh17vv@gmail.com>
2024-05-16 23:03:31 +0500

    DelayQueueDemo added class DelayedTaskConsumer

--- src/chapter21/concurrency/DelayQueueDemo.java
+++ src/chapter21/concurrency/DelayQueueDemo.java
@@ -53,5 +53,20 @@ class DelayedTask implements Runnable, Delayed {
     }
 }
 
+class DelayedTaskConsumer implements Runnable {
+    private DelayQueue<DelayedTask> q;
+    public DelayedTaskConsumer(DelayQueue<DelayedTask> q) {
+        this.q = q;
+    }
+    public void run() {
+        try {
+            while(!Thread.interrupted())
+                q.take().run();
+        } catch(InterruptedException e) {
+        }
+        System.out.println("Finished DelayedTaskConsumer");
+    }
+}
+
 public class DelayQueueDemo {
 }

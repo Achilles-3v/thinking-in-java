d645a32bca1e33e64b2db37ee96e3de1c7430842
Achilles-3v <sh17vv@gmail.com>
2024-05-16 22:52:45 +0500

    CountDownLatchDemo added method main

--- src/chapter21/concurrency/CountDownLatchDemo.java
+++ src/chapter21/concurrency/CountDownLatchDemo.java
@@ -48,4 +48,15 @@ class WaitingTask implements Runnable {
 }
 
 public class CountDownLatchDemo {
+    static final int SIZE = 100;
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        CountDownLatch latch = new CountDownLatch(SIZE);
+        for(int i = 0; i < 10; i++)
+            exec.execute(new WaitingTask(latch));
+        for(int i = 0; i < SIZE; i++)
+            exec.execute(new TaskPortion(latch));
+        System.out.println("Launched all tasks");
+        exec.shutdown();
+    }
 }

7e4d6ee2fad26e52f1d3548ddda1b749a8fcffab
Achilles-3v <sh17vv@gmail.com>
2024-05-17 23:02:39 +0500

    PriorityBlockingQueueDemo added class PrioritizedTaskProducer

--- src/chapter21/concurrency/PriorityBlockingQueueDemo.java
+++ src/chapter21/concurrency/PriorityBlockingQueueDemo.java
@@ -53,5 +53,33 @@ class PrioritizedTask implements
     }
 }
 
+class PrioritizedTaskProducer implements Runnable {
+    private Random rand = new Random(47);
+    private Queue<Runnable> queue;
+    private ExecutorService exec;
+    public PrioritizedTaskProducer(
+            Queue<Runnable> q, ExecutorService e) {
+        queue = q;
+        exec = e;
+    }
+    public void run() {
+        for(int i = 0; i < 20; i++) {
+            queue.add(new PrioritizedTask(rand.nextInt(10)));
+            Thread.yield();
+        }
+        try {
+            for(int i = 0; i < 10; i++) {
+                TimeUnit.MILLISECONDS.sleep(250);
+                queue.add(new PrioritizedTask(10));
+            }
+            for(int i = 0; i < 10; i++)
+                queue.add(new PrioritizedTask(i));
+            queue.add(new PrioritizedTask.EndSentinel(exec));
+        } catch(InterruptedException e) {
+        }
+        System.out.println("Finished PrioritizedTaskProducer");
+    }
+}
+
 public class PriorityBlockingQueueDemo {
 }

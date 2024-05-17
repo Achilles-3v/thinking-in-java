be62420dbcdbfe18df201aa08e5649f3c5cb0aa5
Achilles-3v <sh17vv@gmail.com>
2024-05-17 23:03:40 +0500

    PriorityBlockingQueueDemo added class PrioritizedTaskConsumer

--- src/chapter21/concurrency/PriorityBlockingQueueDemo.java
+++ src/chapter21/concurrency/PriorityBlockingQueueDemo.java
@@ -81,5 +81,21 @@ class PrioritizedTaskProducer implements Runnable {
     }
 }
 
+class PrioritizedTaskConsumer implements Runnable {
+    private PriorityBlockingQueue<Runnable> q;
+    public PrioritizedTaskConsumer(
+            PriorityBlockingQueue<Runnable> q) {
+        this.q = q;
+    }
+    public void run() {
+        try {
+            while(!Thread.interrupted())
+                q.take().run();
+        } catch(InterruptedException e) {
+        }
+        System.out.println("Finished PrioritizedTaskConsumer");
+    }
+}
+
 public class PriorityBlockingQueueDemo {
 }

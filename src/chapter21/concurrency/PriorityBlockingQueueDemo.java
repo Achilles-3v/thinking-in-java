d6d95b59995c723dd131a503fa373e888beb3cab
Achilles-3v <sh17vv@gmail.com>
2024-05-17 23:04:42 +0500

    PriorityBlockingQueueDemo added method main

--- src/chapter21/concurrency/PriorityBlockingQueueDemo.java
+++ src/chapter21/concurrency/PriorityBlockingQueueDemo.java
@@ -98,4 +98,12 @@ class PrioritizedTaskConsumer implements Runnable {
 }
 
 public class PriorityBlockingQueueDemo {
+    public static void main(String[] args) throws Exception {
+        Random rand = new Random(47);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        PriorityBlockingQueue<Runnable> queue =
+                new PriorityBlockingQueue<Runnable>();
+        exec.execute(new PrioritizedTaskProducer(queue, exec));
+        exec.execute(new PrioritizedTaskConsumer(queue));
+    }
 }

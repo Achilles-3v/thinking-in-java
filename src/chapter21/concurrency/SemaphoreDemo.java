147ecc737b1d814148494a2a10e2361ea00a619e
Achilles-3v <sh17vv@gmail.com>
2024-05-18 18:31:33 +0500

    SemaphoreDemo added method main

--- src/chapter21/concurrency/SemaphoreDemo.java
+++ src/chapter21/concurrency/SemaphoreDemo.java
@@ -26,4 +26,37 @@ class CheckoutTask<T> implements Runnable {
 }
 
 public class SemaphoreDemo {
+    final static int SIZE = 25;
+    public static void main(String[] args) throws Exception {
+        final Pool<Fat> pool =
+                new Pool<Fat>(Fat.class, SIZE);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0; i < SIZE; i++)
+            exec.execute(new CheckoutTask<Fat>(pool));
+        System.out.println("All CheckoutTasks created");
+        List<Fat> list = new ArrayList<Fat>();
+        for(int i = 0; i < SIZE; i++) {
+            Fat f = pool.checkOut();
+            System.out.print(i + ": main() thread checked out ");
+            f.operation();
+            list.add(f);
+        }
+        Future<?> blocked = exec.submit(new Runnable() {
+            public void run() {
+                try {
+                    pool.checkOut();
+                } catch(InterruptedException e) {
+                    System.out.println("checkOut() Interrupted");
+                }
+            }
+        });
+        TimeUnit.SECONDS.sleep(2);
+        blocked.cancel(true);
+        System.out.println("Checking in objects in " + list);
+        for(Fat f : list)
+            pool.checkIn(f);
+        for(Fat f : list)
+            pool.checkIn(f);
+        exec.shutdown();
+    }
 }

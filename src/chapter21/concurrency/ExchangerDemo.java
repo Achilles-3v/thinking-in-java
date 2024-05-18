568507215035ff3bd6b51bad3027ef6dbd08ced5
Achilles-3v <sh17vv@gmail.com>
2024-05-18 18:36:43 +0500

    ExchangerDemo added class ExchengerConsumer

--- src/chapter21/concurrency/ExchangerDemo.java
+++ src/chapter21/concurrency/ExchangerDemo.java
@@ -26,6 +26,29 @@ class ExchangerProducer<T> implements Runnable {
     }
 }
 
+class ExchangerConsumer<T> implements Runnable {
+    private Exchanger<List<T>> exchanger;
+    private List<T> holder;
+    private volatile T value;
+    ExchangerConsumer(Exchanger<List<T>> ex, List<T> holder){
+        exchanger = ex;
+        this.holder = holder;
+    }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                holder = exchanger.exchange(holder);
+                for(T x : holder) {
+                    value = x;
+                    holder.remove(x);
+                }
+            }
+        } catch(InterruptedException e) {
+        }
+        System.out.println("Final value: " + value);
+    }
+}
+
 public class ExchangerDemo {
     static int size = 10;
     static int delay = 5;

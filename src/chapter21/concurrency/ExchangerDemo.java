79070f510b52eb0033ee1af5e47bc62fafd4f8f7
Achilles-3v <sh17vv@gmail.com>
2024-05-18 18:37:36 +0500

    ExchangerDemo added method main

--- src/chapter21/concurrency/ExchangerDemo.java
+++ src/chapter21/concurrency/ExchangerDemo.java
@@ -52,4 +52,21 @@ class ExchangerConsumer<T> implements Runnable {
 public class ExchangerDemo {
     static int size = 10;
     static int delay = 5;
+    public static void main(String[] args) throws Exception {
+        if(args.length > 0)
+            size = new Integer(args[0]);
+        if(args.length > 1)
+            delay = new Integer(args[1]);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Exchanger<List<Fat>> xc = new Exchanger<List<Fat>>();
+        List<Fat>
+                producerList = new CopyOnWriteArrayList<Fat>(),
+                consumerList = new CopyOnWriteArrayList<Fat>();
+        exec.execute(new ExchangerProducer<Fat>(xc,
+                BasicGenerator.create(Fat.class), producerList));
+        exec.execute(
+                new ExchangerConsumer<Fat>(xc,consumerList));
+        TimeUnit.SECONDS.sleep(delay);
+        exec.shutdownNow();
+    }
 }

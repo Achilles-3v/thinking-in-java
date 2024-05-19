b53ef64f65f412d8b638ee20ffc9c63f59ab636d
Achilles-3v <sh17vv@gmail.com>
2024-05-19 22:51:12 +0500

    BankTellerSimulator added class Teller

--- src/chapter21/concurrency/BankTellerSimulation.java
+++ src/chapter21/concurrency/BankTellerSimulation.java
@@ -45,6 +45,47 @@ class CustomerGenerator implements Runnable {
     }
 }
 
+class Teller implements Runnable, Comparable<Teller> {
+    private static int counter = 0;
+    private final int id = counter++;
+    private int customersServed = 0;
+    private CustomerLine customers;
+    private boolean servingCustomerLine = true;
+    public Teller(CustomerLine cq) { customers = cq; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                Customer customer = customers.take();
+                TimeUnit.MILLISECONDS.sleep(
+                        customer.getServiceTime());
+                synchronized(this) {
+                    customersServed++;
+                    while(!servingCustomerLine)
+                        wait();
+                }
+            }
+        } catch(InterruptedException e) {
+            System.out.println(this + "interrupted");
+        }
+        System.out.println(this + "terminating");
+    }
+    public synchronized void doSomethingElse() {
+        customersServed = 0;
+        servingCustomerLine = false;
+    }
+    public synchronized void serveCustomerLine() {
+        assert !servingCustomerLine:"already serving: " + this;
+        servingCustomerLine = true;
+        notifyAll();
+    }
+    public String toString() { return "Teller " + id + " "; }
+    public String shortString() { return "T" + id; }
+    public synchronized int compareTo(Teller other) {
+        return customersServed < other.customersServed ? -1 :
+                (customersServed == other.customersServed ? 0 : 1);
+    }
+}
+
 public class BankTellerSimulation {
     static final int MAX_LINE_SIZE = 50;
     static final int ADJUSTMENT_PERIOD = 1000;

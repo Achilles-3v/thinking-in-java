95b16886c086a2a154670375af424d1d74393300
Achilles-3v <sh17vv@gmail.com>
2024-05-19 22:50:27 +0500

    BankTellerSimulation added class CustomerGenerator

--- src/chapter21/concurrency/BankTellerSimulation.java
+++ src/chapter21/concurrency/BankTellerSimulation.java
@@ -26,6 +26,25 @@ class CustomerLine extends ArrayBlockingQueue<Customer> {
     }
 }
 
+class CustomerGenerator implements Runnable {
+    private CustomerLine customers;
+    private static Random rand = new Random(47);
+    public CustomerGenerator(CustomerLine cq) {
+        customers = cq;
+    }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(rand.nextInt(300));
+                customers.put(new Customer(rand.nextInt(1000)));
+            }
+        } catch(InterruptedException e) {
+            System.out.println("CustomerGenerator interrupted");
+        }
+        System.out.println("CustomerGenerator terminating");
+    }
+}
+
 public class BankTellerSimulation {
     static final int MAX_LINE_SIZE = 50;
     static final int ADJUSTMENT_PERIOD = 1000;

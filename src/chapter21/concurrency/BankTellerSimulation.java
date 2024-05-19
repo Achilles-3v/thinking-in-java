01459b9fb5e5e1074d932d90e9359d8bca0de821
Achilles-3v <sh17vv@gmail.com>
2024-05-19 22:49:29 +0500

    BankTellerSimulation added class CustomerLine

--- src/chapter21/concurrency/BankTellerSimulation.java
+++ src/chapter21/concurrency/BankTellerSimulation.java
@@ -12,6 +12,20 @@ class Customer {
     }
 }
 
+class CustomerLine extends ArrayBlockingQueue<Customer> {
+    public CustomerLine(int maxLineSize) {
+        super(maxLineSize);
+    }
+    public String toString() {
+        if(this.size() == 0)
+            return "[Empty]";
+        StringBuilder result = new StringBuilder();
+        for(Customer customer : this)
+            result.append(customer);
+        return result.toString();
+    }
+}
+
 public class BankTellerSimulation {
     static final int MAX_LINE_SIZE = 50;
     static final int ADJUSTMENT_PERIOD = 1000;

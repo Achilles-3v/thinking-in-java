280dbef44c82591ca18c064df16e098d45194c29
Achilles-3v <sh17vv@gmail.com>
2024-05-20 22:03:09 +0500

    CarBuilder added class Assembler

--- src/chapter21/concurrency/CarBuilder.java
+++ src/chapter21/concurrency/CarBuilder.java
@@ -43,5 +43,36 @@ class ChassisBuilder implements Runnable {
     }
 }
 
+class Assembler implements Runnable {
+    private CarQueue chassisQueue, finishingQueue;
+    private Car car;
+    private CyclicBarrier barrier = new CyclicBarrier(4);
+    private RobotPool robotPool;
+    public Assembler(CarQueue cq, CarQueue fq, RobotPool rp){
+        chassisQueue = cq;
+        finishingQueue = fq;
+        robotPool = rp;
+    }
+    public Car car() { return car; }
+    public CyclicBarrier barrier() { return barrier; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                car = chassisQueue.take();
+                robotPool.hire(EngineRobot.class, this);
+                robotPool.hire(DriveTrainRobot.class, this);
+                robotPool.hire(WheelRobot.class, this);
+                barrier.await();
+                finishingQueue.put(car);
+            }
+        } catch(InterruptedException e) {
+            System.out.println("Exiting Assembler via interrupt");
+        } catch(BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Assembler off");
+    }
+}
+
 public class CarBuilder {
 }

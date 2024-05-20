81e107c354f09c903ce50c91e1dc8011889c3608
Achilles-3v <sh17vv@gmail.com>
2024-05-20 22:04:37 +0500

    CarBuilder added class Robot

--- src/chapter21/concurrency/CarBuilder.java
+++ src/chapter21/concurrency/CarBuilder.java
@@ -89,5 +89,45 @@ class Reporter implements Runnable {
     }
 }
 
+abstract class Robot implements Runnable {
+    private RobotPool pool;
+    public Robot(RobotPool p) { pool = p; }
+    protected Assembler assembler;
+    public Robot assignAssembler(Assembler assembler) {
+        this.assembler = assembler;
+        return this;
+    }
+    private boolean engage = false;
+    public synchronized void engage() {
+        engage = true;
+        notifyAll();
+    }
+    abstract protected void performService();
+    public void run() {
+        try {
+            powerDown();
+            while(!Thread.interrupted()) {
+                performService();
+                assembler.barrier().await();
+                powerDown();
+            }
+        } catch(InterruptedException e) {
+            System.out.println("Exiting " + this + " via interrupt");
+        } catch(BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(this + " off");
+    }
+    private synchronized void
+    powerDown() throws InterruptedException {
+        engage = false;
+        assembler = null;
+        pool.release(this);
+        while(engage == false)
+            wait();
+    }
+    public String toString() { return getClass().getName(); }
+}
+
 public class CarBuilder {
 }

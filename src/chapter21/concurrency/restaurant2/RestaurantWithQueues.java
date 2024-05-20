b3172539d69c42c9c0cf856ae5d55b96588058b9
Achilles-3v <sh17vv@gmail.com>
2024-05-20 21:50:51 +0500

    RestaurantWithQueues added method main

--- src/chapter21/concurrency/restaurant2/RestaurantWithQueues.java
+++ src/chapter21/concurrency/restaurant2/RestaurantWithQueues.java
@@ -162,4 +162,16 @@ class Restaurant implements Runnable {
 }
 
 public class RestaurantWithQueues {
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Restaurant restaurant = new Restaurant(exec, 5, 2);
+        exec.execute(restaurant);
+        if(args.length > 0)
+            TimeUnit.SECONDS.sleep(new Integer(args[0]));
+        else {
+            System.out.println("Press 'Enter' to quit");
+            System.in.read();
+        }
+        exec.shutdownNow();
+    }
 }

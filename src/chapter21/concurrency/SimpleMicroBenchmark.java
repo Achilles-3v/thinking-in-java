deed221cdf7f7389330de9d04b82414e14180118
Achilles-3v <sh17vv@gmail.com>
2024-05-21 22:18:49 +0500

    SimpleMicroBenchmark added method main

--- src/chapter21/concurrency/SimpleMicroBenchmark.java
+++ src/chapter21/concurrency/SimpleMicroBenchmark.java
@@ -30,4 +30,12 @@ public class SimpleMicroBenchmark {
             incr.increment();
         return System.nanoTime() - start;
     }
+    public static void main(String[] args) {
+        long synchTime = test(new SynchronizingTest());
+        long lockTime = test(new LockingTest());
+        System.out.printf("synchronized: %1$10d\n", synchTime);
+        System.out.printf("Lock:         %1$10d\n", lockTime);
+        System.out.printf("Lock/synchronized = %1$.3f",
+                (double)lockTime/(double)synchTime);
+    }
 }

12b3413eff7eb6a0825290b8e8a98443dc57156c
Achilles-3v <sh17vv@gmail.com>
2024-05-22 21:55:14 +0500

    Tester added method runTest

--- src/chapter21/concurrency/Tester.java
+++ src/chapter21/concurrency/Tester.java
@@ -28,8 +28,24 @@ public abstract class Tester<C> {
         writeData = Generated.array(Integer.class,
                 new RandomGenerator.Integer(), containerSize);
         for(int i = 0; i < testReps; i++) {
+            runTest();
             readTime = 0;
             writeTime = 0;
         }
+    }
+    void runTest() {
+        endLatch = new CountDownLatch(nReaders + nWriters);
+        testContainer = containerInitializer();
+        startReadersAndWriters();
+        try {
+            endLatch.await();
+        } catch(InterruptedException ex) {
+            System.out.println("endLatch interrupted");
+        }
+        System.out.printf("%-27s %14d %14d\n",
+                testId, readTime, writeTime);
+        if(readTime != 0 && writeTime != 0)
+            System.out.printf("%-27s %14d\n",
+                    "readTime + writeTime =", readTime + writeTime);
     }
 }

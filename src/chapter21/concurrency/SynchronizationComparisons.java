fb136e3ef54afa77aa93967522d5fa8b3c30b421
Achilles-3v <sh17vv@gmail.com>
2024-05-22 21:47:59 +0500

    SynchronizationComparisons added class LockTest

--- src/chapter21/concurrency/SynchronizationComparisons.java
+++ src/chapter21/concurrency/SynchronizationComparisons.java
@@ -89,5 +89,27 @@ class SynchronizedTest extends Accumulator {
     }
 }
 
+class LockTest extends Accumulator {
+    { id = "Lock"; }
+    private Lock lock = new ReentrantLock();
+    public void accumulate() {
+        lock.lock();
+        try {
+            value += preLoaded[index++];
+            if(index >= SIZE) index = 0;
+        } finally {
+            lock.unlock();
+        }
+    }
+    public long read() {
+        lock.lock();
+        try {
+            return value;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
 public class SynchronizationComparisons {
 }

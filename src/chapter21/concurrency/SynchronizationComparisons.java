ed75bff7aa0b0ab42d3ad955dcc54ca05d9045d7
Achilles-3v <sh17vv@gmail.com>
2024-05-22 21:48:43 +0500

    SynchronizationComparisons added class AtomicTest

--- src/chapter21/concurrency/SynchronizationComparisons.java
+++ src/chapter21/concurrency/SynchronizationComparisons.java
@@ -111,5 +111,18 @@ class LockTest extends Accumulator {
     }
 }
 
+class AtomicTest extends Accumulator {
+    { id = "Atomic"; }
+    private AtomicInteger index = new AtomicInteger(0);
+    private AtomicLong value = new AtomicLong(0);
+    public void accumulate() {
+        int i = index.getAndIncrement();
+        value.getAndAdd(preLoaded[i]);
+        if(++i >= SIZE)
+            index.set(0);
+    }
+    public long read() { return value.get(); }
+}
+
 public class SynchronizationComparisons {
 }

7c99fd8ebe091419d068f017832d93e389652062
Achilles-3v <sh17vv@gmail.com>
2024-05-23 21:40:52 +0500

    ListComparisons added method main

--- src/chapter21/concurrency/ListComparisons.java
+++ src/chapter21/concurrency/ListComparisons.java
@@ -60,4 +60,14 @@ class CopyOnWriteArrayListTest extends ListTest {
 }
 
 public class ListComparisons {
+    public static void main(String[] args) {
+        Tester.initMain(args);
+        new SynchronizedArrayListTest(10, 0);
+        new SynchronizedArrayListTest(9, 1);
+        new SynchronizedArrayListTest(5, 5);
+        new CopyOnWriteArrayListTest(10, 0);
+        new CopyOnWriteArrayListTest(9, 1);
+        new CopyOnWriteArrayListTest(5, 5);
+        Tester.exec.shutdown();
+    }
 }

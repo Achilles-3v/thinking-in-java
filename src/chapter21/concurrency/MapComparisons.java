3546970bef72fa7edfbcfcbdfbc26799c1a3a4b6
Achilles-3v <sh17vv@gmail.com>
2024-05-23 21:43:00 +0500

    chapter21 added class MapComparisons

---
+++ src/chapter21/concurrency/MapComparisons.java
@@ -0,0 +1,43 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+import java.util.*;
+import net.achilles.util.*;
+
+abstract class MapTest
+        extends Tester<Map<Integer,Integer>> {
+    MapTest(String testId, int nReaders, int nWriters) {
+        super(testId, nReaders, nWriters);
+    }
+    class Reader extends TestTask {
+        long result = 0;
+        void test() {
+            for(long i = 0; i < testCycles; i++)
+                for(int index = 0; index < containerSize; index++)
+                    result += testContainer.get(index);
+        }
+        void putResults() {
+            readResult += result;
+            readTime += duration;
+        }
+    }
+    class Writer extends TestTask {
+        void test() {
+            for(long i = 0; i < testCycles; i++)
+                for(int index = 0; index < containerSize; index++)
+                    testContainer.put(index, writeData[index]);
+        }
+        void putResults() {
+            writeTime += duration;
+        }
+    }
+    void startReadersAndWriters() {
+        for(int i = 0; i < nReaders; i++)
+            exec.execute(new Reader());
+        for(int i = 0; i < nWriters; i++)
+            exec.execute(new Writer());
+    }
+}
+
+public class MapComparisons {
+}

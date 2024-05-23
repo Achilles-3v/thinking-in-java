d68bfc1e7e7da682202da9594c81071759917d48
Achilles-3v <sh17vv@gmail.com>
2024-05-23 21:43:44 +0500

    MapComparisons added class SynchronizedHashMapTest

--- src/chapter21/concurrency/MapComparisons.java
+++ src/chapter21/concurrency/MapComparisons.java
@@ -39,5 +39,19 @@ abstract class MapTest
     }
 }
 
+class SynchronizedHashMapTest extends MapTest {
+    Map<Integer,Integer> containerInitializer() {
+        return Collections.synchronizedMap(
+                new HashMap<Integer,Integer>(
+                        MapData.map(
+                                new CountingGenerator.Integer(),
+                                new CountingGenerator.Integer(),
+                                containerSize)));
+    }
+    SynchronizedHashMapTest(int nReaders, int nWriters) {
+        super("Synched HashMap", nReaders, nWriters);
+    }
+}
+
 public class MapComparisons {
 }

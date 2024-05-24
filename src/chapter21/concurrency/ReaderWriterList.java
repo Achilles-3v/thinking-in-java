05ce46eb03f47efb9d01833a89ac412a32f72bd7
Achilles-3v <sh17vv@gmail.com>
2024-05-24 22:30:11 +0500

    ReaderWriterListTest added construction

--- src/chapter21/concurrency/ReaderWriterList.java
+++ src/chapter21/concurrency/ReaderWriterList.java
@@ -66,4 +66,10 @@ class ReaderWriterListTest {
             }
         }
     }
+    public ReaderWriterListTest(int readers, int writers) {
+        for(int i = 0; i < readers; i++)
+            exec.execute(new Reader());
+        for(int i = 0; i < writers; i++)
+            exec.execute(new Writer());
+    }
 }

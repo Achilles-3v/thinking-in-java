ff9db2adb24b78e4a1ace5dbe7319f6af3fa1066
Achilles-3v <sh17vv@gmail.com>
2024-05-24 22:26:12 +0500

    chapter21 added class ReaderWriterList

---
+++ src/chapter21/concurrency/ReaderWriterList.java
@@ -0,0 +1,36 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
+import java.util.*;
+
+public class ReaderWriterList<T> {
+    private ArrayList<T> lockedList;
+    private ReentrantReadWriteLock lock =
+            new ReentrantReadWriteLock(true);
+    public ReaderWriterList(int size, T initialValue) {
+        lockedList = new ArrayList<T>(
+                Collections.nCopies(size, initialValue));
+    }
+    public T set(int index, T element) {
+        Lock wlock = lock.writeLock();
+        wlock.lock();
+        try {
+            return lockedList.set(index, element);
+        } finally {
+            wlock.unlock();
+        }
+    }
+    public T get(int index) {
+        Lock rlock = lock.readLock();
+        rlock.lock();
+        try {
+            if(lock.getReadLockCount() > 1)
+                System.out.println(lock.getReadLockCount());
+            return lockedList.get(index);
+        } finally {
+            rlock.unlock();
+        }
+    }
+}
+

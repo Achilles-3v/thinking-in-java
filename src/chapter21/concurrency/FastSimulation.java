87fc4d38aa76cabbc3de4ad81aa820ef9d186ec6
Achilles-3v <sh17vv@gmail.com>
2024-05-24 22:21:56 +0500

    chapter21 added class FastSimulation

---
+++ src/chapter21/concurrency/FastSimulation.java
@@ -0,0 +1,35 @@
+package chapter21.concurrency;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.*;
+
+public class FastSimulation {
+    static final int N_ELEMENTS = 100000;
+    static final int N_GENES = 30;
+    static final int N_EVOLVERS = 50;
+    static final AtomicInteger[][] GRID =
+            new AtomicInteger[N_ELEMENTS][N_GENES];
+    static Random rand = new Random(47);
+    static class Evolver implements Runnable {
+        public void run() {
+            while(!Thread.interrupted()) {
+                int element = rand.nextInt(N_ELEMENTS);
+                for(int i = 0; i < N_GENES; i++) {
+                    int previous = element - 1;
+                    if(previous < 0) previous = N_ELEMENTS - 1;
+                    int next = element + 1;
+                    if(next >= N_ELEMENTS) next = 0;
+                    int oldvalue = GRID[element][i].get();
+                    int newvalue = oldvalue +
+                            GRID[previous][i].get() + GRID[next][i].get();
+                    newvalue /= 3;
+                    if(!GRID[element][i]
+                            .compareAndSet(oldvalue, newvalue)) {
+                        System.out.println("Old value changed from " + oldvalue);
+                    }
+                }
+            }
+        }
+    }
+}

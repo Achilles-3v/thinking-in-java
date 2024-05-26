ce4c57f24419f0302e7d000c194b6d9a3a62e061
Achilles-3v <sh17vv@gmail.com>
2024-05-26 22:20:51 +0500

    chapter22 added class Button1

---
+++ src/chapter22/gui/Button1.java
@@ -0,0 +1,16 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class Button1 extends JFrame {
+    private JButton
+            b1 = new JButton("Button 1"),
+            b2 = new JButton("Button 2");
+    public Button1() {
+        setLayout(new FlowLayout());
+        add(b1);
+        add(b2);
+    }
+}

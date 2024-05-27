430527a120dc4cf0f768cb9b211cb054b7dca198
Achilles-3v <sh17vv@gmail.com>
2024-05-27 22:12:27 +0500

    chapter22 added class TextArea

---
+++ src/chapter22/gui/TextArea.java
@@ -0,0 +1,35 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import net.achilles.util.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class TextArea extends JFrame {
+    private JButton
+            b = new JButton("Add Data"),
+            c = new JButton("Clear Data");
+    private JTextArea t = new JTextArea(20, 40);
+    private Map<String,String> m =
+            new HashMap<String,String>();
+    public TextArea() {
+        m.putAll(Countries.capitals());
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                for(Map.Entry me : m.entrySet())
+                    t.append(me.getKey() + ": "+ me.getValue()+"\n");
+            }
+        });
+        c.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                t.setText("");
+            }
+        });
+        setLayout(new FlowLayout());
+        add(new JScrollPane(t));
+        add(b);
+        add(c);
+    }
+}

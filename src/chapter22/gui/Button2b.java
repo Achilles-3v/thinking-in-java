aa789db6ebb8a8610df5d16d129f12c8ce63caf5
Achilles-3v <sh17vv@gmail.com>
2024-05-27 22:09:28 +0500

    chapter22 added class Button2b

---
+++ src/chapter22/gui/Button2b.java
@@ -0,0 +1,27 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class Button2b extends JFrame {
+    private JButton
+            b1 = new JButton("Button 1"),
+            b2 = new JButton("Button 2");
+    private JTextField txt = new JTextField(10);
+    private ActionListener bl = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            String name = ((JButton)e.getSource()).getText();
+            txt.setText(name);
+        }
+    };
+    public Button2b() {
+        b1.addActionListener(bl);
+        b2.addActionListener(bl);
+        setLayout(new FlowLayout());
+        add(b1);
+        add(b2);
+        add(txt);
+    }
+}

1b404b3aa141afbef8f398791875dada1d505c49
Achilles-3v <sh17vv@gmail.com>
2024-05-29 22:22:04 +0500

    Buttons added constructor

--- src/chapter22/gui/Buttons.java
+++ src/chapter22/gui/Buttons.java
@@ -13,4 +13,18 @@ public class Buttons extends JFrame {
             down = new BasicArrowButton(BasicArrowButton.SOUTH),
             right = new BasicArrowButton(BasicArrowButton.EAST),
             left = new BasicArrowButton(BasicArrowButton.WEST);
+    public Buttons() {
+        setLayout(new FlowLayout());
+        add(jb);
+        add(new JToggleButton("JToggleButton"));
+        add(new JCheckBox("JCheckBox"));
+        add(new JRadioButton("JRadioButton"));
+        JPanel jp = new JPanel();
+        jp.setBorder(new TitledBorder("Directions"));
+        jp.add(up);
+        jp.add(down);
+        jp.add(left);
+        jp.add(right);
+        add(jp);
+    }
 }

69bff1b9d9749402c6bbc973746bfb67e65cf151
Achilles-3v <sh17vv@gmail.com>
2024-05-29 22:15:54 +0500

    TrackEvent added class MyButton

--- src/chapter22/gui/TrackEvent.java
+++ src/chapter22/gui/TrackEvent.java
@@ -18,4 +18,61 @@ public class TrackEvent extends JFrame {
     private MyButton
             b1 = new MyButton(Color.BLUE, "test1"),
             b2 = new MyButton(Color.RED, "test2");
+    class MyButton extends JButton {
+        void report(String field, String msg) {
+            h.get(field).setText(msg);
+        }
+        FocusListener fl = new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                report("focusGained", e.paramString());
+            }
+            public void focusLost(FocusEvent e) {
+                report("focusLost", e.paramString());
+            }
+        };
+        KeyListener kl = new KeyListener() {
+            public void keyPressed(KeyEvent e) {
+                report("keyPressed", e.paramString());
+            }
+            public void keyReleased(KeyEvent e) {
+                report("keyReleased", e.paramString());
+            }
+            public void keyTyped(KeyEvent e) {
+                report("keyTyped", e.paramString());
+            }
+        };
+        MouseListener ml = new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                report("mouseClicked", e.paramString());
+            }
+            public void mouseEntered(MouseEvent e) {
+                report("mouseEntered", e.paramString());
+            }
+            public void mouseExited(MouseEvent e) {
+                report("mouseExited", e.paramString());
+            }
+            public void mousePressed(MouseEvent e) {
+                report("mousePressed", e.paramString());
+            }
+            public void mouseReleased(MouseEvent e) {
+                report("mouseReleased", e.paramString());
+            }
+        };
+        MouseMotionListener mml = new MouseMotionListener() {
+            public void mouseDragged(MouseEvent e) {
+                report("mouseDragged", e.paramString());
+            }
+            public void mouseMoved(MouseEvent e) {
+                report("mouseMoved", e.paramString());
+            }
+        };
+        public MyButton(Color color, String label) {
+            super(label);
+            setBackground(color);
+            addFocusListener(fl);
+            addKeyListener(kl);
+            addMouseListener(ml);
+            addMouseMotionListener(mml);
+        }
+    }
 }

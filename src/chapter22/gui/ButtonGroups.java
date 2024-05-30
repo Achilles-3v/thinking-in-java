7e35e674207dc4e8dc148fdbdcae3ca06e960437
Achilles-3v <sh17vv@gmail.com>
2024-05-30 22:17:55 +0500

    ButtonGroups added method makeBPanel

--- src/chapter22/gui/ButtonGroups.java
+++ src/chapter22/gui/ButtonGroups.java
@@ -10,4 +10,25 @@ public class ButtonGroups extends JFrame {
     private static String[] ids = {
             "June", "Ward", "Beaver", "Wally", "Eddie", "Lumpy"
     };
+    static JPanel makeBPanel(
+            Class<? extends AbstractButton> kind, String[] ids) {
+        ButtonGroup bg = new ButtonGroup();
+        JPanel jp = new JPanel();
+        String title = kind.getName();
+        title = title.substring(title.lastIndexOf('.') + 1);
+        jp.setBorder(new TitledBorder(title));
+        for(String id : ids) {
+            AbstractButton ab = new JButton("failed");
+            try {
+                Constructor ctor =
+                        kind.getConstructor(String.class);
+                ab = (AbstractButton)ctor.newInstance(id);
+            } catch(Exception ex) {
+                System.err.println("can't create " + kind);
+            }
+            bg.add(ab);
+            jp.add(ab);
+        }
+        return jp;
+    }
 }

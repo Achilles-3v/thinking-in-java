26d396ff0839df1fd86911602c5b4fe5c1c8cd82
Achilles-3v <sh17vv@gmail.com>
2024-05-30 22:21:40 +0500

    chapter22 added class Faces

---
+++ src/chapter22/gui/Faces.java
@@ -0,0 +1,12 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class Faces extends JFrame {
+    private static Icon[] faces;
+    private JButton jb, jb2 = new JButton("Disable");
+    private boolean mad = false;
+}

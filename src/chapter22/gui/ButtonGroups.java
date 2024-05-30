b0117a1b34c8f7b87ce5200432425130c2a506fd
Achilles-3v <sh17vv@gmail.com>
2024-05-30 22:17:08 +0500

    chapter22 added class ButtonGroups

---
+++ src/chapter22/gui/ButtonGroups.java
@@ -0,0 +1,13 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.lang.reflect.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class ButtonGroups extends JFrame {
+    private static String[] ids = {
+            "June", "Ward", "Beaver", "Wally", "Eddie", "Lumpy"
+    };
+}

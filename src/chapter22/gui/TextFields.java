084419f4d8b91fa509011b5a07d955cdd6d7242b
Achilles-3v <sh17vv@gmail.com>
2024-05-31 21:47:05 +0500

    TextFields added class UpperCaseDocument

--- src/chapter22/gui/TextFields.java
+++ src/chapter22/gui/TextFields.java
@@ -16,4 +16,18 @@ public class TextFields extends JFrame {
             t2 = new JTextField(30),
             t3 = new JTextField(30);
     private String s = "";
+    private UpperCaseDocument ucd = new UpperCaseDocument();
+}
+
+class UpperCaseDocument extends PlainDocument {
+    private boolean upperCase = true;
+    public void setUpperCase(boolean flag) {
+        upperCase = flag;
+    }
+    public void
+    insertString(int offset, String str, AttributeSet attSet)
+            throws BadLocationException {
+        if(upperCase) str = str.toUpperCase();
+        super.insertString(offset, str, attSet);
+    }
 }

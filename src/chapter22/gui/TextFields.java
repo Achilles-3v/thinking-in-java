081024b29ecc6a427e5173a124867212d0efcda6
Achilles-3v <sh17vv@gmail.com>
2024-05-31 21:46:22 +0500

    chapter22 added class TextFields

---
+++ src/chapter22/gui/TextFields.java
@@ -0,0 +1,19 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class TextFields extends JFrame {
+    private JButton
+            b1 = new JButton("Get Text"),
+            b2 = new JButton("Set Text");
+    private JTextField
+            t1 = new JTextField(30),
+            t2 = new JTextField(30),
+            t3 = new JTextField(30);
+    private String s = "";
+}

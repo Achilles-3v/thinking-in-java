0ec2d88acef49bb3fea46e0465297230d83393bb
Achilles-3v <sh17vv@gmail.com>
2024-06-01 22:07:00 +0500

    Borders added constructor

--- src/chapter22/gui/Borders.java
+++ src/chapter22/gui/Borders.java
@@ -16,4 +16,19 @@ public class Borders extends JFrame {
         jp.setBorder(b);
         return jp;
     }
+    public Borders() {
+        setLayout(new GridLayout(2,4));
+        add(showBorder(new TitledBorder("Title")));
+        add(showBorder(new EtchedBorder()));
+        add(showBorder(new LineBorder(Color.BLUE)));
+        add(showBorder(
+                new MatteBorder(5,5,30,30,Color.GREEN)));
+        add(showBorder(
+                new BevelBorder(BevelBorder.RAISED)));
+        add(showBorder(
+                new SoftBevelBorder(BevelBorder.LOWERED)));
+        add(showBorder(new CompoundBorder(
+                new EtchedBorder(),
+                new LineBorder(Color.RED))));
+    }
 }

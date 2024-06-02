78c813c645a67c49318468e30867cf9a9ad185b6
Achilles-3v <sh17vv@gmail.com>
2024-06-02 21:16:48 +0500

    chapter22 added class CheckBoxes

---
+++ src/chapter22/gui/CheckBoxes.java
@@ -0,0 +1,14 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class CheckBoxes extends JFrame {
+    private JTextArea t = new JTextArea(6, 15);
+    private JCheckBox
+            cb1 = new JCheckBox("Check Box 1"),
+            cb2 = new JCheckBox("Check Box 2"),
+            cb3 = new JCheckBox("Check Box 3");
+}

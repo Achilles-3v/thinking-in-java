be82c85cd2f88b31e66b3fe81e321d75ac1b1b3d
Achilles-3v <sh17vv@gmail.com>
2024-06-02 21:18:18 +0500

    CheckBoxes added constructor

--- src/chapter22/gui/CheckBoxes.java
+++ src/chapter22/gui/CheckBoxes.java
@@ -11,6 +11,28 @@ public class CheckBoxes extends JFrame {
             cb1 = new JCheckBox("Check Box 1"),
             cb2 = new JCheckBox("Check Box 2"),
             cb3 = new JCheckBox("Check Box 3");
+    public CheckBoxes() {
+        cb1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                trace("1", cb1);
+            }
+        });
+        cb2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                trace("2", cb2);
+            }
+        });
+        cb3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                trace("3", cb3);
+            }
+        });
+        setLayout(new FlowLayout());
+        add(new JScrollPane(t));
+        add(cb1);
+        add(cb2);
+        add(cb3);
+    }
     private void trace(String b, JCheckBox cb) {
         if(cb.isSelected())
             t.append("Box " + b + " Set\n");

5155630d0f28df73bed2493810aaaeebbe7ad8b6
Achilles-3v <sh17vv@gmail.com>
2024-06-02 21:21:48 +0500

    RadioButtons added constructor

--- src/chapter22/gui/RadioButtons.java
+++ src/chapter22/gui/RadioButtons.java
@@ -18,4 +18,16 @@ public class RadioButtons extends JFrame {
                     ((JRadioButton)e.getSource()).getText());
         }
     };
+    public RadioButtons() {
+        rb1.addActionListener(al);
+        rb2.addActionListener(al);
+        rb3.addActionListener(al);
+        g.add(rb1); g.add(rb2); g.add(rb3);
+        t.setEditable(false);
+        setLayout(new FlowLayout());
+        add(t);
+        add(rb1);
+        add(rb2);
+        add(rb3);
+    }
 }

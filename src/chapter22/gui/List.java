caf0dd66bdc82b271197ca7a042094ff947735d4
Achilles-3v <sh17vv@gmail.com>
2024-06-03 20:46:48 +0500

    List update

--- src/chapter22/gui/List.java
+++ src/chapter22/gui/List.java
@@ -18,5 +18,23 @@ public class List extends JFrame {
     private JTextArea t =
             new JTextArea(flavors.length, 20);
     private JButton b = new JButton("Add Item");
+    private ActionListener bl = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            if(count < flavors.length) {
+                lItems.add(0, flavors[count++]);
+            } else {
+                b.setEnabled(false);
+            }
+        }
+    };
+    private ListSelectionListener ll =
+            new ListSelectionListener() {
+                public void valueChanged(ListSelectionEvent e) {
+                    if(e.getValueIsAdjusting()) return;
+                    t.setText("");
+                    for(Object item : lst.getSelectedValues())
+                        t.append(item + "\n");
+                }
+            };
     private int count = 0;
 }

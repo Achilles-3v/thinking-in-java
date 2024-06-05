a75bce85cd933a8b8ed9d450282c10251a68a3c0
Achilles-3v <sh17vv@gmail.com>
2024-06-05 20:32:49 +0500

    chapter22 added class SimpleMenus

---
+++ src/chapter22/gui/SimpleMenus.java
@@ -0,0 +1,15 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class SimpleMenus extends JFrame {
+    private JTextField t = new JTextField(15);
+    private ActionListener al = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            t.setText(((JMenuItem)e.getSource()).getText());
+        }
+    };
+}

da547e07d92ddf3ef835c7d76bbd845494abb4ad
Achilles-3v <sh17vv@gmail.com>
2024-06-07 20:46:52 +0500

    SineDraw added method setCycles

--- src/chapter22/gui/SineWave.java
+++ src/chapter22/gui/SineWave.java
@@ -11,6 +11,17 @@ class SineDraw extends JPanel {
     private int points;
     private double[] sines;
     private int[] pts;
+    public SineDraw() { setCycles(5); }
+    public void setCycles(int newCycles) {
+        cycles = newCycles;
+        points = SCALEFACTOR * cycles * 2;
+        sines = new double[points];
+        for(int i = 0; i < points; i++) {
+            double radians = (Math.PI / SCALEFACTOR) * i;
+            sines[i] = Math.sin(radians);
+        }
+        repaint();
+    }
 }
 
 public class SineWave extends JFrame {

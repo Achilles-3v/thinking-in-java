b91f2e06a8272e1dd9b3946a2dbbd1420dc4a225
Achilles-3v <sh17vv@gmail.com>
2024-06-08 20:21:57 +0500

    TicTacToe added class ToeDialog

--- src/chapter22/gui/TicTacToe.java
+++ src/chapter22/gui/TicTacToe.java
@@ -10,4 +10,49 @@ public class TicTacToe extends JFrame {
             rows = new JTextField("3"),
             cols = new JTextField("3");
     private enum State { BLANK, XX, OO }
+    static class ToeDialog extends JDialog {
+        private State turn = State.XX; // Start with x's turn
+        ToeDialog(int cellsWide, int cellsHigh) {
+            setTitle("The game itself");
+            setLayout(new GridLayout(cellsWide, cellsHigh));
+            for(int i = 0; i < cellsWide * cellsHigh; i++)
+                add(new ToeButton());
+            setSize(cellsWide * 50, cellsHigh * 50);
+            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        }
+        class ToeButton extends JPanel {
+            private State state = State.BLANK;
+            public ToeButton() { addMouseListener(new ML()); }
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                int
+                        x1 = 0, y1 = 0,
+                        x2 = getSize().width - 1,
+                        y2 = getSize().height - 1;
+                g.drawRect(x1, y1, x2, y2);
+                x1 = x2/4;
+                y1 = y2/4;
+                int wide = x2/2, high = y2/2;
+                if(state == State.XX) {
+                    g.drawLine(x1, y1, x1 + wide, y1 + high);
+                    g.drawLine(x1, y1 + high, x1 + wide, y1);
+                }
+                if(state == State.OO)
+                    g.drawOval(x1, y1, x1 + wide/2, y1 + high/2);
+            }
+            class ML extends MouseAdapter {
+                public void mousePressed(MouseEvent e) {
+                    if(state == State.BLANK) {
+                        state = turn;
+                        turn =
+                                (turn == State.XX ? State.OO : State.XX);
+                    }
+                    else
+                        state =
+                                (state == State.XX ? State.OO : State.XX);
+                    repaint();
+                }
+            }
+        }
+    }
 }

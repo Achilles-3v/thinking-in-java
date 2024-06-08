433072b31725223d252cd0166de98c82528e10f7
Achilles-3v <sh17vv@gmail.com>
2024-06-08 20:18:07 +0500

    Dialogs added class MyDialogs

--- src/chapter22/gui/Dialogs.java
+++ src/chapter22/gui/Dialogs.java
@@ -5,5 +5,21 @@ import java.awt.*;
 import java.awt.event.*;
 import static net.achilles.util.SwingConsole.*;
 
+class MyDialog extends JDialog {
+    public MyDialog(JFrame parent) {
+        super(parent, "My dialog", true);
+        setLayout(new FlowLayout());
+        add(new JLabel("Here is my dialog"));
+        JButton ok = new JButton("OK");
+        ok.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        add(ok);
+        setSize(150,125);
+    }
+}
+
 public class Dialogs extends JFrame {
 }

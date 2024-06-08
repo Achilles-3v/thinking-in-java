6d3783c9e98e4f257de834ed0ccfae0da4cebd71
Achilles-3v <sh17vv@gmail.com>
2024-06-08 20:17:34 +0500

    chapter22 added class Dialogs

---
+++ src/chapter22/gui/Dialogs.java
@@ -0,0 +1,9 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class Dialogs extends JFrame {
+}

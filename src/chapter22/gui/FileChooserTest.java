fd428c0eb5ae0cffd8b42001574a079aab708608
Achilles-3v <sh17vv@gmail.com>
2024-06-09 20:33:41 +0500

    chapter22 added class FileChooserTest

---
+++ src/chapter22/gui/FileChooserTest.java
@@ -0,0 +1,15 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class FileChooserTest extends JFrame {
+    private JTextField
+            fileName = new JTextField(),
+            dir = new JTextField();
+    private JButton
+            open = new JButton("Open"),
+            save = new JButton("Save");
+}

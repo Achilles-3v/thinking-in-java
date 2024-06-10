7927f05299ef495fc64ff3dd7b562cefc1654c83
Achilles-3v <sh17vv@gmail.com>
2024-06-10 20:44:11 +0500

    Progress added constructor

--- src/chapter22/gui/Progress.java
+++ src/chapter22/gui/Progress.java
@@ -12,4 +12,22 @@ public class Progress extends JFrame {
             this, "Monitoring Progress", "Test", 0, 100);
     private JSlider sb =
             new JSlider(JSlider.HORIZONTAL, 0, 100, 60);
+    public Progress() {
+        setLayout(new GridLayout(2,1));
+        add(pb);
+        pm.setProgress(0);
+        pm.setMillisToPopup(1000);
+        sb.setValue(0);
+        sb.setPaintTicks(true);
+        sb.setMajorTickSpacing(20);
+        sb.setMinorTickSpacing(5);
+        sb.setBorder(new TitledBorder("Slide Me"));
+        pb.setModel(sb.getModel()); // Share model
+        add(sb);
+        sb.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                pm.setProgress(sb.getValue());
+            }
+        });
+    }
 }

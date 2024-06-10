d8b6fe374c1ced4fd59eb9ce23f965c3b80da09e
Achilles-3v <sh17vv@gmail.com>
2024-06-10 20:38:07 +0500

    chapter22 added class HTMLButton

---
+++ src/chapter22/gui/HTMLButton.java
@@ -0,0 +1,12 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class HTMLButton extends JFrame {
+    private JButton b = new JButton(
+            "<html><b><font size=+2>" +
+                    "<center>Hello!<br><i>Press me now!");
+}

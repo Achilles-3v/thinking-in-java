6c8574e674dd5ee39e036d08519d3779b9ea337e
Achilles-3v <sh17vv@gmail.com>
2024-06-11 22:43:43 +0500

    JnlpFileChooser added constructor

--- src/chapter22/gui/jnlp/JnlpFileChooser.java
+++ src/chapter22/gui/jnlp/JnlpFileChooser.java
@@ -14,4 +14,21 @@ public class JnlpFileChooser extends JFrame {
     private JEditorPane ep = new JEditorPane();
     private JScrollPane jsp = new JScrollPane();
     private FileContents fileContents;
+    public JnlpFileChooser() {
+        JPanel p = new JPanel();
+        open.addActionListener(new OpenL());
+        p.add(open);
+        save.addActionListener(new SaveL());
+        p.add(save);
+        jsp.getViewport().add(ep);
+        add(jsp, BorderLayout.CENTER);
+        add(p, BorderLayout.SOUTH);
+        fileName.setEditable(false);
+        p = new JPanel();
+        p.setLayout(new GridLayout(2,1));
+        p.add(fileName);
+        add(p, BorderLayout.NORTH);
+        ep.setContentType("text");
+        save.setEnabled(false);
+    }
 }

28bc7df7ce11bb202fe02550b6ddf1f889200481
Achilles-3v <sh17vv@gmail.com>
2024-06-12 22:05:01 +0500

    chapter22 added class LongRunningTask

---
+++ src/chapter22/gui/LongRunningTask.java
@@ -0,0 +1,13 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class LongRunningTask extends JFrame {
+    private JButton
+            b1 = new JButton("Start Long Running Task"),
+            b2 = new JButton("End Long Running Task");
+}

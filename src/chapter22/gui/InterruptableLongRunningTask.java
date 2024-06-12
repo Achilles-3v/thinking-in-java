c60b614f5ae2bec86c2f79ddca00cb07ab71441d
Achilles-3v <sh17vv@gmail.com>
2024-06-12 22:12:47 +0500

    InterruptableLongRunningTask added class Task

--- src/chapter22/gui/InterruptableLongRunningTask.java
+++ src/chapter22/gui/InterruptableLongRunningTask.java
@@ -6,6 +6,23 @@ import java.awt.event.*;
 import java.util.concurrent.*;
 import static net.achilles.util.SwingConsole.*;
 
+class Task implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    public void run() {
+        System.out.println(this + " started");
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch(InterruptedException e) {
+            System.out.println(this + " interrupted");
+            return;
+        }
+        System.out.println(this + " completed");
+    }
+    public String toString() { return "Task " + id; }
+    public long id() { return id; }
+};
+
 public class InterruptableLongRunningTask extends JFrame {
     private JButton
             b1 = new JButton("Start Long Running Task"),

93b0abf254f7f2e7cb372093a0a37769979cb73f
Achilles-3v <sh17vv@gmail.com>
2024-06-13 21:21:25 +0500

    InterruptableLongRunningCallable added class CallableTask

--- src/chapter22/gui/InterruptableLongRunningCallable.java
+++ src/chapter22/gui/InterruptableLongRunningCallable.java
@@ -7,9 +7,19 @@ import java.util.concurrent.*;
 import net.achilles.util.*;
 import static net.achilles.util.SwingConsole.*;
 
+class CallableTask extends Task
+        implements Callable<String> {
+    public String call() {
+        run();
+        return "Return value of " + this;
+    }
+}
+
 public class InterruptableLongRunningCallable extends JFrame {
     private JButton
             b1 = new JButton("Start Long Running Task"),
             b2 = new JButton("End Long Running Task"),
             b3 = new JButton("Get results");
+    private TaskManager<String,CallableTask> manager =
+            new TaskManager<String,CallableTask>();
 }

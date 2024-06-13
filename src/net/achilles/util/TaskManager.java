3f1fb375d47d41821cc86e919a00f616b4be507e
Achilles-3v <sh17vv@gmail.com>
2024-06-13 21:16:17 +0500

    TaskManager added method gerResults

--- src/net/achilles/util/TaskManager.java
+++ src/net/achilles/util/TaskManager.java
@@ -10,4 +10,20 @@ public class TaskManager<R,C extends Callable<R>>
     public void add(C task) {
         add(new TaskItem<R,C>(exec.submit(task),task));
     }
+    public List<R> getResults() {
+        Iterator<TaskItem<R,C>> items = iterator();
+        List<R> results = new ArrayList<R>();
+        while(items.hasNext()) {
+            TaskItem<R,C> item = items.next();
+            if(item.future.isDone()) {
+                try {
+                    results.add(item.future.get());
+                } catch(Exception e) {
+                    throw new RuntimeException(e);
+                }
+                items.remove();
+            }
+        }
+        return results;
+    }
 }

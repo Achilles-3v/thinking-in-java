5a47a267a8ff08aee8b8d81f58429d1255a30cf0
Achilles-3v <sh17vv@gmail.com>
2024-06-13 21:20:35 +0500

    chapter22 added class InterruptableLongRunningCallable

---
+++ src/chapter22/gui/InterruptableLongRunningCallable.java
@@ -0,0 +1,15 @@
+package chapter22.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.*;
+import net.achilles.util.*;
+import static net.achilles.util.SwingConsole.*;
+
+public class InterruptableLongRunningCallable extends JFrame {
+    private JButton
+            b1 = new JButton("Start Long Running Task"),
+            b2 = new JButton("End Long Running Task"),
+            b3 = new JButton("Get results");
+}

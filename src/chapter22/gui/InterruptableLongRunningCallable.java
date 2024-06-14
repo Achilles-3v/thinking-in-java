4503f3a24d8c74171359d1ae89318d2112acf95e
Achilles-3v <sh17vv@gmail.com>
2024-06-14 22:37:12 +0500

    InterruptableLongRunningCallable added constructor

--- src/chapter22/gui/InterruptableLongRunningCallable.java
+++ src/chapter22/gui/InterruptableLongRunningCallable.java
@@ -22,4 +22,32 @@ public class InterruptableLongRunningCallable extends JFrame {
             b3 = new JButton("Get results");
     private TaskManager<String,CallableTask> manager =
             new TaskManager<String,CallableTask>();
+    public InterruptableLongRunningCallable() {
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                CallableTask task = new CallableTask();
+                manager.add(task);
+                System.out.println(task + " added to the queue");
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                for(String result : manager.purge())
+                    System.out.println(result);
+            }
+        });
+        b3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                for(TaskItem<String,CallableTask> tt :
+                        manager)
+                    tt.task.id();
+                for(String result : manager.getResults())
+                    System.out.println(result);
+            }
+        });
+        setLayout(new FlowLayout());
+        add(b1);
+        add(b2);
+        add(b3);
+    }
 }

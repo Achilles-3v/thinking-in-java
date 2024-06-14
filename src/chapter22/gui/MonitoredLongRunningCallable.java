8e7ff19939f8d187fffb42241b7c2057e3c297f4
Achilles-3v <sh17vv@gmail.com>
2024-06-14 22:43:18 +0500

    MonitoredLongRunningCallable added class MonitoredCallable

--- src/chapter22/gui/MonitoredLongRunningCallable.java
+++ src/chapter22/gui/MonitoredLongRunningCallable.java
@@ -7,6 +7,44 @@ import java.util.concurrent.*;
 import net.achilles.util.*;
 import static net.achilles.util.SwingConsole.*;
 
+class MonitoredCallable implements Callable<String> {
+    private static int counter = 0;
+    private final int id = counter++;
+    private final ProgressMonitor monitor;
+    private final static int MAX = 8;
+    public MonitoredCallable(ProgressMonitor monitor) {
+        this.monitor = monitor;
+        monitor.setNote(toString());
+        monitor.setMaximum(MAX - 1);
+        monitor.setMillisToPopup(500);
+    }
+    public String call() {
+        System.out.println(this + " started");
+        try {
+            for(int i = 0; i < MAX; i++) {
+                TimeUnit.MILLISECONDS.sleep(500);
+                if(monitor.isCanceled())
+                    Thread.currentThread().interrupt();
+                final int progress = i;
+                SwingUtilities.invokeLater(
+                        new Runnable() {
+                            public void run() {
+                                monitor.setProgress(progress);
+                            }
+                        }
+                );
+            }
+        } catch(InterruptedException e) {
+            monitor.close();
+            System.out.println(this + " interrupted");
+            return "Result: " + this + " interrupted";
+        }
+        System.out.println(this + " completed");
+        return "Result: " + this + " completed";
+    }
+    public String toString() { return "Task " + id; }
+};
+
 public class MonitoredLongRunningCallable extends JFrame {
     private JButton
             b1 = new JButton("Start Long Running Task"),

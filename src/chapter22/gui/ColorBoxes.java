5f2e345c80900861b124ef04690e6d3b8a291bf6
Achilles-3v <sh17vv@gmail.com>
2024-06-15 21:38:28 +0500

    ColorBoxes added class CBox

--- src/chapter22/gui/ColorBoxes.java
+++ src/chapter22/gui/ColorBoxes.java
@@ -6,6 +6,29 @@ import java.util.concurrent.*;
 import java.util.*;
 import static net.achilles.util.SwingConsole.*;
 
+class CBox extends JPanel implements Runnable {
+    private int pause;
+    private static Random rand = new Random();
+    private Color color = new Color(0);
+    public void paintComponent(Graphics g) {
+        g.setColor(color);
+        Dimension s = getSize();
+        g.fillRect(0, 0, s.width, s.height);
+    }
+    public CBox(int pause) { this.pause = pause; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                color = new Color(rand.nextInt(0xFFFFFF));
+                repaint(); // Asynchronously request a paint()
+                TimeUnit.MILLISECONDS.sleep(pause);
+            }
+        } catch(InterruptedException e) {
+            // Acceptable way to exit
+        }
+    }
+}
+
 public class ColorBoxes extends JFrame {
     private int grid = 12;
     private int pause = 50;
